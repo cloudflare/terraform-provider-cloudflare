3c968c778840211f777738751f268185858f1831
Ben Vickers <ben.vickers@opencredo.com>
2018-03-02 16:41:27 +0000

    add load balancer monitor resource + passing tests

---
+++ cloudflare/import_cloudflare_load_balancer_monitor_test.go
@@ -0,0 +1,27 @@
+package cloudflare
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccCloudFlareLoadBalancerMonitor_Import(t *testing.T) {
+	t.Parallel()
+	name := "cloudflare_load_balancer_monitor.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckCloudFlareLoadBalancerMonitorConfigBasic(),
+			},
+			{
+				ResourceName:      name,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
