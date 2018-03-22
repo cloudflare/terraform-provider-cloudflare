97b038f33bee473867e5b44799eafebaba0e1c4d
Ben Vickers <ben881@gmail.com>
2018-03-23 05:24:55 +0800

    Zone Settings Override resource (updated) (#41)
    * define schema for zone resource
    
    * get basic test working
    
    * fix default zone settings on resource
    
    * fix mapping for when zone settings are left as existing
    
    * fix create and delete, change settings resource to zone_settings_override
    
    * do meaningful test at api for zone settings
    
    * fix zone settings test error
    
    * error checking for setting non-scalars in zone settings override
    
    * add zone_settings_override resource docs
    
    * fix formatting in zone_settings_override docs
    
    * fix webp and always_use_https attrs in zone settings
    
    * zone settings override resource: fix comments and test assertions
    
    * zone settings override should only send overwritten attributes
    
    This prevents errors for users on restricted plans
    
    * remove excess logging + fix typos

--- cloudflare/utils.go
+++ cloudflare/utils.go
@@ -1,5 +1,14 @@
 package cloudflare
 
+func expandInterfaceToStringList(list interface{}) []string {
+	ifaceList := list.([]interface{})
+	vs := make([]string, 0, len(ifaceList))
+	for _, v := range ifaceList {
+		vs = append(vs, v.(string))
+	}
+	return vs
+}
+
 func flattenStringList(list []string) []interface{} {
 	vs := make([]interface{}, 0, len(list))
 	for _, v := range list {
