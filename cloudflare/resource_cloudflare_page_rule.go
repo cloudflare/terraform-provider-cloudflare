d38ec12527de22c8c9deaea895724d42975a6d07
Steve Goldthorpe - Wonga <steve.goldthorpe@wonga.com>
2018-05-15 16:59:55 +0100

    go fmt

--- cloudflare/resource_cloudflare_page_rule.go
+++ cloudflare/resource_cloudflare_page_rule.go
@@ -162,14 +162,14 @@ func resourceCloudFlarePageRule() *schema.Resource {
 						},
 
 						"host_header_override": {
-							Type:         schema.TypeString,
-							Optional:     true,
+							Type:     schema.TypeString,
+							Optional: true,
 						},
 
 						// should really validate if FQDN is resolveable, non-empty and < domain length limit will have to do
 						"resolve_override": {
-							Type:     schema.TypeString,
-							Optional: true,
+							Type:         schema.TypeString,
+							Optional:     true,
 							ValidateFunc: validation.StringLenBetween(1, 255),
 						},
 
