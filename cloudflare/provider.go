0f29f0b74911b6345cfb271ff54d171c924cc779
Patryk Szczygłowski <patryk@cloudflare.com>
2018-10-18 18:48:11 +0100

    New resource: cloudflare_zone. Closes #58

--- cloudflare/provider.go
+++ cloudflare/provider.go
@@ -98,6 +98,7 @@ func Provider() terraform.ResourceProvider {
 			"cloudflare_waf_rule":               resourceCloudflareWAFRule(),
 			"cloudflare_worker_route":           resourceCloudflareWorkerRoute(),
 			"cloudflare_worker_script":          resourceCloudflareWorkerScript(),
+			"cloudflare_zone":                   resourceCloudflareZone(),
 			"cloudflare_zone_lockdown":          resourceCloudflareZoneLockdown(),
 			"cloudflare_zone_settings_override": resourceCloudflareZoneSettingsOverride(),
 			"cloudflare_account_member":         resourceCloudflareAccountMember(),
