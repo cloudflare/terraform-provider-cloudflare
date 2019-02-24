072481417147e7f3b824284e321d634958ab6c95
Radek Simko <radek.simko@gmail.com>
2019-02-24 07:51:58 +0000

    resource/cloudflare_load_balancer_monitor: Fix incorrect data type cast for port

--- cloudflare/resource_cloudflare_load_balancer_monitor.go
+++ cloudflare/resource_cloudflare_load_balancer_monitor.go
@@ -143,7 +143,7 @@ func resourceCloudflareLoadBalancerPoolMonitorCreate(d *schema.ResourceData, met
 	}
 
 	if port, ok := d.GetOk("port"); ok {
-		loadBalancerMonitor.Port = port.(uint16)
+		loadBalancerMonitor.Port = uint16(port.(int))
 	}
 
 	log.Printf("[DEBUG] Creating Cloudflare Load Balancer Monitor from struct: %+v", loadBalancerMonitor)
@@ -188,7 +188,7 @@ func resourceCloudflareLoadBalancerPoolMonitorUpdate(d *schema.ResourceData, met
 	}
 
 	if port, ok := d.GetOk("port"); ok {
-		loadBalancerMonitor.Port = port.(uint16)
+		loadBalancerMonitor.Port = uint16(port.(int))
 	}
 
 	log.Printf("[DEBUG] Update Cloudflare Load Balancer Monitor from struct: %+v", loadBalancerMonitor)
