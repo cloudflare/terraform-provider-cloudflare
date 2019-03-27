eee38b586b32a4db5ad8f9cfc428cf6b7b0a7202
Jacob Bednarz <jacob.bednarz@gmail.com>
2019-03-27 11:11:08 +1100

    rate_limit: Don't set correlate unless it's needed
    We only want to set `rateLimit.Correlate` when it's required. This stops
    the value from continiously being updated in the Terraform state but not
    changed in the remote.
    
    Closes #256

--- cloudflare/resource_cloudflare_rate_limit.go
+++ cloudflare/resource_cloudflare_rate_limit.go
@@ -446,7 +446,10 @@ func resourceCloudflareRateLimitRead(d *schema.ResourceData, meta interface{}) e
 		log.Printf("[WARN] Error setting action on rate limit %q: %s", d.Id(), err)
 	}
 
-	d.Set("correlate", flattenRateLimitCorrelate(*rateLimit.Correlate))
+	if rateLimit.Correlate != nil {
+		d.Set("correlate", flattenRateLimitCorrelate(*rateLimit.Correlate))
+	}
+
 	d.Set("description", rateLimit.Description)
 	d.Set("disabled", rateLimit.Disabled)
 
