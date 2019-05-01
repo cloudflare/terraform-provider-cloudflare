74741c6729bda5faee68eea5217c86324adbc570
Jacob Bednarz <jacob.bednarz@gmail.com>
2019-05-02 07:41:56 +1000

    add zoneID to testAccPreCheck

--- cloudflare/import_cloudflare_spectrum_application_test.go
+++ cloudflare/import_cloudflare_spectrum_application_test.go
@@ -14,17 +14,7 @@ func TestAccCloudflareSpectrumApplication_Import(t *testing.T) {
 	t.Parallel()
 	var application cloudflare.SpectrumApplication
 	zone := os.Getenv("CLOUDFLARE_DOMAIN")
-
-	// This declaration can be removed once CLOUDFLARE_ZONE_ID is set in
-	// TeamCity for the acceptance tests. In the meantime, this environment
-	// variable needs to be set in order to match the CLOUDFLARE_DOMAIN the
-	// tests will run against. This is the zone ID for the
-	// hashicorptest.com zone.
-	zoneID := "25afd8e9b39af234c001b657a2eb2c5c"
-	if id := os.Getenv("CLOUDFLARE_ZONE_ID"); id != "" {
-		zoneID = id
-	}
-
+	zoneID := os.Getenv("CLOUDFLARE_ZONE_ID")
 	rnd := acctest.RandString(10)
 	name := "cloudflare_spectrum_application." + rnd
 
