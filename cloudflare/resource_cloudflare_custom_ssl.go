4b079a54631073a21fc2d59f618fd97910fdab4f
John Terzis <jterzis@cloudflare.com>
2019-07-29 11:04:26 -0700

    set partial for atomic updates

--- cloudflare/resource_cloudflare_custom_ssl.go
+++ cloudflare/resource_cloudflare_custom_ssl.go
@@ -153,39 +153,53 @@ func resourceCloudflareCustomSslUpdate(d *schema.ResourceData, meta interface{})
 	client := meta.(*cloudflare.API)
 	zoneID := d.Get("zone_id").(string)
 	certID := d.Id()
+	var uErr bool
+	var reErr bool
 	var updateErr bool = false
 	var reprioritizeErr bool = false
 	log.Printf("[DEBUG] zone ID: %s", zoneID)
 
-	zcso, err := expandToZoneCustomSSLOptions(d)
-	if err != nil {
-		return fmt.Errorf("Failed to update custom ssl cert: %s", err)
+	// Enable partial state mode for atomic subsequent updates
+	d.Partial(true)
+
+	if d.HasChange("custom_ssl_options") {
+		zcso, err := expandToZoneCustomSSLOptions(d)
+		if err != nil {
+			return fmt.Errorf("Failed to update custom ssl cert: %s", err)
+		}
+
+		res, uErr := client.UpdateSSL(zoneID, certID, zcso)
+		if uErr != nil {
+			log.Printf("[DEBUG] Failed to update custom ssl cert: %s", uErr)
+			updateErr = true
+		} else {
+			d.SetPartial("custom_ssl_options")
+			log.Printf("[DEBUG] Custom SSL set to: %s", res.ID)
+		}
+
 	}
 
-	res, uErr := client.UpdateSSL(zoneID, certID, zcso)
-	if uErr != nil {
-		log.Printf("[DEBUG] Failed to update custom ssl cert: %s", uErr)
-		updateErr = true
-	} else {
-		log.Printf("[DEBUG] Custom SSL set to: %s", res.ID)
-	}
+	if d.HasChange("custom_ssl_priority") {
+		zcsp, err := expandToZoneCustomSSLPriority(d)
+		if err != nil {
+			log.Printf("Failed to update custom ssl cert: %s", err)
+		}
 
-	zcsp, err := expandToZoneCustomSSLPriority(d)
-	if err != nil {
-		log.Printf("Failed to update custom ssl cert: %s", err)
-	}
-
-	resList, reErr := client.ReprioritizeSSL(zoneID, zcsp)
-	if err != nil {
-		log.Printf("Failed to update / reprioritize custom ssl cert: %s", reErr)
-		reprioritizeErr = true
-	} else {
-		log.Printf("[DEBUG] Custom SSL reprioritized to: %#v", resList)
+		resList, reErr := client.ReprioritizeSSL(zoneID, zcsp)
+		if err != nil {
+			log.Printf("Failed to update / reprioritize custom ssl cert: %s", reErr)
+			reprioritizeErr = true
+		} else {
+			d.SetPartial("custom_ssl_priority")
+			log.Printf("[DEBUG] Custom SSL reprioritized to: %#v", resList)
+		}
 	}
 
 	if updateErr && reprioritizeErr {
 		return fmt.Errorf("Failed to update and reprioritize custom ssl cert: %s, %s", uErr, reErr)
 	}
+	// We succeeded so disable partial mode
+	d.Partial(false)
 
 	return resourceCloudflareCustomSslRead(d, meta)
 }
