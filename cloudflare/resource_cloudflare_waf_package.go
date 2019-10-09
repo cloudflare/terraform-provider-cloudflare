730af4a11e56d41ebdc445eec0c1489c74a4b10e
Raphaël Beamonte <raphael.beamonte@shopify.com>
2019-10-09 17:06:01 -0400

    fixup! 066cdba163a07c9f62932e9f43b549e6909ad7f7

--- cloudflare/resource_cloudflare_waf_package.go
+++ cloudflare/resource_cloudflare_waf_package.go
@@ -5,8 +5,8 @@ import (
 	"strings"
 
 	cloudflare "github.com/cloudflare/cloudflare-go"
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/helper/validation"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
 func resourceCloudflareWAFPackage() *schema.Resource {
