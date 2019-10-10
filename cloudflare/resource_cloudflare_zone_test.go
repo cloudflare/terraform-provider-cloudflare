a1670cb95759d0c0996774d9a87d47b342f26c98
Francesco Gualazzi <francesco.gualazzi@lastminute.com>
2019-10-10 09:04:50 +0200

    cherry-pick dd8fbba50e408adce3b51d450531d7c15a215a6b with merge resolution

--- cloudflare/resource_cloudflare_zone_test.go
+++ cloudflare/resource_cloudflare_zone_test.go
@@ -73,6 +73,28 @@ func TestAccCloudflareZonePartialSetup(t *testing.T) {
 	})
 }
 
+func TestAccCloudflareZonePartialSetupEnterprise(t *testing.T) {
+	name := "cloudflare_zone.tf-acc-partial-setup-zone"
+	resourceName := strings.Split(name, ".")[1]
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testZoneConfigWithPartialSetup(resourceName, "foo.net", "false", "false", "enterprise"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(name, "zone", "foo.net"),
+					resource.TestCheckResourceAttr(name, "paused", "false"),
+					resource.TestCheckResourceAttr(name, "plan", planIDEnterprise),
+					resource.TestCheckResourceAttr(name, "type", "partial"),
+					resource.TestCheckResourceAttrSet(name, "verification_key"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccCloudflareZoneFullSetup(t *testing.T) {
 	name := "cloudflare_zone.tf-acc-full-setup-zone"
 	resourceName := strings.Split(name, ".")[1]
