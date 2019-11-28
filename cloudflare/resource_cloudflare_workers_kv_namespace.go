1096d6c57716d1d127c4fa203d6d1bb3d98ad925
Patryk Szczygłowski <patryk@patryk.net>
2019-11-28 16:31:28 +0000

    Update SDK import path

--- cloudflare/resource_cloudflare_workers_kv_namespace.go
+++ cloudflare/resource_cloudflare_workers_kv_namespace.go
@@ -6,7 +6,7 @@ import (
 	"log"
 
 	"github.com/cloudflare/cloudflare-go"
-	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/pkg/errors"
 )
 
