f1e1c550a32dc6b186d2abe53b828726312f6541
Patryk Szczygłowski <patryk@patryk.net>
2019-11-28 16:33:47 +0000

    Update resource_cloudflare_workers_kv_namespace_test.go

--- cloudflare/resource_cloudflare_workers_kv_namespace_test.go
+++ cloudflare/resource_cloudflare_workers_kv_namespace_test.go
@@ -6,8 +6,8 @@ import (
 	"testing"
 
 	"github.com/cloudflare/cloudflare-go"
-	"github.com/hashicorp/terraform/helper/resource"
-	"github.com/hashicorp/terraform/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func TestAccCloudflareWorkersKVNamespace_Basic(t *testing.T) {
