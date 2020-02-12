964404b9e34ca806036c5f68417c2ce31635a52b
Jacob Bednarz <jacob.bednarz@gmail.com>
2020-02-13 10:06:57 +1100

    fix resource create/update state

--- cloudflare/resource_cloudflare_access_identity_provider.go
+++ cloudflare/resource_cloudflare_access_identity_provider.go
@@ -10,6 +10,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 )
 
+const CONCEALED_STRING = "**********************************"
+
 func resourceCloudflareAccessIdentityProvider() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceCloudflareAccessIdentityProviderCreate,
@@ -24,6 +26,7 @@ func resourceCloudflareAccessIdentityProvider() *schema.Resource {
 			"account_id": {
 				Type:     schema.TypeString,
 				Required: true,
+				ForceNew: true,
 			},
 			"name": {
 				Type:     schema.TypeString,
@@ -32,7 +35,7 @@ func resourceCloudflareAccessIdentityProvider() *schema.Resource {
 			"type": {
 				Type:         schema.TypeString,
 				Required:     true,
-				ValidateFunc: validation.StringInSlice([]string{"centrify", "facebook", "google-apps", "oidc", "github", "google", "saml", "linkedin", "azureAD", "okta", "onetimepin", "onelogin", "authn", "yandex"}, false),
+				ValidateFunc: validation.StringInSlice([]string{"centrify", "facebook", "google-apps", "oidc", "github", "google", "saml", "linkedin", "azureAD", "okta", "onetimepin", "onelogin", "yandex"}, false),
 			},
 			"config": {
 				Type:     schema.TypeList,
@@ -44,7 +47,7 @@ func resourceCloudflareAccessIdentityProvider() *schema.Resource {
 							Optional: true,
 						},
 						"attributes": {
-							Type:     schema.TypeMap,
+							Type:     schema.TypeList,
 							Optional: true,
 							Elem: &schema.Schema{
 								Type: schema.TypeString,
@@ -73,12 +76,11 @@ func resourceCloudflareAccessIdentityProvider() *schema.Resource {
 						"client_secret": {
 							Type:     schema.TypeString,
 							Optional: true,
-							ForceNew: true,
 							// client_secret is a write only operation from the Cloudflare API
 							// and once it's set, it is no longer accessible. To avoid storing
 							// it and messing up the state, hardcode in the concealed version.
 							StateFunc: func(val interface{}) string {
-								return "**********************************"
+								return CONCEALED_STRING
 							},
 						},
 						"directory_id": {
@@ -92,6 +94,13 @@ func resourceCloudflareAccessIdentityProvider() *schema.Resource {
 						"idp_public_cert": {
 							Type:     schema.TypeString,
 							Optional: true,
+							// idp_public_cert is a write only operation from the Cloudflare
+							// API and once it's set, it is no longer accessible. To avoid
+							// storing it and messing up the state, hardcode in the concealed
+							// version.
+							StateFunc: func(val interface{}) string {
+								return CONCEALED_STRING
+							},
 						},
 						"issuer_url": {
 							Type:     schema.TypeString,
@@ -104,6 +113,11 @@ func resourceCloudflareAccessIdentityProvider() *schema.Resource {
 						"onelogin_account": {
 							Type:     schema.TypeString,
 							Optional: true,
+						},
+						"redirect_url": {
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
 						},
 						"sign_request": {
 							Type:     schema.TypeBool,
@@ -143,10 +157,9 @@ func resourceCloudflareAccessIdentityProviderRead(d *schema.ResourceData, meta i
 	}
 
 	d.SetId(accessIdentityProvider.ID)
+	d.Set("account_id", accountID)
 	d.Set("name", accessIdentityProvider.Name)
 	d.Set("type", accessIdentityProvider.Type)
-
-	fmt.Printf("concertStructToSchema: %+v", convertStructToSchema(d, accessIdentityProvider.Config))
 
 	config := convertStructToSchema(d, accessIdentityProvider.Config)
 	if configErr := d.Set("config", config); configErr != nil {
@@ -159,12 +172,8 @@ func resourceCloudflareAccessIdentityProviderRead(d *schema.ResourceData, meta i
 func resourceCloudflareAccessIdentityProviderCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*cloudflare.API)
 	accountID := d.Get("account_id").(string)
-	IDPConfig := cloudflare.AccessIdentityProviderConfiguration{}
 
-	if _, ok := d.GetOk("config"); ok {
-		IDPConfig.ClientID = d.Get("config.0.client_id").(string)
-		IDPConfig.ClientSecret = d.Get("config.0.client_secret").(string)
-	}
+	IDPConfig, _ := convertSchemaToStruct(d)
 
 	identityProvider := cloudflare.AccessIdentityProvider{
 		Name:   d.Get("name").(string),
@@ -188,18 +197,9 @@ func resourceCloudflareAccessIdentityProviderUpdate(d *schema.ResourceData, meta
 	client := meta.(*cloudflare.API)
 	accountID := d.Get("account_id").(string)
 
-	IDPConfig := cloudflare.AccessIdentityProviderConfiguration{}
-
-	if _, ok := d.GetOk("config"); ok {
-		tfAction := d.Get("config").([]interface{})[0].(map[string]interface{})
-
-		fmt.Printf("tfAction: %+v", tfAction)
-
-		clientID := tfAction["client_id"].(string)
-		clientSecret := tfAction["client_secret"].(string)
-
-		IDPConfig.ClientID = clientID
-		IDPConfig.ClientSecret = clientSecret
+	IDPConfig, conversionErr := convertSchemaToStruct(d)
+	if conversionErr != nil {
+		return fmt.Errorf("failed to convert schema into struct: %s", conversionErr)
 	}
 
 	log.Printf("[DEBUG] updatedConfig: %+v", IDPConfig)
@@ -240,41 +240,89 @@ func resourceCloudflareAccessIdentityProviderDelete(d *schema.ResourceData, meta
 }
 
 func resourceCloudflareAccessIdentityProviderImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
-	// attributes := strings.SplitN(d.Id(), "/", 3)
+	attributes := strings.SplitN(d.Id(), "/", 2)
 
-	// if len(attributes) != 3 {
-	// 	return nil, fmt.Errorf("invalid id (\"%s\") specified, should be in format \"zoneID/accessApplicationID/accessPolicyID\"", d.Id())
-	// }
+	if len(attributes) != 2 {
+		return nil, fmt.Errorf("invalid id (\"%s\") specified, should be in format \"accountID/accessIdentityProviderID\"", d.Id())
+	}
 
-	// zoneID, accessAppID, accessPolicyID := attributes[0], attributes[1], attributes[2]
+	accountID, accessIdentityProviderID := attributes[0], attributes[1]
 
-	// log.Printf("[DEBUG] Importing Cloudflare Access Policy: zoneID %q, appID %q, accessPolicyID %q", zoneID, accessAppID, accessPolicyID)
+	log.Printf("[DEBUG] Importing Cloudflare Access Identity Provider: accountID=%s accessIdentityProviderID=%s", accountID, accessIdentityProviderID)
 
-	// d.Set("zone_id", zoneID)
-	// d.Set("application_id", accessAppID)
-	// d.SetId(accessPolicyID)
+	d.Set("account_id", accountID)
+	d.SetId(accessIdentityProviderID)
 
-	// resourceCloudflareAccessIdentityProviderRead(d, meta)
+	resourceCloudflareAccessIdentityProviderRead(d, meta)
 
 	return []*schema.ResourceData{d}, nil
 }
 
-// func convertSchemaToStruct(d *schema.ResourceData) (cloudflare.AccessIdentityProviderConfiguration, error) {
+func convertSchemaToStruct(d *schema.ResourceData) (cloudflare.AccessIdentityProviderConfiguration, error) {
+	IDPConfig := cloudflare.AccessIdentityProviderConfiguration{}
 
-// 	fmt.Printf("IDPConfig: %+v", IDPConfig)
+	if _, ok := d.GetOk("config"); ok {
+		if _, ok := d.GetOk("config.0.attributes"); ok {
+			attrData := make([]string, d.Get("config.0.attributes.#").(int))
+			for id := range attrData {
+				attrData[id] = d.Get(fmt.Sprintf("config.0.attributes.%d", id)).(string)
+			}
+			IDPConfig.Attributes = attrData
+		}
 
-// 	return IDPConfig, nil
-// }
+		IDPConfig.AppsDomain = d.Get("config.0.apps_domain").(string)
+		IDPConfig.AuthURL = d.Get("config.0.auth_url").(string)
+		IDPConfig.CentrifyAccount = d.Get("config.0.centrify_account").(string)
+		IDPConfig.CentrifyAppID = d.Get("config.0.centrify_app_id").(string)
+		IDPConfig.CertsURL = d.Get("config.0.certs_url").(string)
+		IDPConfig.ClientID = d.Get("config.0.client_id").(string)
+		IDPConfig.ClientSecret = d.Get("config.0.client_secret").(string)
+		IDPConfig.DirectoryID = d.Get("config.0.directory_id").(string)
+		IDPConfig.EmailAttributeName = d.Get("config.0.email_attribute_name").(string)
+		IDPConfig.IdpPublicCert = d.Get("config.0.idp_public_cert").(string)
+		IDPConfig.IssuerURL = d.Get("config.0.issuer_url").(string)
+		IDPConfig.OktaAccount = d.Get("config.0.okta_account").(string)
+		IDPConfig.OneloginAccount = d.Get("config.0.onelogin_account").(string)
+		IDPConfig.RedirectURL = d.Get("config.0.redirect_url").(string)
+		IDPConfig.SignRequest = d.Get("config.0.sign_request").(bool)
+		IDPConfig.SsoTargetURL = d.Get("config.0.sso_target_url").(string)
+		IDPConfig.SupportGroups = d.Get("config.0.support_groups").(bool)
+		IDPConfig.TokenURL = d.Get("config.0.token_url").(string)
+	}
+
+	return IDPConfig, nil
+}
 
 func convertStructToSchema(d *schema.ResourceData, options cloudflare.AccessIdentityProviderConfiguration) []interface{} {
-	// todo: find a better way of confirming we have options
-	if options.ClientID == "" {
+	if _, ok := d.GetOk("config"); !ok {
 		return []interface{}{}
 	}
 
+	attributes := make([]string, 0)
+	for _, value := range options.Attributes {
+		attributes = append(attributes, value)
+	}
+
 	m := map[string]interface{}{
-		"client_id":     options.ClientID,
-		"client_secret": options.ClientSecret,
+		"apps_domain":          options.AppsDomain,
+		"attributes":           attributes,
+		"auth_url":             options.AuthURL,
+		"centrify_account":     options.CentrifyAccount,
+		"centrify_app_id":      options.CentrifyAppID,
+		"certs_url":            options.CertsURL,
+		"client_id":            options.ClientID,
+		"client_secret":        options.ClientSecret,
+		"directory_id":         options.DirectoryID,
+		"email_attribute_name": options.EmailAttributeName,
+		"idp_public_cert":      options.IdpPublicCert,
+		"issuer_url":           options.IssuerURL,
+		"okta_account":         options.OktaAccount,
+		"onelogin_account":     options.OneloginAccount,
+		"redirect_url":         options.RedirectURL,
+		"sign_request":         options.SignRequest,
+		"sso_target_url":       options.SsoTargetURL,
+		"support_groups":       options.SupportGroups,
+		"token_url":            options.TokenURL,
 	}
 
 	return []interface{}{m}
