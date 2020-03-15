02329c1c8d197ef6528eee0e1edff4da543c718b
Jacob Bednarz <jacob.bednarz@gmail.com>
2020-03-16 08:13:13 +1100

    unset API_TOKEN for Access tests as it doesn't support it

--- cloudflare/resource_cloudflare_access_policy_test.go
+++ cloudflare/resource_cloudflare_access_policy_test.go
@@ -9,6 +9,16 @@ import (
 )
 
 func TestAccAccessPolicyServiceToken(t *testing.T) {
+	// Temporarily unset CLOUDFLARE_API_TOKEN if it is set as the Access
+	// service does not yet support the API tokens and it results in
+	// misleading state error messages.
+	if os.Getenv("CLOUDFLARE_API_TOKEN") != "" {
+		defer func(apiToken string) {
+			os.Setenv("CLOUDFLARE_API_TOKEN", apiToken)
+		}(os.Getenv("CLOUDFLARE_API_TOKEN"))
+		os.Setenv("CLOUDFLARE_API_TOKEN", "")
+	}
+
 	rnd := generateRandomResourceName()
 	name := "cloudflare_access_policy." + rnd
 	zone := os.Getenv("CLOUDFLARE_DOMAIN")
@@ -35,6 +45,16 @@ func TestAccAccessPolicyServiceToken(t *testing.T) {
 }
 
 func TestAccAccessPolicyAnyServiceToken(t *testing.T) {
+	// Temporarily unset CLOUDFLARE_API_TOKEN if it is set as the Access
+	// service does not yet support the API tokens and it results in
+	// misleading state error messages.
+	if os.Getenv("CLOUDFLARE_API_TOKEN") != "" {
+		defer func(apiToken string) {
+			os.Setenv("CLOUDFLARE_API_TOKEN", apiToken)
+		}(os.Getenv("CLOUDFLARE_API_TOKEN"))
+		os.Setenv("CLOUDFLARE_API_TOKEN", "")
+	}
+
 	rnd := generateRandomResourceName()
 	name := "cloudflare_access_policy." + rnd
 	zone := os.Getenv("CLOUDFLARE_DOMAIN")
