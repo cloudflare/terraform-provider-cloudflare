b1590216781a70ad2e6381cc9b1082f8c8a66135
Jacob Bednarz <jacob.bednarz@gmail.com>
2021-01-11 11:54:38 +1100

    access_policy: err check `d.Set` statements on complex attributes
    To avoid swallowing exception messages we should err check `d.Set` calls
    where the value isn't a simple type to avoid missing underlying issues.

--- cloudflare/resource_cloudflare_access_policy.go
+++ cloudflare/resource_cloudflare_access_policy.go
@@ -96,9 +96,18 @@ func resourceCloudflareAccessPolicyRead(d *schema.ResourceData, meta interface{}
 	d.Set("name", accessPolicy.Name)
 	d.Set("decision", accessPolicy.Decision)
 	d.Set("precedence", accessPolicy.Precedence)
-	d.Set("require", accessPolicy.Require)
-	d.Set("exclude", accessPolicy.Exclude)
-	d.Set("include", accessPolicy.Include)
+
+	if err := d.Set("require", accessPolicy.Require); err != nil {
+		return fmt.Errorf("failed to set require attribute: %s", err)
+	}
+
+	if err := d.Set("exclude", accessPolicy.Exclude); err != nil {
+		return fmt.Errorf("failed to set exclude attribute: %s", err)
+	}
+
+	if err := d.Set("include", accessPolicy.Include); err != nil {
+		return fmt.Errorf("failed to set include attribute: %s", err)
+	}
 
 	return nil
 }
