8e188107eae0be7c5482694185b4aeb3f5218f90
Jacob Bednarz <jacob.bednarz@gmail.com>
2021-02-01 14:22:35 +1100

    use correct JSON key for lookup

--- cloudflare/resource_cloudflare_access_group.go
+++ cloudflare/resource_cloudflare_access_group.go
@@ -597,7 +597,7 @@ func TransformAccessGroupForSchema(accessGroup []interface{}) []map[string]inter
 				gsuiteCfg := groupValue.(map[string]interface{})
 				gsuiteID = gsuiteCfg["identity_provider_id"].(string)
 				gsuiteEmails = append(gsuiteEmails, gsuiteCfg["name"].(string))
-			case "github":
+			case "github-organization":
 				githubCfg := groupValue.(map[string]interface{})
 				githubID = githubCfg["identity_provider_id"].(string)
 				githubName = githubCfg["name"].(string)
