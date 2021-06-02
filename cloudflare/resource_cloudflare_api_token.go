733de44b800ea7aab4f958e70f3d8758da3ec454
uros.simovic <uros.simovic@kiwi.com>
2021-06-02 13:22:34 +0200

    fix(APIToken): change 'permission_groups' from list to set of strings

--- cloudflare/resource_cloudflare_api_token.go
+++ cloudflare/resource_cloudflare_api_token.go
@@ -88,7 +88,8 @@ func resourceDataToApiTokenPolices(d *schema.ResourceData) []cloudflare.APIToken
 	for _, p := range policies {
 		policy := p.(map[string]interface{})
 
-		permissionGroups := expandInterfaceToStringList(policy["permission_groups"])
+		permissionGroups := expandSetToStringList(policy["permission_groups"])
+
 		if len(permissionGroups) == 0 {
 			continue
 		}
