41a5512e0ed88cca3785aef7c9be08568ea392cc
Jacob Bednarz <jacob.bednarz@gmail.com>
2021-07-19 09:41:21 +1000

    resource/load_balancing_pool: fix test assertion for TypeSet

--- cloudflare/resource_cloudflare_load_balancer_pool_test.go
+++ cloudflare/resource_cloudflare_load_balancer_pool_test.go
@@ -64,10 +64,10 @@ func TestAccCloudflareLoadBalancerPool_FullySpecified(t *testing.T) {
 					// checking our overrides of default values worked
 					resource.TestCheckResourceAttr(name, "enabled", "false"),
 					resource.TestCheckResourceAttr(name, "load_shedding.#", "1"),
-					resource.TestCheckResourceAttr(name, "load_shedding.0.default_percent", "55"),
-					resource.TestCheckResourceAttr(name, "load_shedding.0.default_policy", "random"),
-					resource.TestCheckResourceAttr(name, "load_shedding.0.session_percent", "12"),
-					resource.TestCheckResourceAttr(name, "load_shedding.0.session_policy", "hash"),
+					resource.TestCheckResourceAttr(name, "load_shedding.2749995019.default_percent", "55"),
+					resource.TestCheckResourceAttr(name, "load_shedding.2749995019.default_policy", "random"),
+					resource.TestCheckResourceAttr(name, "load_shedding.2749995019.session_percent", "12"),
+					resource.TestCheckResourceAttr(name, "load_shedding.2749995019.session_policy", "hash"),
 					resource.TestCheckResourceAttr(name, "description", "tfacc-fully-specified"),
 					resource.TestCheckResourceAttr(name, "check_regions.#", "1"),
 					resource.TestCheckResourceAttr(name, "minimum_origins", "2"),
@@ -226,31 +226,35 @@ func testAccCheckCloudflareLoadBalancerPoolConfigFullySpecified(id string, heade
 	return fmt.Sprintf(`
 resource "cloudflare_load_balancer_pool" "%[1]s" {
   name = "my-tf-pool-basic-%[1]s"
+
   origins {
     name = "example-1"
     address = "192.0.2.1"
     enabled = false
     weight = 1.0
     header {
-    	header = "Host"
-    	values = ["test1.%[2]s"]
- 	}
+      header = "Host"
+      values = ["test1.%[2]s"]
+     }
   }
+
   origins {
     name = "example-2"
     address = "192.0.2.2"
     weight = 0.5
     header {
-    	header = "Host"
-    	values = ["test2.%[2]s"]
- 	}
+      header = "Host"
+      values = ["test2.%[2]s"]
+    }
   }
+
   load_shedding {
-	default_percent = 55
-	default_policy = "random"
-	session_percent = 12
-	session_policy = "hash"
+    default_percent = 55
+    default_policy = "random"
+    session_percent = 12
+    session_policy = "hash"
   }
+
   check_regions = ["WEU"]
   description = "tfacc-fully-specified"
   enabled = false
