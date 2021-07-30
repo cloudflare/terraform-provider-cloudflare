baf63d740fc2ff6b472689fb12c12c7a37c71dee
Jacob Bednarz <jacob.bednarz@hey.com>
2021-07-30 14:24:29 +1000

    give everything a little ore breathing room

--- cloudflare/resource_cloudflare_notification_policy.go
+++ cloudflare/resource_cloudflare_notification_policy.go
@@ -108,6 +108,7 @@ func resourceCloudflareNotificationPolicyCreate(d *schema.ResourceData, meta int
 
 	return resourceCloudflareNotificationPolicyRead(d, meta)
 }
+
 func resourceCloudflareNotificationPolicyRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*cloudflare.API)
 	policyID := d.Id()
@@ -129,6 +130,7 @@ func resourceCloudflareNotificationPolicyRead(d *schema.ResourceData, meta inter
 	d.Set("conditions", policy.Result.Conditions)
 	d.Set("created", policy.Result.Created.Format(time.RFC3339))
 	d.Set("modified", policy.Result.Modified.Format(time.RFC3339))
+
 	if err := d.Set("email_integration", setNotificationMechanisms(policy.Result.Mechanisms["email"])); err != nil {
 		return fmt.Errorf("failed to set email integration: %s", err)
 	}
@@ -138,6 +140,7 @@ func resourceCloudflareNotificationPolicyRead(d *schema.ResourceData, meta inter
 	if err := d.Set("webhooks_integration", setNotificationMechanisms(policy.Result.Mechanisms["webhooks"])); err != nil {
 		return fmt.Errorf("failed to set webhooks integration: %s", err)
 	}
+
 	return nil
 }
 
@@ -157,6 +160,7 @@ func resourceCloudflareNotificationPolicyUpdate(d *schema.ResourceData, meta int
 
 	return resourceCloudflareNotificationPolicyRead(d, meta)
 }
+
 func resourceCloudflareNotificationPolicyDelete(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*cloudflare.API)
 	policyID := d.Id()
@@ -169,6 +173,7 @@ func resourceCloudflareNotificationPolicyDelete(d *schema.ResourceData, meta int
 	}
 	return nil
 }
+
 func resourceNotificationPolicyImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
 	attributes := strings.SplitN(d.Id(), "/", 2)
 
@@ -185,6 +190,7 @@ func resourceNotificationPolicyImport(d *schema.ResourceData, meta interface{}) 
 	return []*schema.ResourceData{d}, nil
 
 }
+
 func buildNotificationPolicy(d *schema.ResourceData) cloudflare.NotificationPolicy {
 	notificationPolicy := cloudflare.NotificationPolicy{}
 	notificationPolicy.Mechanisms = make(map[string]cloudflare.NotificationMechanismIntegrations)
@@ -194,35 +200,45 @@ func buildNotificationPolicy(d *schema.ResourceData) cloudflare.NotificationPoli
 	if name, ok := d.GetOk("name"); ok {
 		notificationPolicy.Name = name.(string)
 	}
+
 	if description, ok := d.GetOk("description"); ok {
 		notificationPolicy.Description = description.(string)
 	}
+
 	if enabled, ok := d.GetOk("enabled"); ok {
 		notificationPolicy.Enabled = enabled.(bool)
 	}
+
 	if alertType, ok := d.GetOk("alert_type"); ok {
 		notificationPolicy.AlertType = alertType.(string)
 	}
+
 	if emails, ok := d.GetOk("email_integration"); ok {
 		notificationPolicy.Mechanisms["email"] = getNotificationMechanisms(emails.(*schema.Set))
 	}
+
 	if webhooks, ok := d.GetOk("webhooks_integration"); ok {
 		notificationPolicy.Mechanisms["webhooks"] = getNotificationMechanisms(webhooks.(*schema.Set))
 	}
+
 	if pagerduty, ok := d.GetOk("pagerduty_integration"); ok {
 		notificationPolicy.Mechanisms["pagerduty"] = getNotificationMechanisms(pagerduty.(*schema.Set))
 	}
+
 	if filters, ok := d.GetOk("filters"); ok {
 		notificationPolicy.Filters = filters.(map[string][]string)
 	}
+
 	if conditions, ok := d.GetOk("conditions"); ok {
 		notificationPolicy.Conditions = conditions.(map[string]interface{})
 	}
+
 	return notificationPolicy
 }
 
 func getNotificationMechanisms(s *schema.Set) []cloudflare.NotificationMechanismData {
 	var notificationMechanisms []cloudflare.NotificationMechanismData
+
 	for _, m := range s.List() {
 		mechanism := m.(map[string]interface{})
 		data := cloudflare.NotificationMechanismData{
@@ -231,16 +247,19 @@ func getNotificationMechanisms(s *schema.Set) []cloudflare.NotificationMechanism
 		}
 		notificationMechanisms = append(notificationMechanisms, data)
 	}
+
 	return notificationMechanisms
 }
 
 func setNotificationMechanisms(md []cloudflare.NotificationMechanismData) *schema.Set {
 	mechanisms := make([]interface{}, 0)
 	data := make(map[string]interface{})
+
 	for _, m := range md {
 		data["name"] = m.Name
 		data["id"] = m.ID
 		mechanisms = append(mechanisms, data)
 	}
+
 	return schema.NewSet(schema.HashResource(mechanismData), mechanisms)
 }
