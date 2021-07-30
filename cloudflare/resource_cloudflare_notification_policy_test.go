d4e0198d381b53b0374c8dc0395a800cfa7f85f1
Jacob Bednarz <jacob.bednarz@hey.com>
2021-07-30 14:26:40 +1000

    remove duplicate test case
    the second test covers creation + update so we don't need the first one.

--- cloudflare/resource_cloudflare_notification_policy_test.go
+++ cloudflare/resource_cloudflare_notification_policy_test.go
@@ -8,35 +8,11 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
-func TestAccCloudflareCreateNotificationPolicy(t *testing.T) {
+func TestAccCloudflareNotificationPolicy(t *testing.T) {
 	rnd := generateRandomResourceName()
 	resourceName := "cloudflare_notification_policy." + rnd
-	accountID := os.Getenv("CLOUDFLARE_ACCOUNT_ID")
-	resource.Test(t, resource.TestCase{
-		PreCheck: func() {
-			testAccPreCheckAccount(t)
-		},
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testCheckCloudflareNotificationPolicy(rnd, accountID),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "name", "test SSL policy from terraform provider"),
-					resource.TestCheckResourceAttr(resourceName, "description", "test description"),
-					resource.TestCheckResourceAttr(resourceName, "enabled", "true"),
-					resource.TestCheckResourceAttr(resourceName, "alert_type", "universal_ssl_event_type"),
-					resource.TestCheckResourceAttr(resourceName, "account_id", accountID),
-				),
-			},
-		},
-	})
-}
-
-func TestAccCloudflareUpdateNotificationPolicy(t *testing.T) {
-	rnd := generateRandomResourceName()
-	resourceName := "cloudflare_notification_policy." + rnd
-	updatedPolicyName := "*updated* test SSL policy from terraform provider"
-	updatedPolicyDesc := "*updated* description"
+	updatedPolicyName := "updated test SSL policy from terraform provider"
+	updatedPolicyDesc := "updated description"
 	accountID := os.Getenv("CLOUDFLARE_ACCOUNT_ID")
 
 	resource.Test(t, resource.TestCase{
@@ -72,29 +48,29 @@ func TestAccCloudflareUpdateNotificationPolicy(t *testing.T) {
 func testCheckCloudflareNotificationPolicy(name, accountID string) string {
 	return fmt.Sprintf(`
   resource "cloudflare_notification_policy" "%[1]s" {
-	  name        = "test SSL policy from terraform provider"
-	account_id    = "%[2]s"
-    description   = "test description"
-	enabled       =  true
-	alert_type    = "universal_ssl_event_type"
-	email_integration {
-		name =  "test@cloudflare.com"
-		id   =  ""
-}	
-}`, name, accountID)
+    name        = "test SSL policy from terraform provider"
+    account_id  = "%[2]s"
+    description = "test description"
+    enabled     =  true
+    alert_type  = "universal_ssl_event_type"
+    email_integration {
+      name =  "test@cloudflare.com"
+      id   =  ""
+    }
+  }`, name, accountID)
 }
 
 func testCheckCloudflareNotificationPolicyUpdated(resName, policyName, policyDesc, accountID string) string {
 	return fmt.Sprintf(`
   resource "cloudflare_notification_policy" "%[1]s" {
-	  name        = "%[2]s"
-	account_id    = "%[4]s"
-    description   = "%[3]s"
-	enabled       =  true
-	alert_type    = "universal_ssl_event_type"
-	email_integration {
-		name =  "test@cloudflare.com"
-		id   =  ""
-}		
-}`, resName, policyName, policyDesc, accountID)
+    name        = "%[2]s"
+    account_id  = "%[4]s"
+    description = "%[3]s"
+    enabled     =  true
+    alert_type  = "universal_ssl_event_type"
+    email_integration {
+      name =  "test@cloudflare.com"
+      id   =  ""
+    }
+  }`, resName, policyName, policyDesc, accountID)
 }
