63fb5f4a554b0b5fc3aabfa0cb52c6c32f47105c
Mike <Mibor2000@gmail.com>
2021-08-26 18:58:45 -0500

    GATE-1848: Adds support for gateway location API (#1154)
    Co-authored-by: Michael Borkenstein <mborkenstein@cloudflare.com>

---
+++ cloudflare/resource_cloudflare_teams_location_test.go
@@ -0,0 +1,60 @@
+package cloudflare
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/cloudflare/cloudflare-go"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccCloudflareTeamsLocationBasic(t *testing.T) {
+	rnd := generateRandomResourceName()
+	name := fmt.Sprintf("cloudflare_teams_location.%s", rnd)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccessAccPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCloudflareTeamsLocationDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudflareTeamsLocationConfigBasic(rnd, accountID),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(name, "account_id", accountID),
+					resource.TestCheckResourceAttr(name, "name", rnd),
+					resource.TestCheckResourceAttr(name, "client_default", "false"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCloudflareTeamsLocationConfigBasic(rnd, accountID string) string {
+	return fmt.Sprintf(`
+resource "cloudflare_teams_location" "%[1]s" {
+  name        = "%[1]s"
+  account_id  = "%[2]s"
+}
+`, rnd, accountID)
+}
+
+func testAccCheckCloudflareTeamsLocationDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*cloudflare.API)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "cloudflare_teams_location" {
+			continue
+		}
+
+		_, err := client.TeamsLocation(context.Background(), rs.Primary.Attributes["account_id"], rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("teams Location still exists")
+		}
+	}
+
+	return nil
+}
