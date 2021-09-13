9b1587a711d8027cd5509500e744940290c9cf80
Eduardo Gomes <egomes@cloudflare.com>
2021-09-13 19:49:51 +0100

    AUTH-3689: Adds support for access keys configuration

--- cloudflare/provider.go
+++ cloudflare/provider.go
@@ -113,6 +113,7 @@ func Provider() terraform.ResourceProvider {
 			"cloudflare_access_service_token":                   resourceCloudflareAccessServiceToken(),
 			"cloudflare_access_identity_provider":               resourceCloudflareAccessIdentityProvider(),
 			"cloudflare_access_mutual_tls_certificate":          resourceCloudflareAccessMutualTLSCertificate(),
+			"cloudflare_access_keys_configuration":              resourceCloudflareAccessKeysConfiguration(),
 			"cloudflare_account_member":                         resourceCloudflareAccountMember(),
 			"cloudflare_api_token":                              resourceCloudflareApiToken(),
 			"cloudflare_argo":                                   resourceCloudflareArgo(),
---
+++ cloudflare/resource_cloudflare_access_keys_configuration.go
@@ -0,0 +1,97 @@
+package cloudflare
+
+import (
+	"context"
+	"fmt"
+	cloudflare "github.com/cloudflare/cloudflare-go"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"log"
+	"strings"
+)
+
+func resourceCloudflareAccessKeysConfiguration() *schema.Resource {
+	return &schema.Resource{
+		Read:   resourceCloudflareAccessKeysConfigurationRead,
+		Create: resourceCloudflareAccessKeysConfigurationCreate,
+		Update: resourceCloudflareAccessKeysConfigurationUpdate,
+		Delete: resourceCloudflareKeysConfigDelete,
+		Importer: &schema.ResourceImporter{
+			State: resourceCloudflareKeysConfigImport,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"account_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"key_rotation_interval_days": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceCloudflareAccessKeysConfigurationRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*cloudflare.API)
+	accountID := d.Get("account_id").(string)
+
+	keysConfig, err := client.AccessKeysConfig(context.Background(), accountID)
+	if err != nil {
+		if strings.Contains(err.Error(), "HTTP status 404") {
+			log.Printf("[INFO] Access Keys Configuration %s not enabled for account %s", d.Id(), accountID)
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("error finding Access Keys Configuration %s: %s", d.Id(), err)
+	}
+
+	d.SetId(accountID)
+	d.Set("key_rotation_interval_days", keysConfig.KeyRotationIntervalDays)
+
+	return nil
+}
+
+func resourceCloudflareAccessKeysConfigurationCreate(d *schema.ResourceData, meta interface{}) error {
+	// keys configuration share the same lifetime as an organization, so creating is a no-op, unless
+	// key_rotation_interval_days was explicitly passed, in which case we need to update its value.
+
+	if keyRotationIntervalDays := d.Get("key_rotation_interval_days").(int); keyRotationIntervalDays == 0 {
+		return resourceCloudflareAccessKeysConfigurationRead(d, meta)
+	}
+
+	return resourceCloudflareAccessKeysConfigurationUpdate(d, meta)
+}
+
+func resourceCloudflareAccessKeysConfigurationUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*cloudflare.API)
+	accountID := d.Get("account_id").(string)
+
+	keysConfigUpdateReq := cloudflare.AccessKeysConfigUpdateRequest{
+		KeyRotationIntervalDays: d.Get("key_rotation_interval_days").(int),
+	}
+
+	_, err := client.UpdateAccessKeysConfig(context.Background(), accountID, keysConfigUpdateReq)
+	if err != nil {
+		return fmt.Errorf("error updating Access Keys Configuration %s for account %s: %s", d.Id(), accountID, err)
+	}
+
+	return resourceCloudflareAccessKeysConfigurationRead(d, meta)
+}
+
+func resourceCloudflareKeysConfigDelete(_ *schema.ResourceData, _ interface{}) error {
+	// keys configuration share the same lifetime as an organization, and can not be
+	// explicitly deleted by the user. so this is a no-op.
+	return nil
+}
+
+func resourceCloudflareKeysConfigImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	accountID := d.Id()
+
+	d.SetId(accountID)
+	d.Set("account_id", accountID)
+
+	err := resourceCloudflareAccessKeysConfigurationRead(d, meta)
+	return []*schema.ResourceData{d}, err
+}
