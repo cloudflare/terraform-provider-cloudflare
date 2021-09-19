16d6af9b60644273d2fc66b40710d5e54a55f85d
Eduardo Gomes <technikome@gmail.com>
2021-09-19 22:58:30 +0100

    AUTH-3689: Adds support for access keys configuration (#1186)
    Co-authored-by: Eduardo Gomes <egomes@cloudflare.com>
    Co-authored-by: Jacob Bednarz <jacob.bednarz@gmail.com>

---
+++ cloudflare/resource_cloudflare_access_keys_configuration_test.go
@@ -0,0 +1,62 @@
+package cloudflare
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccCloudflareAccessKeysConfiguration_WithKeyRotationIntervalDaysSet(t *testing.T) {
+	rnd := generateRandomResourceName()
+	name := fmt.Sprintf("cloudflare_access_keys_configuration.%s", rnd)
+	accountID := os.Getenv("CLOUDFLARE_ACCOUNT_ID")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccessAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccessKeysConfigurationWithKeyRotationIntervalDays(rnd, accountID, 60),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(name, "key_rotation_interval_days", "60"),
+				),
+			},
+		},
+	})
+}
+
+func testAccessKeysConfigurationWithKeyRotationIntervalDays(rnd, accountID string, days int) string {
+	return fmt.Sprintf(`
+resource "cloudflare_access_keys_configuration" "%[1]s" {
+  account_id = "%[2]s"
+  key_rotation_interval_days = "%[3]d"
+}`, rnd, accountID, days)
+}
+
+func TestAccCloudflareAccessKeysConfiguration_WithoutKeyRotationIntervalDaysSet(t *testing.T) {
+	rnd := generateRandomResourceName()
+	name := fmt.Sprintf("cloudflare_access_keys_configuration.%s", rnd)
+	accountID := os.Getenv("CLOUDFLARE_ACCOUNT_ID")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccessAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccessKeysConfigurationWithoutKeyRotationIntervalDays(rnd, accountID),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(name, "key_rotation_interval_days"),
+				),
+			},
+		},
+	})
+}
+
+func testAccessKeysConfigurationWithoutKeyRotationIntervalDays(rnd, accountID string) string {
+	return fmt.Sprintf(`
+resource "cloudflare_access_keys_configuration" "%[1]s" {
+  account_id = "%[2]s"
+}`, rnd, accountID)
+}
