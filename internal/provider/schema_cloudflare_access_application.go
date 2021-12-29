fcc08a95071e6164fabeb0c8912dbb069a2630c9
Olivier Brisse <olivier.brisse@rangeme.com>
2021-12-29 18:05:02 +1100

    Add sass app

--- internal/provider/schema_cloudflare_access_application.go
+++ internal/provider/schema_cloudflare_access_application.go
@@ -38,15 +38,16 @@ func resourceCloudflareAccessApplicationSchema() map[string]*schema.Schema {
 		},
 		"domain": {
 			Type:        schema.TypeString,
-			Required:    true,
+			Optional:    true,
+			Computed:    true,
 			Description: "The complete URL of the asset you wish to put Cloudflare Access in front of. Can include subdomains or paths. Or both.",
 		},
 		"type": {
 			Type:         schema.TypeString,
 			Optional:     true,
 			Default:      "self_hosted",
-			ValidateFunc: validation.StringInSlice([]string{"self_hosted", "ssh", "vnc", "file"}, false),
-			Description:  fmt.Sprintf("The application type. %s", renderAvailableDocumentationValuesStringSlice([]string{"self_hosted", "ssh", "vnc", "file"})),
+			ValidateFunc: validation.StringInSlice([]string{"self_hosted", "saas", "ssh", "vnc", "file"}, false),
+			Description:  fmt.Sprintf("The application type. %s", renderAvailableDocumentationValuesStringSlice([]string{"self_hosted", "saas", "ssh", "vnc", "file"})),
 		},
 		"session_duration": {
 			Type:     schema.TypeString,
@@ -117,6 +118,33 @@ func resourceCloudflareAccessApplicationSchema() map[string]*schema.Schema {
 						Optional:     true,
 						ValidateFunc: validation.IntBetween(-1, 86400),
 						Description:  "The maximum time a preflight request will be cached.",
+					},
+				},
+			},
+		},
+		"saas_app": {
+			Type:        schema.TypeList,
+			Optional:    true,
+			MaxItems:    1,
+			Description: "SaaS configuration for the Access Application.",
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					"sp_entity_id": {
+						Type:        schema.TypeString,
+						Required:    true,
+						Description: "A globally unique name for an identity or service provider.",
+					},
+					"consumer_service_url": {
+						Type:        schema.TypeString,
+						Required:    true,
+						Description: "The service provider's endpoint that is responsible for receiving and parsing a SAML assertion.",
+					},
+					"name_id_format": {
+						Type:         schema.TypeString,
+						Optional:     true,
+						Default:      "email",
+						ValidateFunc: validation.StringInSlice([]string{"email", "id"}, false),
+						Description:  "The format of the name identifier sent to the SaaS application.",
 					},
 				},
 			},
@@ -258,6 +286,31 @@ func convertCORSStructToSchema(d *schema.ResourceData, headers *cloudflare.Acces
 	m["allowed_methods"] = flattenStringList(headers.AllowedMethods)
 	m["allowed_headers"] = flattenStringList(headers.AllowedHeaders)
 	m["allowed_origins"] = flattenStringList(headers.AllowedOrigins)
+
+	return []interface{}{m}
+}
+
+func convertSaasSchemaToStruct(d *schema.ResourceData) *cloudflare.SaasApplication {
+	SaasConfig := cloudflare.SaasApplication{}
+	if _, ok := d.GetOk("saas_app"); ok {
+		SaasConfig.SPEntityID = d.Get("saas_app.0.sp_entity_id").(string)
+		SaasConfig.ConsumerServiceUrl = d.Get("saas_app.0.consumer_service_url").(string)
+		SaasConfig.NameIDFormat = d.Get("saas_app.0.name_id_format").(string)
+	}
+
+	return &SaasConfig
+}
+
+func convertSaasStructToSchema(d *schema.ResourceData, app *cloudflare.SaasApplication) []interface{} {
+	if _, ok := d.GetOk("saas_app"); !ok {
+		return []interface{}{}
+	}
+
+	m := map[string]interface{}{
+		"sp_entity_id":         app.SPEntityID,
+		"consumer_service_url": app.ConsumerServiceUrl,
+		"name_id_format":       app.NameIDFormat,
+	}
 
 	return []interface{}{m}
 }
