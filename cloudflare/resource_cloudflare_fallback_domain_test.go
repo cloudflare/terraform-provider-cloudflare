a535bac42f11be3f13a809f484f371eefa0e99d1
Jacob Bednarz <jacob.bednarz@gmail.com>
2022-01-13 13:06:05 +1100

    fix tests

--- cloudflare/resource_cloudflare_fallback_domain_test.go
+++ cloudflare/resource_cloudflare_fallback_domain_test.go
@@ -29,35 +29,35 @@ func TestAccCloudflareFallbackDomain(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCloudflareFallbackDomain(rnd, accountID, "example domain", "example.com", []string{"2.2.2.2"}),
+				Config: testAccCloudflareFallbackDomain(rnd, accountID, "example domain", "example.com", "2.2.2.2"),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(name, "account_id", accountID),
 					resource.TestCheckResourceAttr(name, "domains.0.description", "example domain"),
 					resource.TestCheckResourceAttr(name, "domains.0.suffix", "example.com"),
-					resource.TestCheckResourceAttr(name, "domains.0.dns_server", "['2.2.2.2']"),
+					resource.TestCheckResourceAttr(name, "domains.0.dns_server.0", "2.2.2.2"),
 				),
 			},
 			{
-				Config: testAccCloudflareFallbackDomain(rnd, accountID, "second example domain", "example_two.com", []string{"1.1.1.1"}),
+				Config: testAccCloudflareFallbackDomain(rnd, accountID, "second example domain", "example_two.com", "1.1.1.1"),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(name, "account_id", accountID),
 					resource.TestCheckResourceAttr(name, "domains.0.description", "second example domain"),
-					resource.TestCheckResourceAttr(name, "domains.0.host", "example_two.com"),
-					resource.TestCheckResourceAttr(name, "domains.0.dns_server", "['1.1.1.1']"),
+					resource.TestCheckResourceAttr(name, "domains.0.suffix", "example_two.com"),
+					resource.TestCheckResourceAttr(name, "domains.0.dns_server.0", "1.1.1.1"),
 				),
 			},
 		},
 	})
 }
 
-func testAccCloudflareFallbackDomain(rnd, accountID string, description string, suffix string, dns_server []string) string {
+func testAccCloudflareFallbackDomain(rnd, accountID string, description string, suffix string, dns_server string) string {
 	return fmt.Sprintf(`
 resource "cloudflare_fallback_domain" "%[1]s" {
   account_id = "%[2]s"
   domains {
     description = "%[3]s"
-    host = "%[4]s"
-		dns_server = "%[5]s"
+    suffix = "%[4]s"
+		dns_server = ["%[5]s"]
   }
 }
 `, rnd, accountID, description, suffix, dns_server)
