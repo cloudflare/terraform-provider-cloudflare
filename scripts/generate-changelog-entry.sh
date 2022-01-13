ed24b7d9d5987e0485acf0a1f7c170f036afa1d0
Jacob Bednarz <jacob.bednarz@gmail.com>
2022-01-13 11:06:16 +1100

    add script to guide changelog entry creation
    To help generate CHANGELOG entries, this script calls the GitHub API to
    get the next available auto incrementing ID and prompt the user for the
    entry contents.

---
+++ scripts/generate-changelog-entry.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#
+# Generate a changelog entry for a proposed PR by grabbing the next available
+# auto incrementing ID in GitHub.
+
+if ! command -v curl &> /dev/null
+then
+  echo "jq not be found"
+  exit 1
+fi
+
+if ! command -v jq &> /dev/null
+then
+  echo "jq not be found"
+  exit 1
+fi
+
+current_pr=$(curl -s "https://api.github.com/repos/cloudflare/terraform-provider-cloudflare/issues?state=all&per_page=1" | jq -r ".[].number")
+next_pr=$(($current_pr + 1))
+
+echo "==> What type of change is this? (enhancement, bug, breaking-change, new-resource, new-data-source, new-guide)"
+read entry_type
+
+echo "==> What is the summary of this change? Example: resource/example: made a change to CRUD operations"
+read entry_summary
+
+touch $(pwd)/.changelog/$next_pr.txt
+cat > $(pwd)/.changelog/$next_pr.txt <<EOF
+\`\`\`release-note:$entry_type
+$entry_summary
+\`\`\`
+EOF
+
+echo
+echo "Successfully created .changelog/$next_pr.txt. Don't forget to commit it and open the PR!"
