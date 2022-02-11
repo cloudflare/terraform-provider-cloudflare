a6073785785ab858fbe8a9a1afc59ced2c834630
Marc-Antoine Leblond <11162235+maleblond@users.noreply.github.com>
2022-02-11 15:59:29 -0500

    Make cache variants schema easier to read / more static

--- cloudflare/schema_cloudflare_zone_cache_variants.go
+++ cloudflare/schema_cloudflare_zone_cache_variants.go
@@ -4,39 +4,34 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-var supportedCacheVariantsExtensions = []string{
-	"avif",
-	"bmp",
-	"gif",
-	"jpeg",
-	"jpg",
-	"jpg2",
-	"jp2",
-	"png",
-	"tiff",
-	"tif",
-	"webp",
+func resourceCloudflareZoneCacheVariantsExtensionSchema() *schema.Schema {
+	return &schema.Schema{
+		MinItems: 1,
+		Optional: true,
+		Type:     schema.TypeSet,
+		Elem: &schema.Schema{
+			Type: schema.TypeString,
+		},
+	}
 }
 
 func resourceCloudflareZoneCacheVariantsSchema() map[string]*schema.Schema {
-	variantsSchema := map[string]*schema.Schema{
+	return map[string]*schema.Schema{
 		"zone_id": {
 			Type:     schema.TypeString,
 			Required: true,
 			ForceNew: true,
 		},
+		"avif": resourceCloudflareZoneCacheVariantsExtensionSchema(),
+		"bmp":  resourceCloudflareZoneCacheVariantsExtensionSchema(),
+		"gif":  resourceCloudflareZoneCacheVariantsExtensionSchema(),
+		"jpeg": resourceCloudflareZoneCacheVariantsExtensionSchema(),
+		"jpg":  resourceCloudflareZoneCacheVariantsExtensionSchema(),
+		"jpg2": resourceCloudflareZoneCacheVariantsExtensionSchema(),
+		"jp2":  resourceCloudflareZoneCacheVariantsExtensionSchema(),
+		"png":  resourceCloudflareZoneCacheVariantsExtensionSchema(),
+		"tiff": resourceCloudflareZoneCacheVariantsExtensionSchema(),
+		"tif":  resourceCloudflareZoneCacheVariantsExtensionSchema(),
+		"webp": resourceCloudflareZoneCacheVariantsExtensionSchema(),
 	}
-
-	for _, ext := range supportedCacheVariantsExtensions {
-		variantsSchema[ext] = &schema.Schema{
-			MinItems: 1,
-			Optional: true,
-			Type:     schema.TypeSet,
-			Elem: &schema.Schema{
-				Type: schema.TypeString,
-			},
-		}
-	}
-
-	return variantsSchema
 }
