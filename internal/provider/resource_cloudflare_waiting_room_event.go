92e044f2de42ffa3157897bc07d50b4e238f2625
Brad Swenson <broswen@gmail.com>
2022-05-26 21:14:36 -0500

    check if PrequeueStartTime is nil

--- internal/provider/resource_cloudflare_waiting_room_event.go
+++ internal/provider/resource_cloudflare_waiting_room_event.go
@@ -110,7 +110,7 @@ func resourceCloudflareWaitingRoomEventRead(ctx context.Context, d *schema.Resou
 	d.Set("event_end_time", waitingRoomEvent.EventEndTime.Format(time.RFC3339))
 	d.Set("session_duration", waitingRoomEvent.SessionDuration)
 
-	if !waitingRoomEvent.PrequeueStartTime.IsZero() {
+	if waitingRoomEvent.PrequeueStartTime != nil {
 		d.Set("prequeue_start_time", waitingRoomEvent.PrequeueStartTime.Format(time.RFC3339))
 	}
 
