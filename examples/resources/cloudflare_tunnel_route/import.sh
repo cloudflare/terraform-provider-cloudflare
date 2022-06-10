09eeff2d6fd01da058df565eb7bac400fa7ad8e3
Jacob Bednarz <jacob.bednarz@gmail.com>
2022-06-10 19:48:51 +1000

    [skip ci] make import strings consistent

--- examples/resources/cloudflare_tunnel_route/import.sh
+++ examples/resources/cloudflare_tunnel_route/import.sh
@@ -1,2 +1,2 @@
 # Use account ID, network CIDR and virtual network ID.
-$ terraform import cloudflare_tunnel_route c4a7362d577a6c3019a474fd6f485821/192.0.2.24/32/bdc39a3c-3104-4c23-8ac0-9f455dda691a
+$ terraform import cloudflare_tunnel_route <account_id/<network_cidr>/<virtual_network_id>
