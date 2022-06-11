60fcc423176841ba3d1d61fd3936a73be4844dda
Jacob Bednarz <jacob.bednarz@gmail.com>
2022-06-11 11:49:36 +1000

    docs: cloudflare_access_mutual_tls_certificate to autogen

---
+++ examples/resources/cloudflare_access_mutual_tls_certificate/import.sh
@@ -0,0 +1,5 @@
+# Account level import.
+$ terraform import cloudflare_access_mutual_tls_certificate.example account/<account_id>/<mutual_tls_certificate_id>
+
+# Zone level import.
+$ terraform import cloudflare_access_mutual_tls_certificate.example zone/<zone_id>/<mutual_tls_certificate_id>
