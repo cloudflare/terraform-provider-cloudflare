0177a4c5792201e85d2819fd3a8f32b4ee7ddf45
Brad <broswen@users.noreply.github.com>
2022-06-11 20:26:26 -0500

    Update import.sh

--- examples/resources/cloudflare_healthcheck/import.sh
+++ examples/resources/cloudflare_healthcheck/import.sh
@@ -1,5 +1,2 @@
 # Use the Zone ID and Healthcheck ID to import.
 $ terraform import cloudflare_healthcheck.example <zone_id>/<healthcheck_id>
-
-# 9a7806061c88ada191ed06f989cc3dac - the zone ID
-# 699d98642c564d2e855e9661899b7252 - healthcheck ID as returned by [API](https://api.cloudflare.com/#health-checks-list-health-checks)
