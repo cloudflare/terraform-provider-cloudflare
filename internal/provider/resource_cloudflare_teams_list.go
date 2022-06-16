a98482d3ca4c24ba1fbc26c4c9cd33de4dded521
Akemi Davisson <akemi@cloudflare.com>
2022-06-16 08:58:26 -0500

    Adds recently supported per_page/pagination teams_list

--- internal/provider/resource_cloudflare_teams_list.go
+++ internal/provider/resource_cloudflare_teams_list.go
@@ -73,6 +73,7 @@ func resourceCloudflareTeamsListRead(ctx context.Context, d *schema.ResourceData
 	listItems, _, err := client.TeamsListItems(ctx, cloudflare.TeamsListItemsParams{
 		AccountID: accountID,
 		ListID:    d.Id(),
+		PaginationOptions: cloudflare.PaginationOptions{ PerPage: 1000 },
 	})
 	if err != nil {
 		return diag.FromErr(fmt.Errorf("error finding Teams List %q: %w", d.Id(), err))
