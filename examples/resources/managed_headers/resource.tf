9256bc8e541e5e33f0489680aad57b368370e7dd
Jacob Bednarz <jacob.bednarz@gmail.com>
2022-06-20 11:30:52 +1000

    anothhhherr `make docs` run

--- examples/resources/managed_headers/resource.tf
+++ examples/resources/managed_headers/resource.tf
@@ -1,23 +1,14 @@
 # Enable security headers using Managed Meaders
 resource "cloudflare_managed_headers" "add_security_headers_example" {
-    zone_id     = "cb029e245cfdd66dc8d2e570d5dd3322"
-    managed_request_headers {
-        id = "add_true_client_ip_headers"
-        enabled = true
-    }
+  zone_id = "cb029e245cfdd66dc8d2e570d5dd3322"
 
-    managed_request_headers {
-        id = "add_visitor_location_headers"
-        enabled = false
-    }
+  managed_request_headers {
+    id      = "add_true_client_ip_headers"
+    enabled = true
+  }
 
-    managed_response_headers {
-        id = "add_security_headers"
-        enabled = false
-    }
-
-    managed_response_headers {
-        id = "remove_x-powered-by_header"
-        enabled = true
-    }
+  managed_response_headers {
+    id      = "remove_x-powered-by_header"
+    enabled = true
+  }
 }
