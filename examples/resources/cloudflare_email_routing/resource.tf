473b4d6dfd04056168d26144608d2a30339cb248
Jacob White <jake@jwhite.network>
2022-08-24 21:30:20 -0400

    Fix semgrep

--- examples/resources/cloudflare_email_routing/resource.tf
+++ examples/resources/cloudflare_email_routing/resource.tf
@@ -1,18 +1,18 @@
 
 # Managed destination address
 resource "cloudflare_email_routing_address" "example" {
-    account_id = "01a7362d577a6c3019a474fd6f485823"
+    account_id = "f037e56e89293a057740de681ac9abbe"
     email = "user@example.com"
 }
 
 # Manage Email Routing on zone
 resource "cloudflare_email_routing_settings" "my_zone" {
-    zone_id = "023e105f4ecef8ad9ca31a8372d0c353"
+    zone_id = "0da42c8d2132a9ddaf714f9e7c920711"
     enabled = "true"
 }
 
 resource "cloudflare_email_routing_rule" "main" {
-    zone_id = "023e105f4ecef8ad9ca31a8372d0c353"
+    zone_id = "0da42c8d2132a9ddaf714f9e7c920711"
     name = "terraform rule"
     enabled =  true
     matchers = [
