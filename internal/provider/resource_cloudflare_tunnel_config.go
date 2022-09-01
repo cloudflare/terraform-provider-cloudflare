1ef5290a74512eaefb0576a2b73c685346d37b05
Jacob White <jake@jwhite.network>
2022-08-31 21:45:04 -0400

    Push latest

--- internal/provider/resource_cloudflare_tunnel_config.go
+++ internal/provider/resource_cloudflare_tunnel_config.go
@@ -2,6 +2,7 @@ package provider
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/cloudflare/cloudflare-go"
@@ -13,25 +14,40 @@ func resourceCloudflareTunnelConfig() *schema.Resource {
 	return &schema.Resource{
 		Schema:        resourceCloudflareTunnelConfigSchema(),
 		ReadContext:   resourceCloudflareTunnelConfigRead,
+		CreateContext: resourceCloudflareTunnelConfigUpdate,
 		UpdateContext: resourceCloudflareTunnelConfigUpdate,
+		DeleteContext: resourceCloudflareTunnelConfigDelete,
 	}
 }
 
 func buildTunnelConfig(d *schema.ResourceData) cloudflare.TunnelConfiguration {
-	connectTimeOut, _ := time.ParseDuration(d.Get("config.0.origin_request.0.connect_timeout").(string))
-	tlsKeepAlive, _ := time.ParseDuration(d.Get("config.0.origin_request.0.tls_timeout").(string))
-	tcpKeepAlive, _ := time.ParseDuration(d.Get("config.0.origin_request.0.tcp_keep_alive").(string))
-	noHappyEyeballs := d.Get("config.0.origin_request.0.no_happy_eyeballs").(bool)
-	keepAliveConnections := d.Get("config.0.origin_request.0.keep_alive_connections").(int)
-	keepAliveTimeout, _ := time.ParseDuration(d.Get("config.0.origin_request.0.keep_alive_timeout").(string))
-	httpHostHeader := d.Get("config.0.origin_request.0.http_host_header").(string)
-	originServerName := d.Get("config.0.origin_request.0.ca_pool").(string)
-	noTLSVerify := d.Get("config.0.origin_request.0.no_tls_verify").(bool)
-	disableChuckedEncoding := d.Get("config.0.origin_request.0.disabled_chunked_encoding").(bool)
-	bastionMode := d.Get("config.0.origin_request.0.bastion_mode").(bool)
-	proxyAddress := d.Get("config.0.origin_request.0.proxy_address").(string)
-	proxyPort := uint(d.Get("config.0.origin_request.0.proxy_port").(int))
-	proxyType := d.Get("config.0.origin_request.0.proxy_type").(string)
+	fmt.Println("Starting Tunnel config")
+	// config := d.Get("config").(*schema.Set).List()[0].(map[string]interface{})
+	// fmt.Printf("%#v\n", config["ingress_rule"].(map[string]interface{}))
+	warpRouting := cloudflare.WarpRoutingConfig{}
+	if _, ok := d.GetOk("config.0.warp_routing"); ok {
+		if _, ok := d.GetOk("config.0.warp_routing.0.enabled"); ok {
+			warpRouting.Enabled = d.Get("config.0.warp_routing.enabled").(bool)
+		}
+	}
+	fmt.Println("Building origin")
+	originRequest := "config.0.origin_request.0"
+
+	connectTimeOut, _ := time.ParseDuration(d.Get(originRequest+".connect_timeout").(string))
+	tlsKeepAlive, _ := time.ParseDuration(d.Get(originRequest+".tls_timeout").(string))
+	tcpKeepAlive, _ := time.ParseDuration(d.Get(originRequest+".tcp_keep_alive").(string))
+	noHappyEyeballs := d.Get(originRequest+".no_happy_eyeballs").(bool)
+	keepAliveConnections := d.Get(originRequest+".keep_alive_connections").(int)
+	keepAliveTimeout, _ := time.ParseDuration(d.Get(originRequest+".keep_alive_timeout").(string))
+	httpHostHeader := d.Get(originRequest+".http_host_header").(string)
+	originServerName := d.Get(originRequest+".ca_pool").(string)
+	noTLSVerify := d.Get(originRequest+".no_tls_verify").(bool)
+	disableChuckedEncoding := d.Get(originRequest+".disable_chunked_encoding").(bool)
+	bastionMode := d.Get(originRequest+".bastion_mode").(bool)
+	proxyAddress := d.Get(originRequest+".proxy_address").(string)
+	proxyPort := uint(d.Get(originRequest+".proxy_port").(int))
+	proxyType := d.Get(originRequest+".proxy_type").(string)
+	// fmt.Printf("%s\n", connectTimeOut)
 	origin := cloudflare.OriginRequestConfig{
 		ConnectTimeout:         &connectTimeOut,
 		TLSTimeout:             &tlsKeepAlive,
@@ -48,23 +64,43 @@ func buildTunnelConfig(d *schema.ResourceData) cloudflare.TunnelConfiguration {
 		ProxyPort:              &proxyPort,
 		ProxyType:              &proxyType,
 	}
-	ingressIpRules := []cloudflare.UnvalidatedIngressRule{}
-	if _, ok := d.GetOk("origin_request.0.ip_rules"); ok {
-		if _, ok := d.GetOk("origin_request.0.ip_rules.0.prefix"); ok {
+
+	
+	var ipRules []cloudflare.IngressIPRule
+	if items, ok := d.GetOk(originRequest+".ip_rules"); ok {
+		fmt.Println("Building IP Rules")
+		for _, item := range items.(*schema.Set).List() {
+			rule := item.(map[string]interface{})
+			newRule := cloudflare.IngressIPRule{}
+			newRule.Prefix = cloudflare.StringPtr(rule["ip"].(string))
+			newRule.Allow = rule["allow"].(bool)
+			for _, value := range rule["ports"].([]interface{}) {
+				newRule.Ports = append(newRule.Ports, value.(int))
+			}
+			ipRules = append(ipRules, newRule)
 		}
 	}
+	origin.IPRules = ipRules
 
-	warpRouting := cloudflare.WarpRoutingConfig{}
-	if _, ok := d.GetOk("config.0.warp_routing"); ok {
-		if _, ok := d.GetOk("config.0.warp_routing.enabled"); ok {
-			warpRouting.Enabled = d.Get("config.0.warp_routing.enabled").(bool)
+	
+	var ingressRules []cloudflare.UnvalidatedIngressRule
+	if items, ok := d.GetOk("config.0.ingress_rule"); ok {
+		fmt.Println("Building Ingress Rules")
+		for _, item := range items.(*schema.Set).List() {
+			data := item.(map[string]interface{})
+			fmt.Printf("%#v\n", data)
+			ingressRules = append(ingressRules, cloudflare.UnvalidatedIngressRule{
+				Hostname: data["hostname"].(string),
+				Path:     data["path"].(string),
+				Service:  data["service"].(string),
+			})
 		}
 	}
 
 	return cloudflare.TunnelConfiguration{
 		OriginRequest: origin,
 		WarpRouting:   &warpRouting,
-		Ingress:       ingressIpRules,
+		Ingress:       ingressRules,
 	}
 }
 
@@ -74,9 +110,9 @@ func resourceCloudflareTunnelConfigRead(ctx context.Context, d *schema.ResourceD
 	tunnelID := d.Get("tunnel_id").(string)
 	config, err := client.GetTunnelConfiguration(ctx, cloudflare.AccountIdentifier(accountID), tunnelID)
 	if err != nil {
-		return diag.Errorf("error find tunnel config %q: %w", d.Id(), err)
+		return diag.FromErr(fmt.Errorf("error getting tunnel config %q: %w", d.Id(), err))
 	}
-	d.Set("tunnel_id", config.TunnelID)
+	d.SetId(config.TunnelID)
 	return nil
 }
 
@@ -84,14 +120,28 @@ func resourceCloudflareTunnelConfigUpdate(ctx context.Context, d *schema.Resourc
 	client := meta.(*cloudflare.API)
 	accountID := d.Get("account_id").(string)
 	tunnelID := d.Get("tunnel_id").(string)
-	tunnelConfig := buildTunnelConfig(d)
 	tunnel := cloudflare.TunnelConfigurationParams{
 		TunnelID: tunnelID,
-		Config:   tunnelConfig,
+		Config:   buildTunnelConfig(d),
+	}
+	// fmt.Printf("%#v\n", tunnel)
+	_, err := client.UpdateTunnelConfiguration(ctx, cloudflare.AccountIdentifier(accountID), tunnel)
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error creating / updating tunnel config %q: %w", tunnelID, err))
+	}
+	return resourceCloudflareTunnelConfigRead(ctx, d, meta)
+}
+
+func resourceCloudflareTunnelConfigDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*cloudflare.API)
+	accountID := d.Get("account_id").(string)
+	tunnel := cloudflare.TunnelConfigurationParams{
+		TunnelID: d.Id(),
+		Config:   cloudflare.TunnelConfiguration{},
 	}
 	_, err := client.UpdateTunnelConfiguration(ctx, cloudflare.AccountIdentifier(accountID), tunnel)
 	if err != nil {
-		return diag.Errorf("error updating tunnel config %q: %w", d.Id(), err)
+		return diag.FromErr(fmt.Errorf("error deleting tunnel config %q: %w", d.Id(), err))
 	}
 	return resourceCloudflareTunnelConfigRead(ctx, d, meta)
 }
---
+++ internal/provider/resource_cloudflare_tunnel_config_test.go
@@ -0,0 +1,117 @@
+package provider
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func testTunnelConfig(resourceID, accountID, tunnelSecret string) string {
+	return fmt.Sprintf(`
+		resource "cloudflare_argo_tunnel" "%[1]s" {
+		  account_id = "%[2]s"
+		  name       = "%[1]s"
+		  secret     = "%[3]s"
+		}
+		
+		resource "cloudflare_tunnel_config" "%[1]s" {
+		  account_id         = "%[2]s"
+		  tunnel_id          = cloudflare_argo_tunnel.%[1]s.id
+		
+		  config {
+			warp_routing {
+			  enabled = true
+			}
+			origin_request {
+			  connect_timeout = "60s"
+			  tls_timeout = "60s"
+			  tcp_keep_alive = "60s"
+			  no_happy_eyeballs = false
+			  keep_alive_connections = 1024
+			  keep_alive_timeout = "60s"
+			  http_host_header = "baz"
+			  origin_server_name = "foobar"
+			  ca_pool = "/path/to/unsigned/ca/pool"
+			  no_tls_verify = false
+			  disable_chunked_encoding = false
+			  bastion_mode = false
+			  proxy_address = "127.0.0.1"
+			  proxy_port = "8123"
+			  proxy_type = "socks"
+			  ip_rules {
+				prefix = "/web"
+				ports = [80, 443]
+				allow = false
+			  }
+			}
+			ingress_rule {
+			  hostname = "foo"
+			  path = "/bar"
+			  service = "127.0.0.1:8080"
+			}
+			ingress_rule {
+			  hostname = "bar"
+			  path = ""
+			  service = "127.0.0.1:8081"
+			}
+		  }
+		}
+		`, resourceID, accountID, tunnelSecret)
+}
+
+func TestAccTestTunnelConfig(t *testing.T) {
+	rnd := generateRandomResourceName()
+	name := "cloudflare_tunnel_config." + rnd
+	zoneID := os.Getenv("CLOUDFLARE_ACCOUNT_ID")
+	tunnelSecret := acctest.RandStringFromCharSet(32, acctest.CharSetAlpha)
+
+	//resourceCloudflareTunnelConfig
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheckEmail(t)
+			testAccPreCheckApiKey(t)
+			testAccPreCheckAccount(t)
+		},
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testTunnelConfig(rnd, zoneID, tunnelSecret),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(name, "config.0.warp_routing.0.enabled", "true"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.connect_timeout", "60s"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.tls_timeout", "60s"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.tcp_keep_alive", "60s"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.no_happy_eyeballs", "false"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.keep_alive_connections", "1024"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.keep_alive_timeout", "60s"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.http_host_header", "baz"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.origin_server_name", "foobar"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.ca_pool", "/path/to/unsigned/ca/pool"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.no_tls_verify", "false"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.disable_chunked_encoding", "false"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.bastion_mode", "false"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.proxy_address", ""),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.proxy_port", ""),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.proxy_type", ""),
+
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.ip_rules.0.prefix", "/web"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.ip_rules.0.ports.#", "2"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.ip_rules.0.ports.0", "80"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.ip_rules.0.ports.1", "443"),
+					resource.TestCheckResourceAttr(name, "config.0.origin_request.0.ip_rules.0.allow", "false"),
+
+					resource.TestCheckResourceAttr(name, "config.0.ingress_rule.#", "2"),
+					resource.TestCheckResourceAttr(name, "config.0.ingress_rule.0.hostname", "foo"),
+					resource.TestCheckResourceAttr(name, "config.0.ingress_rule.0.path", "/bar"),
+					resource.TestCheckResourceAttr(name, "config.0.ingress_rule.0.service", "127.0.0.1:8080"),
+					resource.TestCheckResourceAttr(name, "config.0.ingress_rule.1.hostname", "bar"),
+					resource.TestCheckResourceAttr(name, "config.0.ingress_rule.1.path", ""),
+					resource.TestCheckResourceAttr(name, "config.0.ingress_rule.1.service", "127.0.0.1:8081"),
+				),
+			},
+		},
+	})
+}
