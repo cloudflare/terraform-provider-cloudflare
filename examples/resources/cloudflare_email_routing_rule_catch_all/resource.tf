bded2dbfc4b50037225b1acaa57b0622a0efa7ba
marco-lancini <info@marcolancini.it>
2022-09-07 15:33:00 +0100

    catch all

--- examples/resources/cloudflare_email_routing_rule_catch_all/resource.tf
+++ examples/resources/cloudflare_email_routing_rule_catch_all/resource.tf
@@ -2,16 +2,13 @@ resource "cloudflare_email_routing_rule_catch_all" "main" {
   zone_id = "0da42c8d2132a9ddaf714f9e7c920711"
   name    = "terraform rule"
   enabled = true
-  matchers = [
-    {
-      type = "all",
-    }
-  ]
 
-  actions = [
-    {
-      type  = "forward"
-      value = ["destinationaddress@example.net"]
-    }
-  ]
+  matcher {
+    type = "all"
+  }
+
+  action {
+    type  = "forward"
+    value = ["destinationaddress@example.net"]
+  }
 }
