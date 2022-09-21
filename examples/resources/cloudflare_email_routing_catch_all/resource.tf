4ebc3652cf35a4c9cf62e13a7b6e545f60551869
Jacob Bednarz <jacob.bednarz@gmail.com>
2022-09-21 10:56:33 +1000

    fix `email_routing_catch_all docs`

--- examples/resources/cloudflare_email_routing_catch_all/resource.tf
+++ examples/resources/cloudflare_email_routing_catch_all/resource.tf
@@ -1,6 +1,6 @@
-resource "cloudflare_email_routing_rule_catch_all" "main" {
+resource "cloudflare_email_routing_catch_all" "example" {
   zone_id = "0da42c8d2132a9ddaf714f9e7c920711"
-  name    = "terraform rule"
+  name    = "example catch all"
   enabled = true
 
   matcher {
