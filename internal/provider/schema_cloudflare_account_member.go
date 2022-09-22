1e54fe6be276a6e0e7065098e29a1266774c7684
Jacob Bednarz <jacob.bednarz@hey.com>
2022-09-22 19:52:39 +1000

    update schema documentation for available values

--- internal/provider/schema_cloudflare_account_member.go
+++ internal/provider/schema_cloudflare_account_member.go
@@ -25,7 +25,7 @@ func resourceCloudflareAccountMemberSchema() map[string]*schema.Schema {
 		"status": {
 			Type:        schema.TypeString,
 			Optional:    true,
-			Description: "A member's status in the account. default value in API: pending; valid values: accepted, pending",
+			Description: fmt.Sprintf("A member's status in the account. %s", renderAvailableDocumentationValuesStringSlice([]string{"accepted", "pending")),
 		},
 	}
 }
