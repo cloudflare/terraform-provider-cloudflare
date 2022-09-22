910399c3a6eb2255bf507aeafff2bf96598fb324
Jacob Bednarz <jacob.bednarz@hey.com>
2022-09-22 19:59:17 +1000

    go fmt

--- internal/provider/schema_cloudflare_account_member.go
+++ internal/provider/schema_cloudflare_account_member.go
@@ -2,7 +2,7 @@ package provider
 
 import (
 	"fmt"
-	
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
@@ -13,13 +13,11 @@ func resourceCloudflareAccountMemberSchema() map[string]*schema.Schema {
 			Optional:    true,
 			Description: "Account ID to create the account member in.",
 		},
-
 		"email_address": {
 			Type:        schema.TypeString,
 			Required:    true,
 			Description: "The email address of the user who you wish to manage. Following creation, this field becomes read only via the API and cannot be updated.",
 		},
-
 		"role_ids": {
 			Type:        schema.TypeSet,
 			Required:    true,
