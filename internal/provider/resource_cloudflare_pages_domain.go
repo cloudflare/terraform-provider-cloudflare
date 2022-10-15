55de1ca5e5d0bb1de3d29d14872750bcff6dabea
Daniel Walsh <walshydev@gmail.com>
2022-10-15 11:58:34 +0100

    Add Pages project domain importer. Closes #1958

--- internal/provider/resource_cloudflare_pages_domain.go
+++ internal/provider/resource_cloudflare_pages_domain.go
@@ -3,9 +3,11 @@ package provider
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/MakeNowJust/heredoc/v2"
 	"github.com/cloudflare/cloudflare-go"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -77,4 +79,40 @@ func resourceCloudflarePagesDomainDelete(ctx context.Context, d *schema.Resource
 		return diag.FromErr(fmt.Errorf("error creating domain for project %q: %w", accountID, err))
 	}
 	return nil
+}
+
+func resourceCloudflarePagesDomainImport(ctx context.Context, d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	client := meta.(*cloudflare.API)
+
+	// split the id so we can look up
+	idAttr := strings.SplitN(d.Id(), "/", 2)
+	var accountID string
+	var projectName string
+	if len(idAttr) == 2 {
+		accountID = idAttr[0]
+		projectName = idAttr[1]
+	} else {
+		return nil, fmt.Errorf("invalid id %q specified, should be in format \"accountID/project-name\" for import", d.Id())
+	}
+
+	domains, err := client.GetPagesDomains(ctx, cloudflare.PagesDomainsParameters{AccountID: accountID, ProjectName: projectName})
+	if err != nil {
+		return nil, fmt.Errorf("unable to find domains for project %q: %w", d.Id(), err)
+	}
+
+	tflog.Info(ctx, fmt.Sprintf("Found domains: %+v", domains))
+
+	resourceData := []*schema.ResourceData{}
+
+	for _, domain := range domains {
+		d.SetId(domain.ID)
+		d.Set("account_id", accountID)
+		d.Set("project_name", projectName)
+		d.Set("domain", domain.Name)
+		resourceCloudflarePagesDomainRead(ctx, d, meta)
+
+		resourceData = append(resourceData, d)
+	}
+
+	return resourceData, nil
 }
