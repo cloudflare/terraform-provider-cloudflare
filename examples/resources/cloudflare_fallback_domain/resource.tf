8e84f880abe9fd7fca8083bdf556c1dabfbd7587
Jacob Bednarz <jacob.bednarz@gmail.com>
2022-11-18 13:55:50 +1100

    fix documentation account IDs

--- examples/resources/cloudflare_fallback_domain/resource.tf
+++ examples/resources/cloudflare_fallback_domain/resource.tf
@@ -1,6 +1,6 @@
 # Use DNS servers 192.0.2.0 or 192.0.2.1 for example.com
 resource "cloudflare_fallback_domain" "example" {
-  account_id = "1d5fdc9e88c8a8c4518b068cd94331fe"
+  account_id = "f037e56e89293a057740de681ac9abbe"
   domains {
     suffix      = "example.com"
     description = "Example domain"
@@ -10,7 +10,7 @@ resource "cloudflare_fallback_domain" "example" {
 
 # Explicitly adding example.com to the default entries.
 resource "cloudflare_fallback_domain" "example" {
-  account_id = "1d5fdc9e88c8a8c4518b068cd94331fe"
+  account_id = "f037e56e89293a057740de681ac9abbe"
   dynamic "domains" {
     for_each = toset(["intranet", "internal", "private", "localdomain", "domain", "lan", "home", "host", "corp", "local", "localhost", "home.arpa", "invalid", "test"])
     content {
@@ -27,7 +27,7 @@ resource "cloudflare_fallback_domain" "example" {
 
 # Create a device policy
 resource "cloudflare_device_policy" "developer_warp_policy" {
-  account_id    = "1d5fdc9e88c8a8c4518b068cd94331fe"
+  account_id    = "f037e56e89293a057740de681ac9abbe"
   name          = "Developers"
   precedence    = 10
   match         = "any(identity.groups.name[*] in {\"Developers\"})"
@@ -36,7 +36,7 @@ resource "cloudflare_device_policy" "developer_warp_policy" {
 
 # Use DNS servers 192.0.2.0 or 192.0.2.1 for example.com for a particular device policy
 resource "cloudflare_fallback_domain" "example" {
-  account_id = "1d5fdc9e88c8a8c4518b068cd94331fe"
+  account_id = "f037e56e89293a057740de681ac9abbe"
   policy_id  = cloudflare_device_policy.developer_warp_policy.id
   domains {
     suffix      = "example.com"
--- examples/resources/cloudflare_split_tunnel/resource.tf
+++ examples/resources/cloudflare_split_tunnel/resource.tf
@@ -1,6 +1,6 @@
 # Excluding *.example.com from WARP routes
 resource "cloudflare_split_tunnel" "example_split_tunnel_exclude" {
-  account_id = "1d5fdc9e88c8a8c4518b068cd94331fe"
+  account_id = "f037e56e89293a057740de681ac9abbe"
   mode       = "exclude"
   tunnels {
     host        = "*.example.com"
@@ -10,7 +10,7 @@ resource "cloudflare_split_tunnel" "example_split_tunnel_exclude" {
 
 # Including *.example.com in WARP routes
 resource "cloudflare_split_tunnel" "example_split_tunnel_include" {
-  account_id = "1d5fdc9e88c8a8c4518b068cd94331fe"
+  account_id = "f037e56e89293a057740de681ac9abbe"
   mode       = "include"
   tunnels {
     host        = "*.example.com"
@@ -20,7 +20,7 @@ resource "cloudflare_split_tunnel" "example_split_tunnel_include" {
 
 # Create a device policy
 resource "cloudflare_device_policy" "developer_warp_policy" {
-  account_id    = "1d5fdc9e88c8a8c4518b068cd94331fe"
+  account_id    = "f037e56e89293a057740de681ac9abbe"
   name          = "Developers"
   precedence    = 10
   match         = "any(identity.groups.name[*] in {\"Developers\"})"
@@ -29,7 +29,7 @@ resource "cloudflare_device_policy" "developer_warp_policy" {
 
 # Excluding *.example.com from WARP routes for a particular device policy
 resource "cloudflare_split_tunnel" "example_device_policy_split_tunnel_exclude" {
-  account_id = "1d5fdc9e88c8a8c4518b068cd94331fe"
+  account_id = "f037e56e89293a057740de681ac9abbe"
   policy_id  = cloudflare_device_policy.developer_warp_policy.id
   mode       = "exclude"
   tunnels {
@@ -40,7 +40,7 @@ resource "cloudflare_split_tunnel" "example_device_policy_split_tunnel_exclude" 
 
 # Including *.example.com in WARP routes for a particular device policy
 resource "cloudflare_split_tunnel" "example_split_tunnel_include" {
-  account_id = "1d5fdc9e88c8a8c4518b068cd94331fe"
+  account_id = "f037e56e89293a057740de681ac9abbe"
   policy_id  = cloudflare_device_policy.developer_warp_policy.id
   mode       = "include"
   tunnels {
