518516eed8048c9e57730bb420759e76a871ee9e
Fabio Peruzzo Barbosa <hcc-fbarbosa@mbplc.com>
2022-11-22 11:58:21 +0100

    Fix the schema for requested_validity

--- internal/provider/schema_cloudflare_origin_ca_certificate.go
+++ internal/provider/schema_cloudflare_origin_ca_certificate.go
@@ -38,6 +38,7 @@ func resourceCloudflareOriginCACertificateSchema() map[string]*schema.Schema {
 			Type:         schema.TypeInt,
 			Optional:     true,
 			Computed:     true,
+			ForceNew:     true,
 			ValidateFunc: validation.IntInSlice([]int{7, 30, 90, 365, 730, 1095, 5475}),
 		},
 	}
