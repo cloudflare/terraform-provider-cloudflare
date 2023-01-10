0187e408f9c0367df30ae91a17eb236cfc3b1887
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-01-10 14:44:34 +1100

    fix acceptance tests

--- internal/provider/resource_cloudflare_device_managed_networks_test.go
+++ internal/provider/resource_cloudflare_device_managed_networks_test.go
@@ -18,31 +18,29 @@ func TestAccCloudflareDeviceManagedNetworks(t *testing.T) {
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCloudflareDeviceManagedNetworks(accountID, rnd, "custom profile"),
+				Config: testAccCloudflareDeviceManagedNetworks(accountID, rnd),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(name, "account_id", accountID),
 					resource.TestCheckResourceAttr(name, "name", rnd),
-					resource.TestCheckResourceAttr(name, "type", "custom"),
-					resource.TestCheckResourceAttr(name, "entry.0.name", fmt.Sprintf("%s_entry1", rnd)),
-					resource.TestCheckResourceAttr(name, "entry.0.enabled", "true"),
-					resource.TestCheckResourceAttr(name, "entry.0.pattern.0.regex", "^4[0-9]"),
-					resource.TestCheckResourceAttr(name, "entry.0.pattern.0.validation", "luhn"),
+					resource.TestCheckResourceAttr(name, "type", "tls"),
+					resource.TestCheckResourceAttr(name, "config.0.tls_sockaddr", "foobar:1234"),
+					resource.TestCheckResourceAttr(name, "config.0.sha256", "b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c"),
 				),
 			},
 		},
 	})
 }
 
-func testAccCloudflareDeviceManagedNetworks(accountID, rnd, description string) string {
+func testAccCloudflareDeviceManagedNetworks(accountID, rnd string) string {
 	return fmt.Sprintf(`
 resource "cloudflare_device_managed_networks" "%[1]s" {
-  account_id                = "%[3]s"
+  account_id                = "%[2]s"
   name                      = "%[1]s"
   type                      = "tls"
   config {
 	tls_sockaddr = "foobar:1234"
-	sha256 = b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c
+	sha256 = "b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c"
   }
 }
-`, rnd, description, accountID)
+`, rnd, accountID)
 }
