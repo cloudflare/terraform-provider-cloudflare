7d66a879a7903ca509a40cce695ab1f4ed5d8604
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-01-12 12:17:47 +1100

    resource/cloudflare_access_bookmark: remove resource

--- examples/resources/cloudflare_access_bookmark/resource.tf
+++
@@ -1,7 +0,0 @@
-resource "cloudflare_access_bookmark" "my_bookmark_app" {
-  account_id           = "f037e56e89293a057740de681ac9abbe"
-  name                 = "My Bookmark App"
-  domain               = "example.com"
-  logo_url             = "https://example.com/example.png"
-  app_launcher_visible = true
-}
