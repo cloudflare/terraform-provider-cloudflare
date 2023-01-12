c2e410835ccf5b1fa24bfa7a486b8eb1108b44d9
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-01-12 12:25:15 +1100

    resource/cloudflare_ip_list: removed in favour of `cloudflare_list`

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -231,7 +231,6 @@ func New(version string) func() *schema.Provider {
 				"cloudflare_firewall_rule":                          resourceCloudflareFirewallRule(),
 				"cloudflare_gre_tunnel":                             resourceCloudflareGRETunnel(),
 				"cloudflare_healthcheck":                            resourceCloudflareHealthcheck(),
-				"cloudflare_ip_list":                                resourceCloudflareIPList(),
 				"cloudflare_ipsec_tunnel":                           resourceCloudflareIPsecTunnel(),
 				"cloudflare_list":                                   resourceCloudflareList(),
 				"cloudflare_load_balancer_monitor":                  resourceCloudflareLoadBalancerMonitor(),
