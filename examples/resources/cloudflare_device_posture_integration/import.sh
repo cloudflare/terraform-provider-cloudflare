0d3fe1e56dfea4afcaaa5949ad9ce779655d9cd2
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-01-12 13:53:19 +1100

    migrate device posture integration to autogen docs

---
+++ examples/resources/cloudflare_device_posture_integration/import.sh
@@ -0,0 +1,1 @@
+$ terraform import cloudflare_device_posture_integration.example <account_id>/<device_posture_integration_id>
--- internal/provider/schema_cloudflare_device_posture_integration.go
+++ internal/provider/schema_cloudflare_device_posture_integration.go
@@ -1,6 +1,8 @@
 package provider
 
 import (
+	"fmt"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
@@ -13,54 +15,64 @@ func resourceCloudflareDevicePostureIntegrationSchema() map[string]*schema.Schem
 			Required:    true,
 		},
 		"name": {
-			Type:     schema.TypeString,
-			Required: true,
+			Type:        schema.TypeString,
+			Required:    true,
+			Description: "Name of the device posture integration.",
 		},
 		"type": {
 			Type:         schema.TypeString,
 			Required:     true,
 			ValidateFunc: validation.StringInSlice([]string{ws1, uptycs, crowdstrike, intune}, false),
+			Description:  fmt.Sprintf("The device posture integration type. %s", renderAvailableDocumentationValuesStringSlice([]string{ws1, uptycs, crowdstrike, intune})),
 		},
 		"identifier": {
 			Type:     schema.TypeString,
 			Optional: true,
 		},
 		"interval": {
-			Type:     schema.TypeString,
-			Optional: true,
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: "Indicates the frequency with which to poll the third-party API. Must be in the format `1h` or `30m`.",
 		},
 		"config": {
-			Type:     schema.TypeList,
-			Optional: true,
+			Type:        schema.TypeList,
+			Optional:    true,
+			Description: "The device posture integration's connection authorization parameters.",
 			Elem: &schema.Resource{
 				Schema: map[string]*schema.Schema{
 					"auth_url": {
 						Type:         schema.TypeString,
 						Optional:     true,
 						ValidateFunc: validation.IsURLWithHTTPS,
+						Description:  "The third-party authorization API URL.",
 					},
 					"api_url": {
 						Type:         schema.TypeString,
 						Optional:     true,
 						ValidateFunc: validation.IsURLWithHTTPS,
+						Description:  "The third-party API's URL.",
 					},
 					"client_id": {
-						Type:     schema.TypeString,
-						Optional: true,
+						Type:        schema.TypeString,
+						Optional:    true,
+						Description: "The client identifier for authenticating API calls.",
 					},
 					"client_secret": {
-						Type:      schema.TypeString,
-						Optional:  true,
-						Sensitive: true,
+						Type:        schema.TypeString,
+						Optional:    true,
+						Sensitive:   true,
+						Description: "The client secret for authenticating API calls.",
 					},
 					"customer_id": {
-						Type:     schema.TypeString,
-						Optional: true,
+						Type:        schema.TypeString,
+						Optional:    true,
+						Description: "The customer identifier for authenticating API calls.",
 					},
 					"client_key": {
-						Type:      schema.TypeString,
-						Optional:  true,
-						Sensitive: true,
+						Type:        schema.TypeString,
+						Optional:    true,
+						Sensitive:   true,
+						Description: "The client key for authenticating API calls.",
 					},
 				},
 			},
