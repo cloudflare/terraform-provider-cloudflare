b061daf8db732cea54f2539fad0207f6024ba0e1
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-01-12 13:23:51 +1100

    migrate aop to autogen docs

--- internal/provider/schema_cloudflare_authenticated_origin_pulls.go
+++ internal/provider/schema_cloudflare_authenticated_origin_pulls.go
@@ -11,16 +11,19 @@ func resourceCloudflareAuthenticatedOriginPullsSchema() map[string]*schema.Schem
 			ForceNew:    true,
 		},
 		"hostname": {
-			Type:     schema.TypeString,
-			Optional: true,
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: "Specify a hostname to enable Per-Hostname Authenticated Origin Pulls on, using the provided certificate.",
 		},
 		"authenticated_origin_pulls_certificate": {
-			Type:     schema.TypeString,
-			Optional: true,
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: "The ID of an uploaded Authenticated Origin Pulls certificate. If no hostname is provided, this certificate will be used zone wide as Per-Zone Authenticated Origin Pulls.",
 		},
 		"enabled": {
-			Type:     schema.TypeBool,
-			Required: true,
+			Type:        schema.TypeBool,
+			Required:    true,
+			Description: "Whether to enable Authenticated Origin Pulls on the given zone or hostname.",
 		},
 	}
 }
