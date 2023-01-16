3a29e1cbd69b5dc49b986bcffb8846d831901a88
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-01-16 15:49:53 +1100

    provider: mux sdkv2 and framework
    To allow us to migrate incrementally to terraform-plugin-framework, we need to:
    
    - Run a protocol v5 => v6 translator; and
    - Mux SDKv2 and framework servers to serve a unified provider
    
    This gets us moving down that path in to run them in a compatible fashion.

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 
-	"github.com/cloudflare/terraform-provider-cloudflare/internal/provider"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/cloudflare/terraform-provider-cloudflare/internal/framework"
+	"github.com/cloudflare/terraform-provider-cloudflare/internal/sdkv2provider"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6/tf6server"
+	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
+	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
 )
 
 var (
@@ -13,16 +20,41 @@ var (
 )
 
 func main() {
-	var debugMode bool
+	var debug bool
 
-	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers")
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers")
 	flag.Parse()
 
-	opts := &plugin.ServeOpts{
-		ProviderFunc: provider.New(version),
-		ProviderAddr: "registry.terraform.io/cloudflare/cloudflare",
-		Debug:        debugMode,
+	ctx := context.Background()
+	upgradedSdkProvider, err := tf5to6server.UpgradeServer(ctx, sdkv2provider.New(version)().GRPCProvider)
+	if err != nil {
+		log.Fatal(err)
+	}
+	providers := []func() tfprotov6.ProviderServer{
+		func() tfprotov6.ProviderServer {
+			return upgradedSdkProvider
+		},
+		providerserver.NewProtocol6(framework.New(version)()),
 	}
 
-	plugin.Serve(opts)
+	muxServer, err := tf6muxserver.NewMuxServer(ctx, providers...)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var serveOpts []tf6server.ServeOpt
+	if debug {
+		serveOpts = append(serveOpts, tf6server.WithManagedDebug())
+	}
+
+	err = tf6server.Serve(
+		"registry.terraform.io/cloudflare/cloudflare",
+		muxServer.ProviderServer,
+		serveOpts...,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
