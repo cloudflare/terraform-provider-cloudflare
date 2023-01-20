57c6e2753d6d1677344ad085be3668a400bdd2bd
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-01-20 11:57:13 +1100

    comment out framework tests for now

--- internal/framework/service/example/example_data_source_test.go
+++ internal/framework/service/example/example_data_source_test.go
@@ -1,30 +1,23 @@
 package example
 
-import (
-	"testing"
+// func TestAccExampleDataSource(t *testing.T) {
+// 	resource.Test(t, resource.TestCase{
+// 		PreCheck:                 func() { provider.TestAccPreCheck(t) },
+// 		ProtoV6ProviderFactories: provider.TestAccProtoV6ProviderFactories,
+// 		Steps: []resource.TestStep{
+// 			// Read testing
+// 			{
+// 				Config: testAccExampleDataSourceConfig,
+// 				Check: resource.ComposeAggregateTestCheckFunc(
+// 					resource.TestCheckResourceAttr("data.scaffolding_example.test", "id", "example-id"),
+// 				),
+// 			},
+// 		},
+// 	})
+// }
 
-	"github.com/cloudflare/terraform-provider-cloudflare/internal/framework/provider"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-)
-
-func TestAccExampleDataSource(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { provider.TestAccPreCheck(t) },
-		ProtoV6ProviderFactories: provider.TestAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			// Read testing
-			{
-				Config: testAccExampleDataSourceConfig,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.scaffolding_example.test", "id", "example-id"),
-				),
-			},
-		},
-	})
-}
-
-const testAccExampleDataSourceConfig = `
-data "scaffolding_example" "test" {
-  configurable_attribute = "example"
-}
-`
+// const testAccExampleDataSourceConfig = `
+// data "scaffolding_example" "test" {
+//   configurable_attribute = "example"
+// }
+// `
