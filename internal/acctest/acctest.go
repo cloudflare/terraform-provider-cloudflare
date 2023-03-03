bd2ecb331e5f81c85b5c7e8c5efb4337beafd3bd
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-03-03 15:49:11 +1100

    split acceptance testing package into it's own

--- internal/acctest/acctest.go
+++ internal/acctest/acctest.go
@@ -3,6 +3,7 @@ package acctest
 import (
 	"context"
 	"log"
+	"os"
 	"testing"
 
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/framework/provider"
@@ -11,6 +12,21 @@ import (
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
 	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
+)
+
+var (
+	// Integration test account ID.
+	testAccCloudflareAccountID string = "f037e56e89293a057740de681ac9abbe"
+
+	// Integration test account zone ID.
+	testAccCloudflareZoneID string = "0da42c8d2132a9ddaf714f9e7c920711"
+	// Integration test account zone name.
+	testAccCloudflareZoneName string = "terraform.cfapi.net"
+
+	// Integration test account alternate zone ID.
+	testAccCloudflareAltZoneID string = "b72110c08e3382597095c29ba7e661ea"
+	// Integration test account alternate zone name.
+	testAccCloudflareAltZoneName string = "terraform2.cfapi.net"
 )
 
 var TestAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
@@ -34,4 +50,28 @@ func TestAccPreCheck(t *testing.T) {
 	// You can add code here to run prior to any test case execution, for example assertions
 	// about the appropriate environment variables being set are common to see in a pre-check
 	// function.
+}
+
+func TestAccPreCheck_Zone(t *testing.T) {
+	// You can add code here to run prior to any test case execution, for example assertions
+	// about the appropriate environment variables being set are common to see in a pre-check
+	// function.
+}
+
+func TestAccPreCheck_Account(t *testing.T) {
+	// You can add code here to run prior to any test case execution, for example assertions
+	// about the appropriate environment variables being set are common to see in a pre-check
+	// function.
+}
+
+func TestAccSkipForDefaultZone(t *testing.T, reason string) {
+	if os.Getenv("CLOUDFLARE_ZONE_ID") == testAccCloudflareZoneID {
+		t.Skipf("Skipping acceptance test for default zone (%s). %s", testAccCloudflareZoneID, reason)
+	}
+}
+
+func TestAccSkipForDefaultAccount(t *testing.T, reason string) {
+	if os.Getenv("CLOUDFLARE_ACCOUNT_ID") == testAccCloudflareAccountID {
+		t.Skipf("Skipping acceptance test for default account (%s). %s", testAccCloudflareAccountID, reason)
+	}
 }
