2685c80526bf9f5674f213e3089b32f78c2e173d
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-03-20 10:58:03 +1100

    improve test coverage for lists

--- internal/sdkv2provider/data_source_lists_test.go
+++ internal/sdkv2provider/data_source_lists_test.go
@@ -25,6 +25,7 @@ func TestAccCloudflareListsDataSource(t *testing.T) {
 				Config: testAccCheckCloudflareListsDataSource(accountID, rnd),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(name, consts.AccountIDSchemaKey, accountID),
+					resource.TestCheckResourceAttr(name, "lists.0.numitems", "4"),
 				),
 			},
 		},
@@ -33,7 +34,43 @@ func TestAccCloudflareListsDataSource(t *testing.T) {
 
 func testAccCheckCloudflareListsDataSource(accountID, name string) string {
 	return fmt.Sprintf(`
+	resource "cloudflare_list" "%[1]s" {
+		account_id = "%[2]s"
+		name = "%[1]s"
+		description = "example list"
+		kind = "ip"
+
+		item {
+		  value {
+			ip = "192.0.2.0"
+		  }
+		  comment = "one"
+		}
+
+		item {
+		  value {
+			ip = "192.0.2.1"
+		  }
+		  comment = "two"
+		}
+
+		item {
+		  value {
+			ip = "192.0.2.2"
+		  }
+		  comment = "three"
+		}
+
+		item {
+		  value {
+			ip = "192.0.2.3"
+		  }
+		  comment = "four"
+		}
+	  }
+
 data "cloudflare_lists" "%[1]s" {
   account_id = "%[2]s"
+  depends_on = [ cloudflare_list.%[1]s ]
 }`, name, accountID)
 }
