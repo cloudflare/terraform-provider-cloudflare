589d3e0663e061b296271924c394ef220f373bd1
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-03-20 11:57:04 +1100

    inline acctest method

--- internal/utils/random_acc_test_name.go
+++ internal/utils/random_acc_test_name.go
@@ -1,9 +1,31 @@
 package utils
 
-import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+import "math/rand"
+
+const (
+	// charSetAlphaNum is the alphanumeric character set for use with
+	// RandStringFromCharSet.
+	charSetAlphaNum = "abcdefghijklmnopqrstuvwxyz012346789"
+
+	// charSetAlpha is the alphabetical character set for use with
+	// RandStringFromCharSet.
+	charSetAlpha = "abcdefghijklmnopqrstuvwxyz"
+
+	// Length of the resource name we wish to generate.
+	resourceNameLength = 10
+)
 
 // GenerateRandomResourceName builds a unique-ish resource identifier to use in
 // tests.
 func GenerateRandomResourceName() string {
-	return acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
+	result := make([]byte, resourceNameLength)
+	for i := 0; i < resourceNameLength; i++ {
+		result[i] = charSetAlpha[randIntRange(0, len(charSetAlpha))]
+	}
+	return string(result)
+}
+
+// randIntRange returns a random integer between min (inclusive) and max (exclusive)
+func randIntRange(min int, max int) int {
+	return rand.Intn(max-min) + min
 }
