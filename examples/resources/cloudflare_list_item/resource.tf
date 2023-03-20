23f47e622ae0adb8432664cd23ec281ac3a0c9b0
Ryan Whelan <4249368+rwhelan@users.noreply.github.com>
2023-03-20 10:37:48 -0400

    doc: update cloudflare_list_item example

--- examples/resources/cloudflare_list_item/resource.tf
+++ examples/resources/cloudflare_list_item/resource.tf
@@ -9,7 +9,7 @@ resource "cloudflare_list" "example_ip_list" {
 # IP List Item
 resource "cloudflare_list_item" example_ip_item" {
   account_id = "01234567890123456789012345678901"
-  list_id    = cloudflare_list.example_ip_list.id
+  list_id    = data.cloudflare_list.example_ip_list.id
   comment    = "List Item Comment"
   ip         = "192.0.2.0"
 }
@@ -18,7 +18,7 @@ resource "cloudflare_list_item" example_ip_item" {
 # Redirect List Item
 resource "cloudflare_list_item" "test_two" {
   account_id = "01234567890123456789012345678901"
-  list_id    = cloudflare_list.example_ip_list.id
+  list_id    = data.cloudflare_list.example_ip_list.id
   redirect {
     source_url       = "https://source.tld"
     target_url       = "https://target.tld"
