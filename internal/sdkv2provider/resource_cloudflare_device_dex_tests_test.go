53dba0867ee991f685275f726e1fdb437cb4f49d
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-03-23 10:59:43 +1100

    split tests into individual test cases

--- internal/sdkv2provider/resource_cloudflare_device_dex_tests_test.go
+++ internal/sdkv2provider/resource_cloudflare_device_dex_tests_test.go
@@ -4,10 +4,62 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-func TestAccCloudflareDeviceDexTests(t *testing.T) {
+func TestAccCloudflareDeviceDexTest_Traceroute(t *testing.T) {
+	rnd := generateRandomResourceName()
+	name := fmt.Sprintf("cloudflare_device_dex_test.%s", rnd)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheckAccount(t)
+		},
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudflareDeviceDexTestsTraceroute(accountID, rnd),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(name, "account_id", accountID),
+					resource.TestCheckResourceAttr(name, "name", rnd),
+					resource.TestCheckResourceAttr(name, "description", rnd),
+					resource.TestCheckResourceAttr(name, "interval", "0h30m0s"),
+					resource.TestCheckResourceAttr(name, "enabled", "true"),
+					resource.TestCheckResourceAttr(name, "data.0.host", "dash.cloudflare.com"),
+					resource.TestCheckResourceAttr(name, "data.0.kind", "traceroute"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccCloudflareDeviceDexTest_TracerouteIPv4(t *testing.T) {
+	rnd := generateRandomResourceName()
+	name := fmt.Sprintf("cloudflare_device_dex_test.%s", rnd)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheckAccount(t)
+		},
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudflareDeviceDexTestsTracerouteIpv4(accountID, rnd),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(name, "account_id", accountID),
+					resource.TestCheckResourceAttr(name, "name", rnd),
+					resource.TestCheckResourceAttr(name, "description", rnd),
+					resource.TestCheckResourceAttr(name, "interval", "0h30m0s"),
+					resource.TestCheckResourceAttr(name, "enabled", "true"),
+					resource.TestCheckResourceAttr(name, "data.0.host", "1.1.1.1"),
+					resource.TestCheckResourceAttr(name, "data.0.kind", "traceroute"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccCloudflareDeviceDexTest_HTTP(t *testing.T) {
 	rnd := generateRandomResourceName()
 	name := fmt.Sprintf("cloudflare_device_dex_test.%s", rnd)
 
@@ -28,30 +80,6 @@ func TestAccCloudflareDeviceDexTests(t *testing.T) {
 					resource.TestCheckResourceAttr(name, "data.0.host", "https://dash.cloudflare.com/home"),
 					resource.TestCheckResourceAttr(name, "data.0.kind", "http"),
 					resource.TestCheckResourceAttr(name, "data.0.method", "GET"),
-				),
-			},
-			{
-				Config: testAccCloudflareDeviceDexTestsTraceroute(accountID, rnd),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(name, "account_id", accountID),
-					resource.TestCheckResourceAttr(name, "name", rnd),
-					resource.TestCheckResourceAttr(name, "description", rnd),
-					resource.TestCheckResourceAttr(name, "interval", "0h30m0s"),
-					resource.TestCheckResourceAttr(name, "enabled", "true"),
-					resource.TestCheckResourceAttr(name, "data.0.host", "dash.cloudflare.com"),
-					resource.TestCheckResourceAttr(name, "data.0.kind", "http"),
-				),
-			},
-			{
-				Config: testAccCloudflareDeviceDexTestsTracerouteIpv4(accountID, rnd),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(name, "account_id", accountID),
-					resource.TestCheckResourceAttr(name, "name", rnd),
-					resource.TestCheckResourceAttr(name, "description", rnd),
-					resource.TestCheckResourceAttr(name, "interval", "0h30m0s"),
-					resource.TestCheckResourceAttr(name, "enabled", "true"),
-					resource.TestCheckResourceAttr(name, "data.0.host", "1.1.1.1"),
-					resource.TestCheckResourceAttr(name, "data.0.kind", "http"),
 				),
 			},
 		},
