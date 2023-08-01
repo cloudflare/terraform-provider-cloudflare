54f5fc5a0635b33247d425a03c219224df344317
Krzysztof Wilczyński <kwilczynski@redhat.com>
2023-08-01 16:50:21 +0900

    Fix resource usage example
    Signed-off-by: Krzysztof Wilczyński <kwilczynski@redhat.com>

--- examples/resources/cloudflare_zone_cache_reserve/resource.tf
+++ examples/resources/cloudflare_zone_cache_reserve/resource.tf
@@ -1,5 +1,4 @@
-# Enable the Cache Reserve support for a given zone.
-resource "cloudflare_zone_cache_variants" "example" {
+resource "cloudflare_zone_cache_reserve" "example" {
   zone_id = "0da42c8d2132a9ddaf714f9e7c920711"
   enabled = true
 }
