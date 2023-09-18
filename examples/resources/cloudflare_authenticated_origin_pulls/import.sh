4d81f6800763f8bcc069207514796d64ecbdd8c9
Jacob Bednarz <jacob.bednarz@hey.com>
2023-09-18 19:37:22 +1000

    Update import.sh

--- examples/resources/cloudflare_authenticated_origin_pulls/import.sh
+++ examples/resources/cloudflare_authenticated_origin_pulls/import.sh
@@ -2,7 +2,7 @@
 $ terraform import cloudflare_authenticated_origin_pulls.example <zone_id>
 
 # Per-Zone Authenticated Origin Pull configuration
-$ terraform import cloudflare_authenticated_origin_pulls.my_per_zone_aop <zone_id>/<certificate_id>/
+$ terraform import cloudflare_authenticated_origin_pulls.example <zone_id>/<certificate_id>
 
 # Per-Hostname Authenticated Origin Pull configuration
 $ terraform import cloudflare_authenticated_origin_pulls.my_per_hostname_aop <zone_id>/<certificate_id>/<hostname>
