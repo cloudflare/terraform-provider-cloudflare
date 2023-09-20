371726b00b6d6096c18a885211dce7b19b9f11f6
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-09-20 12:55:05 +1000

    datasource/account_roles: use updated methods for autopagination
    Closes #2768

--- internal/sdkv2provider/data_source_account_roles.go
+++ internal/sdkv2provider/data_source_account_roles.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/cloudflare/cloudflare-go"
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/consts"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -15,8 +14,7 @@ func dataSourceCloudflareAccountRolesRead(ctx context.Context, d *schema.Resourc
 	client := meta.(*cloudflare.API)
 	accountID := d.Get(consts.AccountIDSchemaKey).(string)
 
-	tflog.Debug(ctx, fmt.Sprintf("Reading Account Roles"))
-	roles, err := client.AccountRoles(ctx, accountID)
+	roles, err := client.ListAccountRoles(ctx, cloudflare.AccountIdentifier(accountID), cloudflare.ListAccountRolesParams{})
 	if err != nil {
 		return diag.FromErr(fmt.Errorf("error listing Account Roles: %w", err))
 	}
