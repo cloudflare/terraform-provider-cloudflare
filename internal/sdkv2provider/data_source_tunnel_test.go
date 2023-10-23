3dcb9d6a14ea0c838065a6172d46961a63b1eb1d
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-10-23 11:56:00 +1100

    make test reliable
    use an explicit depends_on to make sure the resource always exists before attempting to find it

--- internal/sdkv2provider/data_source_tunnel_test.go
+++ internal/sdkv2provider/data_source_tunnel_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-func TestAccCloudflareTunnelMatchName(t *testing.T) {
+func TestAccCloudflareTunnel_MatchName(t *testing.T) {
 	rnd := generateRandomResourceName()
 
 	resource.Test(t, resource.TestCase{
@@ -18,7 +18,7 @@ func TestAccCloudflareTunnelMatchName(t *testing.T) {
 			{
 				Config: testCloudflareTunnelMatchName(rnd),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.cloudflare_tunnel.example", "status", "inactive"),
+					resource.TestCheckResourceAttr("data.cloudflare_tunnel."+rnd, "status", "inactive"),
 				),
 			},
 		},
@@ -28,14 +28,16 @@ func TestAccCloudflareTunnelMatchName(t *testing.T) {
 func testCloudflareTunnelMatchName(name string) string {
 	accountID := os.Getenv("CLOUDFLARE_ACCOUNT_ID")
 	return fmt.Sprintf(`
-resource "cloudflare_tunnel" "example" {
+resource "cloudflare_tunnel" "%[2]s" {
 	account_id = "%[1]s"
-	name       = %[2]q
+	name       = "%[2]s"
 	secret     = "AQIDBAUGBwgBAgMEBQYHCAECAwQFBgcIAQIDBAUGBwg="
 }
-data "cloudflare_tunnel" "example" {
-	account_id = cloudflare_tunnel.example.account_id
-	name       = cloudflare_tunnel.example.name
+
+data "cloudflare_tunnel" "%[2]s" {
+	account_id = cloudflare_tunnel.%[2]s.account_id
+	name       = cloudflare_tunnel.%[2]s.name
+	depends_on = [cloudflare_tunnel.%[2]s]
 }
 `, accountID, name)
 }
