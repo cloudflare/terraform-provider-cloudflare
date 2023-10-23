b82429e030f77e4c7438e8f92eb81b221fc08ab6
Lee Boynton <lee.boynton@truelayer.com>
2023-10-23 15:08:12 +0100

    fix import example

--- examples/resources/cloudflare_worker_secret/import.sh
+++ examples/resources/cloudflare_worker_secret/import.sh
@@ -1,1 +1,1 @@
-$ terraform import cloudflare_worker_script.example <account_id>/<script_name>/<secret_name>
+$ terraform import cloudflare_worker_secret.example <account_id>/<script_name>/<secret_name>
