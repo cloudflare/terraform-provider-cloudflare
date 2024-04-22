ae0ebb87ff97a54d501223dce6b526823eca2de4
Greg Dubicki <grzegorz.dubicki@gmail.com>
2024-04-22 14:04:41 +0100

    Depend explicitly on the challenge to prevent some errors
    like
    ```
    "registry.terraform.io/cloudflare/cloudflare" produced an invalid new value
    for .ownership_challenge: was null, but now cty.StringVal("[redacted]..[redacted]")
    ```
    
    As recommended by @jacobbednarz in
    https://github.com/cloudflare/terraform-provider-cloudflare/issues/2752#issuecomment-1712190497
    
    Workaround for issues like
    https://github.com/cloudflare/terraform-provider-cloudflare/issues/2794
    https://github.com/cloudflare/terraform-provider-cloudflare/issues/2752
    https://github.com/cloudflare/terraform-provider-cloudflare/issues/3001

--- examples/resources/cloudflare_logpush_job/resource.tf
+++ examples/resources/cloudflare_logpush_job/resource.tf
@@ -45,6 +45,9 @@ data "aws_s3_bucket_object" "challenge_file" {
 }
 
 resource "cloudflare_logpush_job" "example_job" {
+  depends_on = [
+    cloudflare_logpush_ownership_challenge.ownership_challenge, data.aws_s3_bucket_object.challenge_file
+  ]
   enabled             = true
   zone_id             = "0da42c8d2132a9ddaf714f9e7c920711"
   name                = "My-logpush-job"
