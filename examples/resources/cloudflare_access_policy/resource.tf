6da63b470fc2f8782675ac737ae982d707e6e17c
Eduardo Gomes <egomes@cloudflare.com>
2024-06-03 20:42:22 +0200

    Update cloudflare_access_application and cloudflare_access_policy examples

--- examples/resources/cloudflare_access_policy/resource.tf
+++ examples/resources/cloudflare_access_policy/resource.tf
@@ -1,7 +1,6 @@
 # Allowing access to `test@example.com` email address only
 resource "cloudflare_access_policy" "test_policy" {
-  application_id = "cb029e245cfdd66dc8d2e570d5dd3322"
-  zone_id        = "0da42c8d2132a9ddaf714f9e7c920711"
+  account_id     = "0da42c8d2132a9ddaf714f9e7c920711"
   name           = "staging policy"
   precedence     = "1"
   decision       = "allow"
@@ -18,8 +17,7 @@ resource "cloudflare_access_policy" "test_policy" {
 # Allowing `test@example.com` to access but only when coming from a
 # specific IP.
 resource "cloudflare_access_policy" "test_policy" {
-  application_id = "cb029e245cfdd66dc8d2e570d5dd3322"
-  zone_id        = "0da42c8d2132a9ddaf714f9e7c920711"
+  account_id     = "0da42c8d2132a9ddaf714f9e7c920711"
   name           = "staging policy"
   precedence     = "1"
   decision       = "allow"
