eb77d18955b5c0f4e6ea55a7fa7611abb4bc06a0
Brenno Oliveira <brenno.oliveira@deliveryhero.com>
2024-07-26 16:29:59 +0200

    resource/cloudflare_teams_rule: add disable_clipboard_redirection
    Signed-off-by: Brenno Oliveira <brenno.oliveira@deliveryhero.com>

--- internal/sdkv2provider/resource_cloudflare_teams_rules_test.go
+++ internal/sdkv2provider/resource_cloudflare_teams_rules_test.go
@@ -266,3 +266,83 @@ resource "cloudflare_teams_rule" "%[1]s" {
 }
 `, rnd, accountID)
 }
+
+func TestAccCloudflareTeamsRule_WithClipboardRedirection(t *testing.T) {
+	// Temporarily unset CLOUDFLARE_API_TOKEN if it is set as the Access
+	// service does not yet support the API tokens and it results in
+	// misleading state error messages.
+	if os.Getenv("CLOUDFLARE_API_TOKEN") != "" {
+		t.Setenv("CLOUDFLARE_API_TOKEN", "")
+	}
+
+	rnd := generateRandomResourceName()
+	name := fmt.Sprintf("cloudflare_teams_rule.%s", rnd)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		ProviderFactories: providerFactories,
+		CheckDestroy:      testAccCheckCloudflareTeamsRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudflareTeamsRuleConfigWithClipboardRedirection(rnd, accountID),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(name, consts.AccountIDSchemaKey, accountID),
+					resource.TestCheckResourceAttr(name, "name", rnd),
+					resource.TestCheckResourceAttr(name, "description", "desc"),
+					resource.TestCheckResourceAttr(name, "precedence", "12302"),
+					resource.TestCheckResourceAttr(name, "action", "block"),
+					resource.TestCheckResourceAttr(name, "filters.0", "dns"),
+					resource.TestCheckResourceAttr(name, "traffic", "any(dns.domains[*] == \"example.com\")"),
+					resource.TestCheckResourceAttr(name, "rule_settings.#", "1"),
+					resource.TestCheckResourceAttr(name, "rule_settings.0.block_page_enabled", "true"),
+					resource.TestCheckResourceAttr(name, "rule_settings.0.block_page_reason", "cuz"),
+					resource.TestCheckResourceAttr(name, "rule_settings.0.insecure_disable_dnssec_validation", "false"),
+					resource.TestCheckResourceAttr(name, "rule_settings.0.egress.0.ipv4", "203.0.113.1"),
+					resource.TestCheckResourceAttr(name, "rule_settings.0.egress.0.ipv6", "2001:db8::/32"),
+					resource.TestCheckResourceAttr(name, "rule_settings.0.untrusted_cert.0.action", "error"),
+					resource.TestCheckResourceAttr(name, "rule_settings.0.payload_log.0.enabled", "true"),
+					resource.TestCheckResourceAttr(name, "rule_settings.0.biso_admin_controls.0.disable_clipboard_redirection", "true"), // Check new parameter
+				),
+			},
+		},
+	})
+}
+
+func testAccCloudflareTeamsRuleConfigWithClipboardRedirection(rnd, accountID string) string {
+	return fmt.Sprintf(`
+resource "cloudflare_teams_rule" "%[1]s" {
+  name = "%[1]s"
+  account_id = "%[2]s"
+  description = "desc"
+  precedence = 12302
+  action = "block"
+  filters = ["dns"]
+  traffic = "any(dns.domains[*] == \"example.com\")"
+  rule_settings {
+    block_page_enabled = true
+    block_page_reason = "cuz"
+    insecure_disable_dnssec_validation = false
+    egress {
+      ipv4 = "203.0.113.1"
+      ipv6 = "2001:db8::/32"
+    }
+    untrusted_cert {
+      action = "error"
+    }
+    payload_log {
+      enabled = true
+    }
+    biso_admin_controls {
+      disable_printing = false
+      disable_copy_paste = false
+      disable_download = false
+      disable_upload = false
+      disable_keyboard = false
+      disable_clipboard_redirection = true // Set new parameter
+    }
+  }
+}
+`, rnd, accountID)
+}
