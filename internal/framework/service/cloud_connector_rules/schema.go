676d35d1257da2b25b59a81c7b51c12f57cec0d7
Denis Davydov <ddavydov@cloudflare.com>
2024-08-13 01:49:04 +0100

    FLPROD-1370: Register cloud connector rules resource
    Create rule resource for a new Cloud Connector service

---
+++ internal/framework/service/cloud_connector_rules/schema.go
@@ -0,0 +1,89 @@
+package cloud_connector_rules
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/MakeNowJust/heredoc/v2"
+	"github.com/cloudflare/terraform-provider-cloudflare/internal/consts"
+	"github.com/cloudflare/terraform-provider-cloudflare/internal/utils"
+	"github.com/hashicorp/terraform-plugin-framework-validators/setvalidator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+)
+
+func (r *CloudConnectorRulesResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: heredoc.Doc(`
+			The [Cloud Connector Rules](add link to doc) resource allows you to create and manage cloud connector rules for a zone.
+		`),
+		Version: 1,
+
+		Attributes: map[string]schema.Attribute{
+			consts.ZoneIDSchemaKey: schema.StringAttribute{
+				MarkdownDescription: consts.ZoneIDSchemaDescription,
+				Required:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+		},
+		Blocks: map[string]schema.Block{
+			"rules": schema.SetNestedBlock{
+				MarkdownDescription: "List of Cloud Connector Rules",
+				Validators: []validator.Set{
+					setvalidator.SizeAtLeast(1),
+					setvalidator.IsRequired(),
+				},
+				NestedObject: schema.NestedBlockObject{
+					Attributes: map[string]schema.Attribute{
+						"provider": schema.StringAttribute{
+							Required: true,
+							MarkdownDescription: fmt.Sprintf("Type of provider. %s", utils.RenderAvailableDocumentationValuesStringSlice([]string{
+								"aws_s3",
+								"cloudflare_r2",
+								"azure_storage",
+								"gcp_storage",
+							})),
+							Validators: []validator.String{
+								stringvalidator.OneOf(
+									"aws_s3",
+									"cloudflare_r2",
+									"azure_storage",
+									"gcp_storage",
+								),
+							},
+						},
+						"enabled": schema.BoolAttribute{
+							Optional:            true,
+							MarkdownDescription: "Whether the headers rule is active.",
+						},
+						"expression": schema.StringAttribute{
+							Required:            true,
+							MarkdownDescription: "Criteria for an HTTP request to trigger the cloud connector rule. Uses the Firewall Rules expression language based on Wireshark display filters.",
+						},
+						"description": schema.StringAttribute{
+							Optional:            true,
+							MarkdownDescription: "Brief summary of the cloud connector rule and its intended use.",
+						},
+					},
+					Blocks: map[string]schema.Block{
+						"parameters": schema.SingleNestedBlock{
+							MarkdownDescription: "Cloud Connector Rule Parameters",
+							Attributes: map[string]schema.Attribute{
+								"host": schema.StringAttribute{
+									Required:            true,
+									MarkdownDescription: "Host parameter for cloud connector rule",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
