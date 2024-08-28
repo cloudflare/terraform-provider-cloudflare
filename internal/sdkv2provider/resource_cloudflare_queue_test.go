f9b9d33871661d5319993a9deef571262176a27c
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-08-28 22:01:29 +0000

    asdd

--- internal/sdkv2provider/resource_cloudflare_queue_test.go
+++ internal/sdkv2provider/resource_cloudflare_queue_test.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"encoding/json"
 
 	"github.com/cloudflare/cloudflare-go"
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/consts"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 )
 
 func init() {
@@ -80,6 +82,61 @@ func TestAccCloudflareQueue_Basic(t *testing.T) {
 	})
 }
 
+var _ plancheck.PlanCheck = debugPlan{}
+
+type debugPlan struct{}
+
+func (e debugPlan) CheckPlan(ctx context.Context, req plancheck.CheckPlanRequest, resp *plancheck.CheckPlanResponse) {
+	rd, err := json.Marshal(req.Plan)
+	if err != nil {
+		fmt.Println("error marshalling machine-readable plan output:", err)
+	}
+	fmt.Printf("req.Plan - %s\n", string(rd))
+}
+
+func DebugPlan() plancheck.PlanCheck {
+	return debugPlan{}
+}
+
+func TestAccCloudflareQueue_Consumer(t *testing.T) {
+	t.Parallel()
+	var queue cloudflare.Queue
+	accountID := os.Getenv("CLOUDFLARE_ACCOUNT_ID")
+	rnd := generateRandomResourceName()
+	resourceName := "cloudflare_queue." + rnd
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		CheckDestroy:      testAccCloudflareQueueDestroy,
+		Steps: []resource.TestStep{
+			// {
+			// 	Config: testAccCheckCloudflareQueueWConsumer(rnd, accountID, rnd),
+			// 	ConfigPlanChecks: resource.ConfigPlanChecks{
+			// 		PostApplyPreRefresh: []plancheck.PlanCheck{
+			// 			DebugPlan(),
+			// 		},
+			// 	},
+			// },
+			{
+				Config: testAccCheckCloudflareQueueWConsumer(rnd, accountID, rnd),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckCloudflareQueueExists(rnd, &queue),
+					resource.TestCheckResourceAttr(resourceName, "name", rnd),
+					resource.TestCheckResourceAttr(resourceName, "account_id", accountID),
+				),
+			},
+			
+			{
+				ImportState:         true,
+				ImportStateVerify:   true,
+				ResourceName:        resourceName,
+				ImportStateIdPrefix: fmt.Sprintf("%s/", accountID),
+			},
+		},
+	})
+}
+
 func testAccCloudflareQueueDestroy(s *terraform.State) error {
 	client := testAccProvider.Meta().(*cloudflare.API)
 
@@ -110,6 +167,34 @@ resource "cloudflare_queue" "%[1]s" {
 	account_id = "%[2]s"
 	name = "%[3]s"
 }`, rnd, accountID, name)
+}
+
+func testAccCheckCloudflareQueueWConsumer(rnd, accountID, name string) string {
+	queueModuleContent := `export default { queue(batch, env) { return new Response('Hello world'); }, };`
+	tf_content := fmt.Sprintf(`
+	resource "cloudflare_workers_script" "%[1]s" {
+		  account_id = "%[2]s"
+		  name = "%[3]s"
+		  content = "%[4]s"
+		  module = true
+		  compatibility_date = "2023-03-19"
+		  compatibility_flags = ["nodejs_compat", "web_socket_compression"]
+	}
+	
+	resource "cloudflare_queue" "%[1]s" {
+		account_id = "%[2]s"
+		name = "%[3]s"
+		consumer {
+			script_name = "%[1]s"
+			settings {
+				batch_size = 1
+				max_retries = 1
+				max_wait_time_ms = 1000
+			}
+		}
+		depends_on = [cloudflare_workers_script.%[1]s]
+	}`, rnd, accountID, name, queueModuleContent)
+	return tf_content
 }
 
 func testAccCheckCloudflareQueueExists(name string, queue *cloudflare.Queue) resource.TestCheckFunc {
--- internal/sdkv2provider/schema_cloudflare_queue.go
+++ internal/sdkv2provider/schema_cloudflare_queue.go
@@ -17,5 +17,68 @@ func resourceCloudflareQueueSchema() map[string]*schema.Schema {
 			Required:    true,
 			Description: "The name of the queue.",
 		},
+		"consumer": {
+			Type:        schema.TypeList,
+			Optional:    true,
+			Description: "Array of consumers.",
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					"created_on": {
+						Description: "Created on date",
+						Type:        schema.TypeString,
+						Optional:    true,
+						ForceNew:    true,
+					},
+					"environment": {
+						Description: "Environment",
+						Type:        schema.TypeString,
+						Optional:    true,
+						ForceNew:    true,
+					},
+					"queue_name": {
+						Description: "Queue name",
+						Type:        schema.TypeString,
+						Optional:    true,
+						ForceNew:    true,
+					},
+					"script_name": {
+						Description: "script_name",
+						Type:        schema.TypeString,
+						Optional:    true,
+						ForceNew:    true,
+					},
+					"settings": {
+						Description: "Settings",
+						Type:        schema.TypeList,
+						Optional:    true,
+						ForceNew:    true,
+						Elem: &schema.Resource{
+							Schema: map[string]*schema.Schema{
+								"batch_size": {
+									Description: "Batch size",
+									Type:        schema.TypeInt,
+									Optional:    true,
+									ForceNew:    true,
+								},
+								"max_retries": {
+									Description: "Max retries",
+									Type:        schema.TypeInt,
+									Optional:    true,
+									ForceNew:    true,
+								},
+								"max_wait_time_ms": {
+									Description: "Max wait time in milliseconds",
+									Type:        schema.TypeInt,
+									Optional:    true,
+									ForceNew:    true,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 }
+
+
