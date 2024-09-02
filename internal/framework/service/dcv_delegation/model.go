023d24ecfedaa8268546cdd70f7c3239810420b0
Jacob Bednarz <jacob.bednarz@hey.com>
2024-09-03 08:53:28 +1000

    remove `cloudflare` prefix

--- internal/framework/service/dcv_delegation/model.go
+++ internal/framework/service/dcv_delegation/model.go
@@ -2,7 +2,7 @@ package dcv_delegation
 
 import "github.com/hashicorp/terraform-plugin-framework/types"
 
-type CloudflareDCVDelegationModel struct {
+type DCVDelegationModel struct {
 	ID       types.String `tfsdk:"id"`
 	ZoneID   types.String `tfsdk:"zone_id"`
 	Hostname types.String `tfsdk:"hostname"`
