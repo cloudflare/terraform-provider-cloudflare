96c396b5d42d60f84251c2bb00f9b9da757fa260
Jacob Bednarz <jacob.bednarz@gmail.com>
2024-09-05 13:17:15 +1000

    test(access_key_configuration): fix test assertions

--- internal/services/zero_trust_access_key_configuration/resource_test.go
+++ internal/services/zero_trust_access_key_configuration/resource_test.go
@@ -41,35 +41,3 @@ func TestAccCloudflareAccessKeysConfiguration_WithKeyRotationIntervalDaysSet(t *
 func testAccessKeysConfigurationWithKeyRotationIntervalDays(rnd, accountID string, days int) string {
 	return acctest.LoadTestCase("accesskeysconfigurationwithkeyrotationintervaldays.tf", rnd, accountID, days)
 }
-
-func TestAccCloudflareAccessKeysConfiguration_WithoutKeyRotationIntervalDaysSet(t *testing.T) {
-	// Temporarily unset CLOUDFLARE_API_TOKEN if it is set as the Access
-	// service does not yet support the API tokens and it results in
-	// misleading state error messages.
-	if os.Getenv("CLOUDFLARE_API_TOKEN") != "" {
-		t.Setenv("CLOUDFLARE_API_TOKEN", "")
-	}
-
-	rnd := utils.GenerateRandomResourceName()
-	name := fmt.Sprintf("cloudflare_zero_trust_access_key_configuration.%s", rnd)
-	accountID := os.Getenv("CLOUDFLARE_ACCOUNT_ID")
-
-	resource.Test(t, resource.TestCase{
-		PreCheck: func() {
-			acctest.TestAccPreCheck(t)
-		},
-		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccessKeysConfigurationWithoutKeyRotationIntervalDays(rnd, accountID),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(name, "key_rotation_interval_days"),
-				),
-			},
-		},
-	})
-}
-
-func testAccessKeysConfigurationWithoutKeyRotationIntervalDays(rnd, accountID string) string {
-	return acctest.LoadTestCase("accesskeysconfigurationwithoutkeyrotationintervaldays.tf", rnd, accountID)
-}
