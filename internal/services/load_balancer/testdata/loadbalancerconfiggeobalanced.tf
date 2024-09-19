8376e1e73553d2abfbf5b6d91489048789122da7
Mike Girouard <mgirouard@cloudflare.com>
2024-09-19 16:07:30 -0400

    Fix TestAccCloudflareLoadBalancer_GeoBalanced

--- internal/services/load_balancer/testdata/loadbalancerconfiggeobalanced.tf
+++ internal/services/load_balancer/testdata/loadbalancerconfiggeobalanced.tf
@@ -1,22 +1,19 @@
-
 resource "cloudflare_load_balancer" "%[3]s" {
   zone_id = "%[1]s"
   name = "tf-testacc-lb-%[3]s.%[2]s"
-  fallback_pool_id = "${cloudflare_load_balancer_pool.%[3]s.id}"
-  default_pool_ids = ["${cloudflare_load_balancer_pool.%[3]s.id}"]
+  fallback_pool = "${cloudflare_load_balancer_pool.%[3]s.id}"
+  default_pools = ["${cloudflare_load_balancer_pool.%[3]s.id}"]
   description = "tf-acctest load balancer using geo-balancing"
   proxied = true // can't set ttl with proxied
   steering_policy = "geo"
-  pop_pools =[ {
-    pop = "LAX"
-    pool_ids = ["${cloudflare_load_balancer_pool.%[3]s.id}"]
-  }]
-  country_pools =[ {
-    country = "US"
-    pool_ids = ["${cloudflare_load_balancer_pool.%[3]s.id}"]
-  }]
-  region_pools =[ {
-    region = "WNAM"
-    pool_ids = ["${cloudflare_load_balancer_pool.%[3]s.id}"]
-  }]
+  pop_pools ={
+    "LAX": ["${cloudflare_load_balancer_pool.%[3]s.id}"]
+  }
+  country_pools = {
+    "US": ["${cloudflare_load_balancer_pool.%[3]s.id}"]
+  }
+  region_pools = {
+    "WNAM": ["${cloudflare_load_balancer_pool.%[3]s.id}"]
+  }
+  session_affinity = "none"
 }
