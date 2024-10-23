da934d4d900541551688cdd53c6efa9c45d15cad
Jacob Bednarz <jacob.bednarz@gmail.com>
2024-10-23 14:39:05 +1100

    simplify tests

--- internal/services/page_shield_policy/resource_test.go
+++ internal/services/page_shield_policy/resource_test.go
@@ -1,28 +1,24 @@
 package page_shield_policy_test
 
 import (
-	"context"
 	"os"
 	"testing"
 
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/acctest"
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/utils"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-func TestAccCloudflarePageShieldPolicy_Create(t *testing.T) {
-	ctx := context.Background()
+func TestAccCloudflarePageShieldPolicy_Basic(t *testing.T) {
 	randomID := utils.GenerateRandomResourceName()
 	resourceName := "cloudflare_page_shield_policy." + randomID
-
 	zoneID := os.Getenv("CLOUDFLARE_ZONE_ID")
-	if zoneID == "" {
-		tflog.Error(ctx, "CLOUDFLARE_ZONE_ID must be set for cloudflare_page_shield_policy")
-	}
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
+		PreCheck: func() {
+			acctest.TestAccPreCheck(t)
+			acctest.TestAccPreCheck_ZoneID(t)
+		},
 		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
@@ -34,27 +30,6 @@ func TestAccCloudflarePageShieldPolicy_Create(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "enabled", "false"),
 					resource.TestCheckResourceAttr(resourceName, "value", "script-src 'none'"),
 				),
-			},
-		},
-	})
-}
-
-func TestAccCloudflarePageShieldPolicy_Update(t *testing.T) {
-	ctx := context.Background()
-	randomID := utils.GenerateRandomResourceName()
-	resourceName := "cloudflare_page_shield_policy." + randomID
-
-	zoneID := os.Getenv("CLOUDFLARE_ZONE_ID")
-	if zoneID == "" {
-		tflog.Error(ctx, "CLOUDFLARE_ZONE_ID must be set for cloudflare_page_shield_policy")
-	}
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
-		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			{
-				Config: testPageShieldPolicyConfig(randomID, zoneID, "description", "log", "true", "false", "script-src 'none'"),
 			},
 			{
 				Config: testPageShieldPolicyConfig(
