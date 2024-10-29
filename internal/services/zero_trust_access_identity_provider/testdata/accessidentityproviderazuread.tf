ce7ed04964592e70a127954fde55a5f7d2cf8400
Musa Jundi <musa@cloudflare.com>
2024-10-29 12:55:52 -0500

    Fix test data and acceptance test cases for Zero Trust IDP

--- internal/services/zero_trust_access_identity_provider/testdata/accessidentityproviderazuread.tf
+++ internal/services/zero_trust_access_identity_provider/testdata/accessidentityproviderazuread.tf
@@ -1,18 +1,17 @@
-
 resource "cloudflare_zero_trust_access_identity_provider" "%[2]s" {
-	account_id = "%[1]s"
-	name       = "%[2]s"
-	type       = "azureAD"
-	config = {
-  client_id      = "test"
-		client_secret  = "test"
-		directory_id   = "directory"
-		support_groups = true
-}
-	scim_config = {
-  enabled                  = true
-		group_member_deprovision = true
-		seat_deprovision         = true
-		user_deprovision         = true
-}
+  account_id = "%[1]s"
+  name       = "%[2]s"
+  type       = "azureAD"
+  config = {
+    client_id      = "test"
+    client_secret  = "test"
+    directory_id   = "directory"
+    support_groups = true
+  }
+  scim_config = {
+    enabled                  = true
+    group_member_deprovision = true
+    seat_deprovision         = true
+    user_deprovision         = true
+  }
 }
