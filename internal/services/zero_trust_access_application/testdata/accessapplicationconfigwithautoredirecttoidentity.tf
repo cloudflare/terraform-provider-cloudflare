60a12b8cf4f26b50d076c284a8372d7c75830471
Musa Jundi <musa@cloudflare.com>
2024-11-11 17:00:43 -0600

    Fix access application auto redirect test data

--- internal/services/zero_trust_access_application/testdata/accessapplicationconfigwithautoredirecttoidentity.tf
+++ internal/services/zero_trust_access_application/testdata/accessapplicationconfigwithautoredirecttoidentity.tf
@@ -2,7 +2,10 @@ resource "cloudflare_zero_trust_access_identity_provider" "%[1]s" {
   zone_id = "%[2]s"
   name    = "%[1]s"
   type    = "onetimepin"
-  config = {}
+  config = {
+    client_name = "test"
+    client_secret = "test"
+  }
 }
 
 resource "cloudflare_zero_trust_access_application" "%[1]s" {
