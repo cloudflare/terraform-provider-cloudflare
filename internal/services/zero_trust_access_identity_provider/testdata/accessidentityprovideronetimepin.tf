4a01af4a08a93c347bc46dd1987b9512454cbd78
Musa Jundi <musa@cloudflare.com>
2024-11-11 16:16:09 -0600

    Fix test data for zero trust IDP 'onetimepin' variant

--- internal/services/zero_trust_access_identity_provider/testdata/accessidentityprovideronetimepin.tf
+++ internal/services/zero_trust_access_identity_provider/testdata/accessidentityprovideronetimepin.tf
@@ -2,5 +2,7 @@ resource "cloudflare_zero_trust_access_identity_provider" "%[1]s" {
   %[2]s_id = "%[3]s"
   name     = "%[1]s"
   type     = "onetimepin"
-  config = {}
+  config = {
+    client_name = "test_name"
+  }
 }
