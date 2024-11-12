696bb4a34621141bce04158f59cf0eb5bd282d52
Jacob Bednarz <jacob.bednarz@gmail.com>
2024-11-12 14:51:03 +1100

    fix(api_token): fix test data
    Signed-off-by: Jacob Bednarz <jacob.bednarz@gmail.com>

--- internal/services/api_token/testdata/apitokenwithindividualcondition.tf
+++ internal/services/api_token/testdata/apitokenwithindividualcondition.tf
@@ -1,18 +1,17 @@
+resource "cloudflare_api_token" "%[1]s" {
+	name = "%[1]s"
 
-	resource "cloudflare_api_token" "%[1]s" {
-		name = "%[1]s"
-
-		policy = [{
-			effect = "allow"
-			permission_groups = [{
-		    id = "%[2]s"
-			}]
-			resources = { "com.cloudflare.api.account.zone.*" = "*" }
+	policies = [{
+		effect = "allow"
+		permission_groups = [{
+	    id = "%[2]s"
 		}]
+		resources = { "com.cloudflare.api.account.zone.*" = "*" }
+	}]
 
-		condition = {
-      request_ip = {
-				in = ["192.0.2.1/32"]
-			}
+	condition = {
+    request_ip = {
+			in = ["192.0.2.1/32"]
 		}
 	}
+}
