2baaf8c59d34f2264324902a5607aa6de478385e
Jacob Bednarz <jacob.bednarz@gmail.com>
2024-11-15 13:44:25 +1100

    test(zone): fix data source examples

--- internal/services/zone/data_source_zone_test.go
+++ internal/services/zone/data_source_zone_test.go
@@ -1,55 +1,42 @@
 package zone_test
 
-// func TestAccCloudflareZone_PreventZoneIdAndNameConflicts(t *testing.T) {
-// 	t.Parallel()
-// 	rnd := utils.GenerateRandomResourceName()
-// 	resource.Test(t, resource.TestCase{
-// 		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
-// 		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
-// 		Steps: []resource.TestStep{
-// 			{
-// 				Config:      testAccCloudflareZoneConfigConflictingFields(rnd),
-// 				ExpectError: regexp.MustCompile(regexp.QuoteMeta("only one of `name,zone_id` can be specified")),
-// 			},
-// 		},
-// 	})
-// }
+import (
+	"fmt"
+	"testing"
 
-// func testAccCloudflareZoneConfigConflictingFields(rnd string) string {
-// 	return fmt.Sprintf(`
-// data "cloudflare_zone" "%[1]s" {
-//   name    = "terraform.cfapi.net"
-//   zone_id = "abc123"
-// }
-// `, rnd)
-// }
+	"github.com/cloudflare/terraform-provider-cloudflare/internal/acctest"
+	"github.com/cloudflare/terraform-provider-cloudflare/internal/consts"
+	"github.com/cloudflare/terraform-provider-cloudflare/internal/utils"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
 
-// func TestAccCloudflareZone_NameLookup(t *testing.T) {
-// 	t.Parallel()
-// 	rnd := utils.GenerateRandomResourceName()
-// 	name := fmt.Sprintf("data.cloudflare_zone.%s", rnd)
+func TestAccCloudflareZone_NameLookup(t *testing.T) {
+	t.Parallel()
+	rnd := utils.GenerateRandomResourceName()
+	name := fmt.Sprintf("data.cloudflare_zone.%s", rnd)
 
-// 	resource.Test(t, resource.TestCase{
-// 		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
-// 		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
-// 		Steps: []resource.TestStep{
-// 			{
-// 				Config: testAccCloudflareZoneConfigBasic(rnd),
-// 				Check: resource.ComposeTestCheckFunc(
-// 					testAccCheckCloudflareZonesDataSourceID(name),
-// 					resource.TestCheckResourceAttr(name, "name", "terraform.cfapi.net"),
-// 					resource.TestCheckResourceAttr(name, consts.ZoneIDSchemaKey, acctest.TestAccCloudflareZoneID),
-// 					resource.TestCheckResourceAttr(name, "status", "active"),
-// 				),
-// 			},
-// 		},
-// 	})
-// }
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudflareZoneConfigBasic(rnd),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(name, "name", "terraform.cfapi.net"),
+					resource.TestCheckResourceAttr(name, consts.IDSchemaKey, acctest.TestAccCloudflareZoneID),
+					resource.TestCheckResourceAttr(name, "status", "active"),
+				),
+			},
+		},
+	})
+}
 
-// func testAccCloudflareZoneConfigBasic(rnd string) string {
-// 	return fmt.Sprintf(`
-// data "cloudflare_zone" "%[1]s" {
-//   name = "terraform.cfapi.net"
-// }
-// `, rnd)
-// }
+func testAccCloudflareZoneConfigBasic(rnd string) string {
+	return fmt.Sprintf(`
+data "cloudflare_zone" "%[1]s" {
+  filter = {
+  	name = "terraform.cfapi.net"
+  }
+}
+`, rnd)
+}
