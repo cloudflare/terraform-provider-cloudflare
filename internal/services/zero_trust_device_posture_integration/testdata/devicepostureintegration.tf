ec96c679cde4dabf036e3345f966a85a6e116516
Musa Jundi <musa@cloudflare.com>
2024-11-18 08:56:20 -0600

    Format 'device_posture_integration' test data

--- internal/services/zero_trust_device_posture_integration/testdata/devicepostureintegration.tf
+++ internal/services/zero_trust_device_posture_integration/testdata/devicepostureintegration.tf
@@ -1,13 +1,12 @@
-
 resource "cloudflare_zero_trust_device_posture_integration" "%[1]s" {
 	account_id                = "%[2]s"
 	name                      = "%[1]s"
 	type                      = "workspace_one"
 	interval                  = "24h"
 	config = {
-  api_url       =  "%[5]s"
+		api_url       =  "%[5]s"
 		auth_url      =  "%[6]s"
 		client_id     =  "%[3]s"
 		client_secret =  "%[4]s"
-}
+	}
 }
