b7ed6c33b05c0921d82b907cb47356ab448738bf
Jacob Bednarz <jacob.bednarz@gmail.com>
2024-11-26 10:10:24 +1100

    page rule foundations

--- internal/services/page_rule/model.go
+++ internal/services/page_rule/model.go
@@ -13,14 +13,14 @@ type PageRuleResultEnvelope struct {
 }
 
 type PageRuleModel struct {
-	ID         types.String             `tfsdk:"id" json:"id,computed"`
-	ZoneID     types.String             `tfsdk:"zone_id" path:"zone_id,required"`
-	Actions    *[]*PageRuleActionsModel `tfsdk:"actions" json:"actions,required"`
-	Targets    *[]*PageRuleTargetsModel `tfsdk:"targets" json:"targets,required"`
-	Priority   types.Int64              `tfsdk:"priority" json:"priority,computed_optional"`
-	Status     types.String             `tfsdk:"status" json:"status,computed_optional"`
-	CreatedOn  timetypes.RFC3339        `tfsdk:"created_on" json:"created_on,computed" format:"date-time"`
-	ModifiedOn timetypes.RFC3339        `tfsdk:"modified_on" json:"modified_on,computed" format:"date-time"`
+	ID         types.String          `tfsdk:"id" json:"id,computed"`
+	ZoneID     types.String          `tfsdk:"zone_id" path:"zone_id,required"`
+	Actions    *PageRuleActionsModel `tfsdk:"actions" json:"actions,required"`
+	Target     types.String          `tfsdk:"target" json:"target,required"`
+	Priority   types.Int64           `tfsdk:"priority" json:"priority,computed_optional"`
+	Status     types.String          `tfsdk:"status" json:"status,computed_optional"`
+	CreatedOn  timetypes.RFC3339     `tfsdk:"created_on" json:"created_on,computed" format:"date-time"`
+	ModifiedOn timetypes.RFC3339     `tfsdk:"modified_on" json:"modified_on,computed" format:"date-time"`
 }
 
 func (m PageRuleModel) MarshalJSON() (data []byte, err error) {
@@ -32,16 +32,6 @@ func (m PageRuleModel) MarshalJSONForUpdate(state PageRuleModel) (data []byte, e
 }
 
 type PageRuleActionsModel struct {
-	ID    types.String `tfsdk:"id" json:"id,optional"`
-	Value types.String `tfsdk:"value" json:"value,optional"`
-}
-
-type PageRuleTargetsModel struct {
-	Constraint *PageRuleTargetsConstraintModel `tfsdk:"constraint" json:"constraint,required"`
-	Target     types.String                    `tfsdk:"target" json:"target,required"`
-}
-
-type PageRuleTargetsConstraintModel struct {
-	Operator types.String `tfsdk:"operator" json:"operator,computed_optional"`
-	Value    types.String `tfsdk:"value" json:"value,required"`
+	AutomaticHTTPSRewrites types.String `tfsdk:"automatic_https_rewrites" json:"automatic_https_rewrites,optional"`
+	// ... more
 }
--- internal/services/page_rule/schema.go
+++ internal/services/page_rule/schema.go
@@ -6,9 +6,11 @@ import (
 	"context"
 
 	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
+	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64default"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
@@ -31,108 +33,419 @@ func ResourceSchema(ctx context.Context) schema.Schema {
 				Required:      true,
 				PlanModifiers: []planmodifier.String{stringplanmodifier.RequiresReplace()},
 			},
-			"actions": schema.ListNestedAttribute{
-				Description: "The set of actions to perform if the targets of this rule match the\nrequest. Actions can redirect to another URL or override settings, but\nnot both.\n",
-				Required:    true,
-				NestedObject: schema.NestedAttributeObject{
-					Attributes: map[string]schema.Attribute{
-						"id": schema.StringAttribute{
-							Description: "If enabled, any `http://`` URL is converted to `https://` through a\n301 redirect.\n",
-							Optional:    true,
-							Validators: []validator.String{
-								stringvalidator.OneOfCaseInsensitive(
-									"always_use_https",
-									"automatic_https_rewrites",
-									"browser_cache_ttl",
-									"browser_check",
-									"bypass_cache_on_cookie",
-									"cache_by_device_type",
-									"cache_deception_armor",
-									"cache_key",
-									"cache_key_fields",
-									"cache_level",
-									"cache_on_cookie",
-									"cache_ttl_by_status",
-									"ddos_protection",
-									"development_mode",
-									"disable_apps",
-									"disable_performance",
-									"disable_security",
-									"disable_zaraz",
-									"edge_cache_ttl",
-									"email_obfuscation",
-									"explicit_cache_control",
-									"forwarding_url",
-									"host_header_override",
-									"hotlink_protection",
-									"ip_geolocation",
-									"minify",
-									"mirage",
-									"opportunistic_encryption",
-									"origin_error_page_pass_thru",
-									"polish",
-									"purge_by_page_rule",
-									"resolve_override",
-									"respect_strong_etag",
-									"response_buffering",
-									"rocket_loader",
-									"security_level",
-									"server_side_exclude",
-									"sort_query_string_for_cache",
-									"ssl",
-									"true_client_ip_header",
-									"waf",
-								),
-							},
-						},
-						"value": schema.StringAttribute{
-							Description: "The status of Automatic HTTPS Rewrites.\n",
-							Optional:    true,
-							Validators: []validator.String{
-								stringvalidator.OneOfCaseInsensitive("on", "off"),
-							},
-						},
-					},
-				},
+			"target": schema.StringAttribute{
+				Required: true,
 			},
-			"targets": schema.ListNestedAttribute{
-				Description: "The rule targets to evaluate on each request.",
-				Required:    true,
-				NestedObject: schema.NestedAttributeObject{
-					Attributes: map[string]schema.Attribute{
-						"constraint": schema.SingleNestedAttribute{
-							Description: "String constraint.",
-							Required:    true,
-							Attributes: map[string]schema.Attribute{
-								"operator": schema.StringAttribute{
-									Description: "The matches operator can use asterisks and pipes as wildcard and 'or' operators.",
-									Computed:    true,
-									Optional:    true,
-									Validators: []validator.String{
-										stringvalidator.OneOfCaseInsensitive(
-											"matches",
-											"contains",
-											"equals",
-											"not_equal",
-											"not_contain",
-										),
-									},
-									Default: stringdefault.StaticString("contains"),
-								},
-								"value": schema.StringAttribute{
-									Description: "The URL pattern to match against the current request. The pattern may contain up to four asterisks ('*') as placeholders.",
-									Required:    true,
-								},
-							},
+			"actions": schema.SingleNestedAttribute{
+				Required: true,
+				Attributes: map[string]schema.Attribute{
+					"automatic_https_rewrites": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
 						},
-						"target": schema.StringAttribute{
-							Description: "A target based on the URL of the request.",
-							Required:    true,
-							Validators: []validator.String{
-								stringvalidator.OneOfCaseInsensitive("url"),
+					},
+
+					"bypass_cache_on_cookie": schema.StringAttribute{
+						Optional: true,
+					},
+
+					"cache_by_device_type": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
+						},
+					},
+
+					"cache_deception_armor": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
+						},
+					},
+
+					"cache_on_cookie": schema.StringAttribute{
+						Optional: true,
+					},
+
+					"mirage": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
+						},
+					},
+
+					"explicit_cache_control": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
+						},
+					},
+
+					"origin_error_page_pass_thru": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
+						},
+					},
+
+					"sort_query_string_for_cache": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
+						},
+					},
+
+					"respect_strong_etag": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
+						},
+					},
+
+					"response_buffering": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
+						},
+					},
+
+					// may not be used with disable_performance
+					"rocket_loader": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
+						},
+					},
+
+					"true_client_ip_header": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
+						},
+					},
+
+					"browser_check": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
+						},
+					},
+
+					"email_obfuscation": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
+						},
+					},
+
+					"ip_geolocation": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
+						},
+					},
+
+					// may get api errors trying to set this
+					"opportunistic_encryption": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
+						},
+					},
+
+					"server_side_exclude": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
+						},
+					},
+
+					"waf": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("on", "off"),
+						},
+					},
+
+					"always_use_https": schema.BoolAttribute{
+						Default:  booldefault.StaticBool(false),
+						Optional: true,
+					},
+
+					"disable_apps": schema.BoolAttribute{
+						Default:  booldefault.StaticBool(false),
+						Optional: true,
+					},
+
+					"disable_performance": schema.BoolAttribute{
+						Default:  booldefault.StaticBool(false),
+						Optional: true,
+					},
+
+					"disable_railgun": schema.BoolAttribute{
+						Default:  booldefault.StaticBool(false),
+						Optional: true,
+					},
+
+					"disable_security": schema.BoolAttribute{
+						Default:  booldefault.StaticBool(false),
+						Optional: true,
+					},
+
+					"disable_zaraz": schema.BoolAttribute{
+						Default:  booldefault.StaticBool(false),
+						Optional: true,
+					},
+
+					"browser_cache_ttl": schema.StringAttribute{
+						Optional: true,
+					},
+
+					"edge_cache_ttl": schema.Int64Attribute{
+						Optional: true,
+						Validators: []validator.Int64{
+							int64validator.AtMost(31536000),
+						},
+					},
+
+					"cache_level": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("bypass", "basic", "simplified", "aggressive", "cache_everything"),
+						},
+					},
+
+					"forwarding_url": schema.SingleNestedAttribute{
+						Optional: true,
+						Attributes: map[string]schema.Attribute{
+							"url": schema.StringAttribute{
+								Required: true,
+							},
+
+							"status_code": schema.Int64Attribute{
+								Required: true,
+								Validators: []validator.Int64{
+									int64validator.Between(301, 302),
+								},
 							},
 						},
 					},
+
+					"minify": schema.SingleNestedAttribute{
+						Optional: true,
+						Attributes: map[string]schema.Attribute{
+							"js": schema.StringAttribute{
+								Required: true,
+								Validators: []validator.String{
+									stringvalidator.OneOfCaseInsensitive("on", "off"),
+								},
+							},
+
+							"css": schema.StringAttribute{
+								Required: true,
+								Validators: []validator.String{
+									stringvalidator.OneOfCaseInsensitive("on", "off"),
+								},
+							},
+
+							"html": schema.StringAttribute{
+								Required: true,
+								Validators: []validator.String{
+									stringvalidator.OneOfCaseInsensitive("on", "off"),
+								},
+							},
+						},
+					},
+
+					"host_header_override": schema.StringAttribute{
+						Optional: true,
+					},
+
+					"polish": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("off", "lossless", "lossy"),
+						},
+					},
+
+					"resolve_override": schema.StringAttribute{
+						Optional: true,
+					},
+
+					"security_level": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("off", "essentially_off", "low", "medium", "high", "under_attack"),
+						},
+					},
+
+					"ssl": schema.StringAttribute{
+						Optional: true,
+						Validators: []validator.String{
+							stringvalidator.OneOfCaseInsensitive("off", "flexible", "full", "strict", "origin_pull"),
+						},
+					},
+
+					// "cache_key_fields": {
+					// 	Type:     schema.SingleNestedAttribute,
+					// 	Optional: true,
+					// 	Attributes: map[string]schema.Attribute{
+					// 			"cookie": {
+					// 				Type:     schema.SingleNestedAttribute,
+					// 				Optional: true,
+					// 				Attributes: map[string]schema.Attribute{
+					// 						"check_presence": {
+					// 							Type:     schema.SetAttribute,
+					// 							Optional: true,
+					// 							Computed: true,
+					// 							ElementType:   types.StringType,
+					// 						},
+					// 						"include": {
+					// 							Type:     schema.SetAttribute,
+					// 							Optional: true,
+					// 							Computed: true,
+					// 							ElementType:   types.StringType,
+					// 						},
+					// 					},
+					// 				},
+					// 			},
+
+					// 			"header": {
+					// 				Type:     schema.SingleNestedAttribute,
+					// 				Optional: true,
+					// 				Attributes: map[string]schema.Attribute{
+					// 						"check_presence": {
+					// 							Type:     schema.SetAttribute,
+					// 							Optional: true,
+					// 							Computed: true,
+					// 							ElementType:   types.StringType,
+					// 						},
+					// 						"exclude": {
+					// 							Type:     schema.SetAttribute,
+					// 							Optional: true,
+					// 							Computed: true,
+					// 							ElementType:   types.StringType,
+					// 						},
+					// 						"include": {
+					// 							Type:     schema.SetAttribute,
+					// 							Optional: true,
+					// 							Computed: true,
+					// 							ElementType:   types.StringType,
+					// 						},
+					// 					},
+					// 				},
+					// 			},
+
+					// 			"host": {
+					// 				Type:     schema.SingleNestedAttribute,
+					// 				Required: true,
+					// 				Attributes: map[string]schema.Attribute{
+					// 						"resolved": {
+					// 							Type:     schema.BoolAttribute,
+					// 							Optional: true,
+					// 							Default:  booldefault.StaticBool(false),
+					// 						},
+					// 					},
+					// 				},
+					// 			},
+
+					// 			"query_string": {
+					// 				Type:     schema.SingleNestedAttribute,
+					// 				Required: true,
+					// 				Attributes: map[string]schema.Attribute{
+					// 						"exclude": {
+					// 							Type:     schema.SetAttribute,
+					// 							Optional: true,
+					// 							Computed: true,
+					// 							Elem: &schema.Schema{
+					// 								Type: schema.StringAttribute,
+					// 								ValidateDiagFunc: func(v interface{}, p cty.Path) diag.Diagnostics {
+					// 									value := v.(string)
+					// 									var diags diag.Diagnostics
+
+					// 									if value == "*" {
+					// 										diag := diag.Diagnostic{
+					// 											Severity: diag.Error,
+					// 											Summary:  "Invalid exclude value",
+					// 											Detail:   fmt.Sprintf("full wildcards are not supported for exclude, use ignore=true instead. value: %s", value),
+					// 										}
+					// 										diags = append(diags, diag)
+					// 									}
+
+					// 									return diags
+					// 								}},
+					// 						},
+					// 						"include": {
+					// 							Type:     schema.SetAttribute,
+					// 							Optional: true,
+					// 							Computed: true,
+					// 							Elem: &schema.Schema{
+					// 								Type: schema.StringAttribute,
+					// 								ValidateDiagFunc: func(v interface{}, p cty.Path) diag.Diagnostics {
+					// 									value := v.(string)
+					// 									var diags diag.Diagnostics
+
+					// 									if value == "*" {
+					// 										diag := diag.Diagnostic{
+					// 											Severity: diag.Error,
+					// 											Summary:  "Invalid include value",
+					// 											Detail:   fmt.Sprintf("full wildcards are not supported for include, use ignore=false instead. value: %s", value),
+					// 										}
+					// 										diags = append(diags, diag)
+					// 									}
+
+					// 									return diags
+					// 								},
+					// 							},
+					// 						},
+					// 						"ignore": {
+					// 							Type:     schema.BoolAttribute,
+					// 							Optional: true,
+					// 							Computed: true,
+					// 						},
+					// 					},
+					// 				},
+					// 			},
+
+					// 			"user": {
+					// 				Type:     schema.SingleNestedAttribute,
+					// 				Required: true,
+					// 				Attributes: map[string]schema.Attribute{
+					// 						"device_type": {
+					// 							Type:     schema.BoolAttribute,
+					// 							Optional: true,
+					// 							Computed: true,
+					// 						},
+					// 						"geo": {
+					// 							Type:     schema.BoolAttribute,
+					// 							Optional: true,
+					// 							Computed: true,
+					// 						},
+					// 						"lang": {
+					// 							Type:     schema.BoolAttribute,
+					// 							Optional: true,
+					// 							Computed: true,
+					// 						},
+					// 					},
+					// 				},
+					// 			},
+					// 		},
+					// 	},
+					// 	"cache_ttl_by_status": {
+					// 	Type:     schema.SetAttribute,
+					// 	Optional: true,
+					// 	Attributes: map[string]schema.Attribute{
+					// 			"codes": {
+					// 				Type:     schema.StringAttribute,
+					// 				Required: true,
+					// 			},
+					// 			"ttl": {
+					// 				Type:     schema.Int64Attribute,
+					// 				Required: true,
+					// 			},
+					// 	},
+					// },
 				},
 			},
 			"priority": schema.Int64Attribute{
