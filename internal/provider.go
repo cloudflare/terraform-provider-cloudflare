5544c28eae7e8fc80ba23095841f90dcfb5adfd9
Jacob Bednarz <jacob.bednarz@gmail.com>
2024-11-25 15:00:10 +1100

    unused imports

--- internal/provider.go
+++ internal/provider.go
@@ -96,8 +96,6 @@ import (
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/services/queue"
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/services/queue_consumer"
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/services/r2_bucket"
-	"github.com/cloudflare/terraform-provider-cloudflare/internal/services/r2_custom_domain"
-	"github.com/cloudflare/terraform-provider-cloudflare/internal/services/r2_managed_domain"
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/services/rate_limit"
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/services/regional_hostname"
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/services/regional_tiered_cache"
