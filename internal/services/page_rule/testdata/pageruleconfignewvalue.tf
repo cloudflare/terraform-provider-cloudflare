a5eeab0e42bb4b4a55fe4005b4d0fc7a06150294
Jacob Fowler <jfowler@cloudflare.com>
2024-12-13 14:29:32 -0500

    fix some more page rules tests and testcase files

--- internal/services/page_rule/testdata/pageruleconfignewvalue.tf
+++ internal/services/page_rule/testdata/pageruleconfignewvalue.tf
@@ -1,10 +1,10 @@
 
 resource "cloudflare_page_rule" "%[3]s" {
-	zone_id = "%[1]s"
-	target = "%[2]s/updated"
-	actions =[ {
-		browser_check = "on"
-		ssl = "strict"
-		rocket_loader = "on"
-	}]
+  zone_id = "%[1]s"
+  target  = "%[2]s/updated"
+  actions = {
+    browser_check = "on"
+    ssl           = "strict"
+    rocket_loader = "on"
+  }
 }
