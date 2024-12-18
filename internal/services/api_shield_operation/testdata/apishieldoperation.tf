ff943fcc13f0ce9b6177bb64197ccff07de31a80
Vaishak Dinesh <vaishak@cloudflare.com>
2024-12-18 14:28:48 -0800

    fix: api shield tests and test data

--- internal/services/api_shield_operation/testdata/apishieldoperation.tf
+++ internal/services/api_shield_operation/testdata/apishieldoperation.tf
@@ -1,7 +1,10 @@
-
-	resource "cloudflare_api_shield_operation" "%[1]s" {
-		zone_id = "%[2]s"
-		method = "%[3]s"
-		host = "%[4]s"
-		endpoint = "%[5]s"
-	}
+resource "cloudflare_api_shield_operation" "%[1]s" {
+	zone_id = "%[2]s"
+	operations = [
+		{
+			method = "%[3]s"
+			host = "%[4]s"
+			endpoint = "%[5]s"
+		}
+	]
+}
