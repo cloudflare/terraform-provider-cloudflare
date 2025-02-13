214ea98f75030a350705ecf0997513d9a8f0486e
Jacob Bednarz <jacob.bednarz@gmail.com>
2025-02-13 14:31:53 +1100

    use inbuilt resource instead

--- internal/services/dns_zone_transfers_outgoing/resource_test.go
+++ internal/services/dns_zone_transfers_outgoing/resource_test.go
@@ -58,31 +58,18 @@ func TestAccCloudflareSecondaryDNSOutgoing_Basic(t *testing.T) {
 	zoneID := os.Getenv("CLOUDFLARE_ZONE_ID")
 	accountID := os.Getenv("CLOUDFLARE_ACCOUNT_ID")
 
-	client := acctest.SharedClient()
-
-	peer, err := client.DNS.ZoneTransfers.Peers.New(context.Background(), dns.ZoneTransferPeerNewParams{AccountID: cloudflare.F(accountID), Name: cloudflare.F("terraform-peer")})
-	if err != nil {
-		tflog.Error(context.Background(), fmt.Sprintf("Failed to bootstrap Cloudflare DNS peer: %s", err))
-	}
-
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testSecondaryDNSOutgoingConfig(rnd, zoneName, zoneID, peer.ID),
+				Config: testSecondaryDNSOutgoingConfig(rnd, zoneName, zoneID, accountID),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(name, "name", zoneName),
-					resource.TestCheckResourceAttr(name, "peers.0", peer.ID),
 				),
 			},
 		},
 	})
-	// Delete the original peer after we are done
-	_, err = client.DNS.ZoneTransfers.Peers.Delete(context.Background(), peer.ID, dns.ZoneTransferPeerDeleteParams{AccountID: cloudflare.F(accountID)})
-	if err != nil {
-		tflog.Error(context.Background(), fmt.Sprintf("Failed to cleanup Cloudflare DNS peer in outgoing test: %s", err))
-	}
 }
 
 func TestAccCloudflareSecondaryDNSOutgoing_Update(t *testing.T) {
@@ -92,44 +79,23 @@ func TestAccCloudflareSecondaryDNSOutgoing_Update(t *testing.T) {
 	zoneID := os.Getenv("CLOUDFLARE_ZONE_ID")
 	accountID := os.Getenv("CLOUDFLARE_ACCOUNT_ID")
 
-	client := acctest.SharedClient()
-
-	peer1, err := client.DNS.ZoneTransfers.Peers.New(context.Background(), dns.ZoneTransferPeerNewParams{AccountID: cloudflare.F(accountID), Name: cloudflare.F("terraform-peer")})
-	if err != nil {
-		tflog.Error(context.Background(), fmt.Sprintf("Failed to bootstrap Cloudflare DNS peer: %s", err))
-	}
-
-	peer2, err := client.DNS.ZoneTransfers.Peers.New(context.Background(), dns.ZoneTransferPeerNewParams{AccountID: cloudflare.F(accountID), Name: cloudflare.F("terraform-peer")})
-	if err != nil {
-		tflog.Error(context.Background(), fmt.Sprintf("Failed to bootstrap Cloudflare DNS peer: %s", err))
-	}
-
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testSecondaryDNSOutgoingConfig(rnd, zoneName, zoneID, peer1.ID),
+				Config: testSecondaryDNSOutgoingConfig(rnd, zoneName, zoneID, accountID),
 			},
 			{
-				Config: testSecondaryDNSOutgoingConfig(rnd, zoneName, zoneID, peer2.ID),
+				Config: testSecondaryDNSOutgoingConfig(rnd, zoneName, zoneID, accountID),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(name, "name", zoneName),
-					resource.TestCheckResourceAttr(name, "peers.0", peer2.ID),
 				),
 			},
 		},
 	})
-	// Delete the original peers after we are done
-	for _, id := range []string{peer1.ID, peer2.ID} {
-		_, err = client.DNS.ZoneTransfers.Peers.Delete(context.Background(), id, dns.ZoneTransferPeerDeleteParams{AccountID: cloudflare.F(accountID)})
-		if err != nil {
-			tflog.Error(context.Background(), fmt.Sprintf("Failed to cleanup Cloudflare DNS peer in outgoing test: %s", err))
-		}
-	}
-
 }
 
-func testSecondaryDNSOutgoingConfig(resourceID, zoneName, zoneID string, peers string) string {
-	return acctest.LoadTestCase("outgoing.tf", resourceID, zoneID, zoneName, peers)
+func testSecondaryDNSOutgoingConfig(resourceID, zoneName, zoneID string, accountID string) string {
+	return acctest.LoadTestCase("outgoing.tf", resourceID, zoneID, zoneName, accountID)
 }
