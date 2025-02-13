7511053d43c81dacd1b6c6b91d7992d8372981fe
Jacob Bednarz <jacob.bednarz@gmail.com>
2025-02-13 13:52:07 +1100

    test(dns_zone_transfers_incoming): fix resource names

--- internal/services/dns_zone_transfers_incoming/testdata/incoming.tf
+++ internal/services/dns_zone_transfers_incoming/testdata/incoming.tf
@@ -1,4 +1,4 @@
-resource "cloudflare_secondary_dns_incoming" "%[1]s" {
+resource "cloudflare_dns_zone_transfers_incoming" "%[1]s" {
   zone_id = "%[2]s"
   auto_refresh_seconds = "%[3]d"
   name = "%[4]s"
