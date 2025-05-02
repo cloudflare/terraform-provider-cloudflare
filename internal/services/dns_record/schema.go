98f379c72ad60d3e012e4ee9f8963efbd3ec01f3
Jacob Bednarz <jacob.bednarz@gmail.com>
2025-05-02 13:27:06 +1000

    test: fix integrity assertions

--- internal/services/dns_record/schema.go
+++ internal/services/dns_record/schema.go
@@ -321,8 +321,7 @@ func ResourceSchema(ctx context.Context) schema.Schema {
 			},
 			"ttl": schema.Float64Attribute{
 				Description: "Time To Live (TTL) of the DNS record in seconds. Setting to 1 means 'automatic'. Value must be between 60 and 86400, with the minimum reduced to 30 for Enterprise zones.",
-				Computed:    true,
-				Optional:    true,
+				Required:    true,
 			},
 			"tags": schema.ListAttribute{
 				Description: "Custom tags for the DNS record. This field has no effect on DNS responses.",
