596e18c76f93109fabe22270e6274b185d163e87
Jacob Bednarz <jacob.bednarz@gmail.com>
2025-05-16 11:50:09 +1000

    fix(r2_bucket_event_notification): revert incorrect schema update

--- internal/services/r2_bucket_event_notification/resource.go
+++ internal/services/r2_bucket_event_notification/resource.go
@@ -161,7 +161,6 @@ func (r *R2BucketEventNotificationResource) Read(ctx context.Context, req resour
 	_, err := r.client.R2.Buckets.EventNotifications.Get(
 		ctx,
 		data.BucketName.ValueString(),
-		data.QueueID.ValueString(),
 		r2.BucketEventNotificationGetParams{
 			AccountID: cloudflare.F(data.AccountID.ValueString()),
 		},
