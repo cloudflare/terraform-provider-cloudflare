895e8f39432b37427aa7f2b8d20ba9b201b78f1e
Steve Conrad <sconrad@cloudflare.com>
2025-06-25 12:28:55 -0500

    fixing api token tests

--- internal/services/api_token/testdata/apitokenwithallcondition.tf
+++ internal/services/api_token/testdata/apitokenwithallcondition.tf
@@ -1,19 +1,20 @@
 
 resource "cloudflare_api_token" "%[1]s" {
 	name = "%[1]s"
+	status = "active"
 
 	policies = [{
 		effect = "allow"
 		permission_groups = [{
-	    id = "%[2]s"
+	    	id = "%[2]s"
 		}]
 		resources = { "com.cloudflare.api.account.zone.*" = "*" }
 	}]
 
 	condition = {
-    request_ip = {
-			in     = ["192.0.2.1/32"]
-			not_in = ["198.51.100.1/32"]
-		}
+		request_ip = {
+				in     = ["192.0.2.1/32"]
+				not_in = ["198.51.100.1/32"]
+			}
 	}
 }
