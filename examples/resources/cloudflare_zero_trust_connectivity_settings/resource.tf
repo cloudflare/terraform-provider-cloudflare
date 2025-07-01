1637abb627280051f3cdbf7b99126963ccac2d91
Brenno Oliveira <brenno.oliveira@deliveryhero.com>
2025-07-01 20:50:13 +0200

    add changelog, fix typo in example
    Signed-off-by: Brenno Oliveira <brenno.oliveira@deliveryhero.com>

--- examples/resources/cloudflare_zero_trust_connectivity_settings/resource.tf
+++ examples/resources/cloudflare_zero_trust_connectivity_settings/resource.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_zero_trust_connectivity_settings" "example" {
-  account_id          = "f037e56e89293a057740de681ac9abbe"
-  icmp_proxy_enabled  = true
-  offram_warp_enabled = true
+  account_id           = "f037e56e89293a057740de681ac9abbe"
+  icmp_proxy_enabled   = true
+  offramp_warp_enabled = true
 }
