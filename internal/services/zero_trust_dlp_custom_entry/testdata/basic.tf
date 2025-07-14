c4ce1bd6512d7245a780d7da4d86799b17db21a2
jjohnson <jjohnson@cloudflare.com>
2025-07-14 15:40:37 -0500

    Add acceptance tests for DLP entries

--- internal/services/zero_trust_dlp_custom_entry/testdata/basic.tf
+++ internal/services/zero_trust_dlp_custom_entry/testdata/basic.tf
@@ -1,1 +1,6 @@
-resource "cloudflare_zero_trust_dlp_custom_entry" "%[1]s" {}
+resource "cloudflare_zero_trust_dlp_custom_entry" "%[1]s" {
+  account_id = "%[2]s"
+  profile_id = "%[3]s"
+
+  enabled = "%[4]b"
+}
