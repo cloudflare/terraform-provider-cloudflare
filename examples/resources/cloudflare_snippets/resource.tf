9939965f3d2ab4bedf3a3ec1317a1ebdd3974329
Zak Cutner <zak@cloudflare.com>
2025-07-28 16:05:56 -0400

    Get `cloudflare_snippets` resource working
    * Update schema to support multiple files and to include the name of
      each file.
    
    * Fix multipart uploading of files by using the correct form field name,
      file name, and content type.
    
    * Use the get content endpoint to fetch the snippet's files and
      metadata when refreshing state.
    
    * Mark the created on attribute as `UseStateForUnknown` to prevent plan
      drift.
    
    * Update `apiform` to not marshal computed attributes. This matches the
      behavior of `apijson`, and prevents the snippet's created on and
      modified on attributes being sent in update requests.

--- examples/resources/cloudflare_snippets/resource.tf
+++ examples/resources/cloudflare_snippets/resource.tf
@@ -1,7 +1,18 @@
 resource "cloudflare_snippets" "example_snippets" {
-  zone_id = "9f1839b6152d298aca64c4e906b6d074"
+  zone_id      = "9f1839b6152d298aca64c4e906b6d074"
   snippet_name = "my_snippet"
-  files = [null]
+  files = [
+    {
+      name    = "main.js"
+      content = <<-EOT
+      export default {
+        async fetch(request) {
+          return new Response('Hello, World!');
+        }
+      }
+      EOT
+    }
+  ]
   metadata = {
     main_module = "main.js"
   }
