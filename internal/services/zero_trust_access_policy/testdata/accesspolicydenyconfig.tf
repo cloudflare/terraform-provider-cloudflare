a0326ed93e8e3d15294228387232d1f7a3cd43b4
Tamas Jozsa <tamas@cloudflare.com>
2025-08-01 16:32:47 +0100

    chore: modernize zero_trust_access_policy acceptance tests

--- internal/services/zero_trust_access_policy/testdata/accesspolicydenyconfig.tf
+++ internal/services/zero_trust_access_policy/testdata/accesspolicydenyconfig.tf
@@ -5,23 +5,13 @@ resource "cloudflare_zero_trust_access_application" "%[1]s" {
   type       = "self_hosted"
 }
 
-resource "cloudflare_zero_trust_access_group" "%[1]s" {
-    account_id     = "%[3]s"
-    name           = "%[1]s"
-    include =[{
-      ip = {
-        ip = "127.0.0.1/32"
-      }
-    }]
-}
-
 resource "cloudflare_zero_trust_access_policy" "%[1]s" {
   name           = "%[1]s"
   account_id     = "%[3]s"
-  decision       = "non_identity"
+  decision       = "deny"
   include = [{
-    group = {
-      id = cloudflare_zero_trust_access_group.%[1]s.id
+    email = {
+      email = "blocked@example.com"
     }
   }]
   approval_required              = "false"
