1776ca43e119b29116f1bc1db4c1b98238fece13
Eric Reeves <ereeves@cloudflare.com>
2025-08-01 06:47:45 -0500

    test(argo_smart_routing): add test cases

--- internal/services/argo_smart_routing/resource_test.go
+++ internal/services/argo_smart_routing/resource_test.go
@@ -3,6 +3,7 @@ package argo_smart_routing_test
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"testing"
 
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/acctest"
@@ -17,21 +18,63 @@ func TestAccCloudflareArgoSmartRouting_Basic(t *testing.T) {
 
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
+			acctest.TestAccPreCheck_ZoneID(t)
+			acctest.TestAccPreCheck_Credentials(t)
+		},
+		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckCloudflareArgoSmartRoutingEnable(zoneID, rnd),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(name, "id", zoneID),
+					resource.TestCheckResourceAttr(name, "zone_id", zoneID),
+					resource.TestCheckResourceAttr(name, "value", "on"),
+				),
+			},
+			{
+				Config: testAccCheckCloudflareArgoSmartRoutingDisable(zoneID, rnd),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(name, "id", zoneID),
+					resource.TestCheckResourceAttr(name, "zone_id", zoneID),
+					resource.TestCheckResourceAttr(name, "value", "off"),
+				),
+			},
+			{
+				ResourceName:      name,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccCloudflareArgoSmartRouting_InvalidValue(t *testing.T) {
+	zoneID := os.Getenv("CLOUDFLARE_ZONE_ID")
+	rnd := utils.GenerateRandomResourceName()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
 			acctest.TestAccPreCheck_AccountID(t)
 			acctest.TestAccPreCheck_Credentials(t)
 		},
 		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckCloudflareArgoSmartRoutingBasic(zoneID, rnd),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(name, "value", "on"),
-				),
+				Config:      testAccCheckCloudflareArgoSmartRoutingInvalidValue(zoneID, rnd),
+				ExpectError: regexp.MustCompile(regexp.QuoteMeta("Invalid Attribute Value Match")),
 			},
 		},
 	})
 }
 
-func testAccCheckCloudflareArgoSmartRoutingBasic(zoneID, name string) string {
-	return acctest.LoadTestCase("basic.tf", zoneID, name)
+func testAccCheckCloudflareArgoSmartRoutingEnable(zoneID, name string) string {
+	return acctest.LoadTestCase("enable.tf", zoneID, name)
+}
+
+func testAccCheckCloudflareArgoSmartRoutingDisable(zoneID, name string) string {
+	return acctest.LoadTestCase("disable.tf", zoneID, name)
+}
+
+func testAccCheckCloudflareArgoSmartRoutingInvalidValue(zoneID, name string) string {
+	return acctest.LoadTestCase("invalid_value.tf", zoneID, name)
 }
