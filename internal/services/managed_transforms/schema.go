3edd7ec9c86efe1fc68f3acb7fb6c7aa8ef2a484
Diogo Sousa <dsousa@cloudflare.com>
2025-08-06 17:04:24 +0100

    Manually add missing "managed transforms" items to the plan to avoid drifting.

--- internal/services/managed_transforms/schema.go
+++ internal/services/managed_transforms/schema.go
@@ -4,8 +4,10 @@ package managed_transforms
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/customfield"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
@@ -53,6 +55,7 @@ func ResourceSchema(ctx context.Context) schema.Schema {
 						},
 					},
 				},
+				PlanModifiers: []planmodifier.Set{newMergeDefaultStateValues()},
 			},
 			"managed_response_headers": schema.SetNestedAttribute{
 				Description: "The list of Managed Response Transforms.",
@@ -79,6 +82,7 @@ func ResourceSchema(ctx context.Context) schema.Schema {
 						},
 					},
 				},
+				PlanModifiers: []planmodifier.Set{newMergeDefaultStateValues()},
 			},
 		},
 	}
@@ -90,4 +94,116 @@ func (r *ManagedTransformsResource) Schema(ctx context.Context, req resource.Sch
 
 func (r *ManagedTransformsResource) ConfigValidators(_ context.Context) []resource.ConfigValidator {
 	return []resource.ConfigValidator{}
+}
+
+
+type mergeDefaultStateValues struct{}
+
+// WIP! explain
+func newMergeDefaultStateValues() planmodifier.Set {
+	return mergeDefaultStateValues{}
+}
+
+func (m mergeDefaultStateValues) Description(_ context.Context) string {
+	return "WIP!"
+}
+
+func (m mergeDefaultStateValues) MarkdownDescription(_ context.Context) string {
+	return "WIP!"
+}
+
+func (m mergeDefaultStateValues) PlanModifySet(ctx context.Context, req planmodifier.SetRequest, resp *planmodifier.SetResponse) {
+	getId := func (v attr.Value) types.String {
+		objValue, ok := v.(types.Object)
+
+		if !ok {
+			panic("unexpected value type")
+		}
+
+		id, ok := objValue.Attributes()["id"].(types.String)
+
+		if !ok {
+			panic("unexpected value type")
+		}
+
+		return id
+	}
+	getEnabled := func (v attr.Value) types.Bool {
+		objValue, ok := v.(types.Object)
+
+		if !ok {
+			panic("unexpected value type")
+		}
+
+		id, ok := objValue.Attributes()["enabled"].(types.Bool)
+
+		if !ok {
+			panic("unexpected value type")
+		}
+
+		return id
+	}
+
+	if req.StateValue.IsNull() {
+		return
+	}
+
+	fmt.Printf("\nstate\n")
+	for _, r := range req.StateValue.Elements() {
+		fmt.Printf("    %v\n", r)
+	}
+
+	fmt.Printf("\n\nplan\n")
+	for _, r := range req.PlanValue.Elements() {
+		fmt.Printf("    %v\n", r)
+	}
+
+	fmt.Printf("\nmerging...\n")
+
+	planTransformationIds := make(map[string]bool)
+
+	for _, transformation := range req.PlanValue.Elements() {
+		id := getId(transformation)
+
+		if id.IsNull() || id.IsUnknown() {
+			continue
+		}
+
+		planTransformationIds[id.ValueString()] = true
+	}
+
+	newPlanElements := req.PlanValue.Elements()
+
+	for _, transformation := range req.StateValue.Elements() {
+		id := getId(transformation)
+
+		if id.IsNull() || id.IsUnknown() {
+			continue
+		}
+
+		enabled := getEnabled(transformation)
+
+		if enabled.IsNull() || enabled.IsUnknown() {
+			continue
+		}
+
+		transformationInDefaultState := !enabled.ValueBool()
+
+		if !planTransformationIds[id.ValueString()] && transformationInDefaultState {
+			newPlanElements = append(newPlanElements, transformation)
+
+			fmt.Printf("    adding %v\n", transformation)
+		}
+	}
+
+	newPlanValue, _ := types.SetValue(req.PlanValue.ElementType(ctx), newPlanElements)
+
+	resp.PlanValue = newPlanValue
+
+	fmt.Printf("\n\nnew plan\n")
+	for _, r := range resp.PlanValue.Elements() {
+		fmt.Printf("    %v\n", r)
+	}
+
+	fmt.Printf("\n================================================================\n\n")
 }
