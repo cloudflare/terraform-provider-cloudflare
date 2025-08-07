b2ee8112505920afb4bdc5661c851f3407788a00
Denis Davydov <ddavydov@cloudflare.com>
2025-08-07 17:12:14 +0100

    Get cloudflare_snippet resource working
    Reapplying https://github.com/cloudflare/terraform-provider-cloudflare/pull/5809 but to renamed resource (snippet instead of snippets).

--- internal/services/snippet/testdata/basic_update.tf
+++ internal/services/snippet/testdata/basic_update.tf
@@ -1,7 +1,18 @@
 resource "cloudflare_snippet" "%[1]s" {
   zone_id      = "%[2]s"
   snippet_name = "%[1]s"
-  files        = "export { async function fetch(request, env) { return new Response('Hello Updated World!'); } }"
+  files = [
+    {
+      name    = "main.js"
+      content = <<-EOT
+      export default {
+        async fetch(request) {
+          return new Response('Hello, World!');
+        }
+      }
+      EOT
+    }
+  ]
   metadata = {
     main_module = "main.js"
   }
