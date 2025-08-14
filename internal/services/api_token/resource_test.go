560228a1b28106b875aa3bd68c42dbdaf42bfef6
Xuran Wang <xuran@cloudflare.com>
2025-08-14 13:23:18 -0700

    ACCT-10019 assertion for structure counts of token acceptance tests

--- internal/services/api_token/resource_test.go
+++ internal/services/api_token/resource_test.go
@@ -370,6 +370,9 @@ func TestAccAPIToken_Resources_SimpleToNested_NoDrift(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(name, "name", rnd),
 					resource.TestCheckResourceAttr(name, "policies.0.permission_groups.0.id", permissionID),
+					// resources should be a single-entry map
+					resource.TestCheckResourceAttr(name, "policies.#", "1"),
+					resource.TestCheckResourceAttr(name, "policies.0.resources.%", "1"),
 				),
 			},
 			{
@@ -387,6 +390,11 @@ func TestAccAPIToken_Resources_SimpleToNested_NoDrift(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(name, "name", rnd),
 					resource.TestCheckResourceAttr(name, "policies.0.permission_groups.0.id", permissionID),
+					// top-level resources should have one account key
+					resource.TestCheckResourceAttr(name, "policies.#", "1"),
+					resource.TestCheckResourceAttr(name, "policies.0.resources.%", "1"),
+					// nested map under the account key should have one entry
+					resource.TestCheckResourceAttr(name, fmt.Sprintf("policies.0.resources.com.cloudflare.api.account.%s.%%", accountID), "1"),
 				),
 			},
 			{
