991e4546f1902d648dbcd69cc971f622f351fba0
Cina Saffary <cina@cloudflare.com>
2025-08-14 19:44:54 -0500

    Update "cloudflare_worker_version" example

--- examples/resources/cloudflare_worker_version/resource.tf
+++ examples/resources/cloudflare_worker_version/resource.tf
@@ -41,12 +41,11 @@ resource "cloudflare_worker_version" "example_worker_version" {
     }]
   }
   modules = [{
-    content_base64 = "ZXhwb3J0IGRlZmF1bHQgewogIGFzeW5jIGZldGNoKHJlcXVlc3QsIGVudiwgY3R4KSB7CiAgICByZXR1cm4gbmV3IFJlc3BvbnNlKCdIZWxsbyBXb3JsZCEnKQogIH0KfQ=="
+    content_file = "dist/index.js"
     content_type = "application/javascript+module"
     name = "index.js"
   }]
   placement = {
     mode = "smart"
   }
-  usage_model = "standard"
 }
