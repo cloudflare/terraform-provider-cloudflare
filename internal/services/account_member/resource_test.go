2a85180b74a29544fa475b5d6d406584ff4a4cbb
Xuran Wang <xuran@cloudflare.com>
2025-08-26 14:18:17 -0700

    ACCT-10509 update custom code to better handle roles based member create/update

--- internal/services/account_member/resource_test.go
+++ internal/services/account_member/resource_test.go
@@ -178,8 +178,8 @@ func TestAccCloudflareAccountMember_RolesVsPolicies(t *testing.T) {
 					resource.TestCheckResourceAttr(name, "email", fmt.Sprintf("%s@example.com", rnd)),
 					resource.TestCheckResourceAttr(name, "roles.#", "1"),
 					resource.TestCheckResourceAttr(name, "roles.0", "05784afa30c1afe1440e79d9351c7430"),
-					// Policies should not be set when using roles
-					resource.TestCheckNoResourceAttr(name, "policies.0"),
+					// When using roles, policies are auto-generated but should not cause diffs
+					// We verify this by the test completing without plan differences
 				),
 			},
 			{
@@ -195,56 +195,54 @@ func TestAccCloudflareAccountMember_RolesVsPolicies(t *testing.T) {
 	})
 }
 
-func testCloudflareAccountMemberDirectAdd(resourceID, emailAddress, accountID string) string {
-	return acctest.LoadTestCase("cloudflareaccountmemberdirectadd.tf", resourceID, emailAddress, accountID)
+func testCloudflareAccountMemberDirectAdd(accountID, emailAddress string) string {
+	return acctest.LoadTestCase("cloudflareaccountmemberdirectadd.tf", accountID, emailAddress)
 }
 
 func testCloudflareAccountMemberRolesConfig(resourceID, emailAddress, accountID, roleID string) string {
 	return acctest.LoadTestCase("cloudflareaccountmemberrolesconfig.tf", resourceID, emailAddress, accountID, roleID)
 }
 
-// func TestAccCloudflareAccountMember_Policies(t *testing.T) {
-// 	rnd := utils.GenerateRandomResourceName()
-// 	name := "cloudflare_account_member." + rnd
-// 	accountID := os.Getenv("CLOUDFLARE_ACCOUNT_ID")
+func TestAccCloudflareAccountMember_Policies(t *testing.T) {
+	rnd := utils.GenerateRandomResourceName()
+	name := "cloudflare_account_member." + rnd
+	accountID := os.Getenv("CLOUDFLARE_ACCOUNT_ID")
 
-// 	resource.Test(t, resource.TestCase{
-// 		PreCheck: func() {
-// 			acctest.TestAccPreCheck_AccountID(t)
-// 			acctest.TestAccPreCheck_Credentials(t)
-// 		},
-// 		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
-// 		Steps: []resource.TestStep{
-// 			{
-// 				Config: testCloudflareAccountMemberPoliciesConfig(rnd, fmt.Sprintf("%s@example.com", rnd), accountID, "8e23b19e4e0d44c29d239c5688ba8cbb", accountID),
-// 				Check: resource.ComposeTestCheckFunc(
-// 					resource.TestCheckResourceAttr(name, consts.AccountIDSchemaKey, accountID),
-// 					resource.TestCheckResourceAttr(name, "email", fmt.Sprintf("%s@example.com", rnd)),
-// 					resource.TestCheckResourceAttr(name, "policies.#", "1"),
-// 					resource.TestCheckResourceAttr(name, "policies.0.access", "allow"),
-// 					resource.TestCheckResourceAttr(name, "policies.0.permission_groups.#", "1"),
-// 					resource.TestCheckResourceAttr(name, "policies.0.permission_groups.0.id", "8e23b19e4e0d44c29d239c5688ba8cbb"),
-// 					resource.TestCheckResourceAttr(name, "policies.0.resource_groups.#", "1"),
-// 					resource.TestCheckResourceAttr(name, "policies.0.resource_groups.0.scope.key", fmt.Sprintf("com.cloudflare.api.account.%s", accountID)),
-// 					resource.TestCheckResourceAttr(name, "policies.0.resource_groups.0.scope.objects.#", "1"),
-// 					resource.TestCheckResourceAttr(name, "policies.0.resource_groups.0.scope.objects.0.key", "*"),
-// 					// Roles should not be set when using policies
-// 					resource.TestCheckNoResourceAttr(name, "roles.0"),
-// 				),
-// 			},
-// 			{
-// 				// Second apply should not cause any changes (stable state)
-// 				Config: testCloudflareAccountMemberPoliciesConfig(rnd, fmt.Sprintf("%s@example.com", rnd), accountID, "8e23b19e4e0d44c29d239c5688ba8cbb", accountID),
-// 				Check: resource.ComposeTestCheckFunc(
-// 					resource.TestCheckResourceAttr(name, consts.AccountIDSchemaKey, accountID),
-// 					resource.TestCheckResourceAttr(name, "policies.#", "1"),
-// 					resource.TestCheckResourceAttr(name, "policies.0.access", "allow"),
-// 				),
-// 			},
-// 		},
-// 	})
-// }
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.TestAccPreCheck_AccountID(t)
+			acctest.TestAccPreCheck_Credentials(t)
+		},
+		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testCloudflareAccountMemberPoliciesConfig(accountID, rnd, accountID, fmt.Sprintf("%s@example.com", rnd), "8e23b19e4e0d44c29d239c5688ba8cbb"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(name, consts.AccountIDSchemaKey, accountID),
+					resource.TestCheckResourceAttr(name, "email", fmt.Sprintf("%s@example.com", rnd)),
+					resource.TestCheckResourceAttr(name, "policies.#", "1"),
+					resource.TestCheckResourceAttr(name, "policies.0.access", "allow"),
+					resource.TestCheckResourceAttr(name, "policies.0.permission_groups.#", "1"),
+					resource.TestCheckResourceAttr(name, "policies.0.permission_groups.0.id", "8e23b19e4e0d44c29d239c5688ba8cbb"),
+					resource.TestCheckResourceAttr(name, "policies.0.resource_groups.#", "1"),
+					resource.TestCheckResourceAttrSet(name, "policies.0.resource_groups.0.id"),
+					// Roles should not be set when using policies
+					resource.TestCheckNoResourceAttr(name, "roles.0"),
+				),
+			},
+			{
+				// Second apply should not cause any changes (stable state)
+				Config: testCloudflareAccountMemberPoliciesConfig(accountID, rnd, accountID, fmt.Sprintf("%s@example.com", rnd), "8e23b19e4e0d44c29d239c5688ba8cbb"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(name, consts.AccountIDSchemaKey, accountID),
+					resource.TestCheckResourceAttr(name, "policies.#", "1"),
+					resource.TestCheckResourceAttr(name, "policies.0.access", "allow"),
+				),
+			},
+		},
+	})
+}
 
-// func testCloudflareAccountMemberPoliciesConfig(resourceID, emailAddress, accountID, permissionGroupID, scopeAccountID string) string {
-// 	return acctest.LoadTestCase("cloudflareaccountmemberpoliciesconfig.tf", resourceID, accountID, emailAddress, permissionGroupID, scopeAccountID)
-// }
+func testCloudflareAccountMemberPoliciesConfig(dataSourceAccountID, resourceID, accountID, emailAddress, permissionGroupID string) string {
+	return acctest.LoadTestCase("cloudflareaccountmemberpoliciesconfig.tf", dataSourceAccountID, resourceID, accountID, emailAddress, permissionGroupID)
+}
