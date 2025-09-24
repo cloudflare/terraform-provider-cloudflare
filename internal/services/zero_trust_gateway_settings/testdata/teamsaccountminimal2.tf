8116f7765ad34b8de28b077d21a1d5873e631d67
Rex Scaria <arunrex@cloudflare.com>
2025-09-24 11:28:15 -0400

    tests: added acceptance tests + fixes for gateway policy + certificates + lists + settings

--- internal/services/zero_trust_gateway_settings/testdata/teamsaccountminimal2.tf
+++ internal/services/zero_trust_gateway_settings/testdata/teamsaccountminimal2.tf
@@ -1,15 +1,6 @@
 resource "cloudflare_zero_trust_gateway_settings" "%[1]s" {
   account_id = "%[2]s"
   settings = {
-    protocol_detection = {
-        enabled = true
-    }
-    tls_decrypt = {
-      enabled = true
-    }
-    activity_log = {
-      enabled = true
-    }
     browser_isolation = {
       url_browser_isolation_enabled = true
       non_identity_enabled = false
@@ -30,19 +21,6 @@ resource "cloudflare_zero_trust_gateway_settings" "%[1]s" {
     }
     body_scanning = {
       inspection_mode = "deep"
-    }
-    fips = {
-      tls = true
-    }
-    antivirus = {
-      enabled_download_phase = true
-      enabled_upload_phase = false
-      fail_closed = true
-      notification_settings = {
-          enabled = true
-          msg = "msg"
-          support_url = "https://hello.com/"
-      }
     }
     extended_email_matching = {
       enabled = true
