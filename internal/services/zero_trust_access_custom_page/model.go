0ff8a4dd16ce7337ef50b5580a3eec8ad1fb38ba
Alex Holland <aholland@cloudflare.com>
2025-09-24 04:57:47 -0400

    Remove created_at, updated_at, app_count (#6250)

--- internal/services/zero_trust_access_custom_page/model.go
+++ internal/services/zero_trust_access_custom_page/model.go
@@ -3,7 +3,6 @@
 package zero_trust_access_custom_page
 
 import (
-	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/apijson"
@@ -14,15 +13,12 @@ type ZeroTrustAccessCustomPageResultEnvelope struct {
 }
 
 type ZeroTrustAccessCustomPageModel struct {
-	ID         types.String      `tfsdk:"id" json:"-,computed"`
-	UID        types.String      `tfsdk:"uid" json:"uid,computed"`
-	AccountID  types.String      `tfsdk:"account_id" path:"account_id,required"`
-	CustomHTML types.String      `tfsdk:"custom_html" json:"custom_html,required"`
-	Name       types.String      `tfsdk:"name" json:"name,required"`
-	Type       types.String      `tfsdk:"type" json:"type,required"`
-	AppCount   types.Int64       `tfsdk:"app_count" json:"app_count,computed"`
-	CreatedAt  timetypes.RFC3339 `tfsdk:"created_at" json:"created_at,computed" format:"date-time"`
-	UpdatedAt  timetypes.RFC3339 `tfsdk:"updated_at" json:"updated_at,computed" format:"date-time"`
+	ID         types.String `tfsdk:"id" json:"-,computed"`
+	UID        types.String `tfsdk:"uid" json:"uid,computed"`
+	AccountID  types.String `tfsdk:"account_id" path:"account_id,required"`
+	CustomHTML types.String `tfsdk:"custom_html" json:"custom_html,required"`
+	Name       types.String `tfsdk:"name" json:"name,required"`
+	Type       types.String `tfsdk:"type" json:"type,required"`
 }
 
 func (m ZeroTrustAccessCustomPageModel) MarshalJSON() (data []byte, err error) {
