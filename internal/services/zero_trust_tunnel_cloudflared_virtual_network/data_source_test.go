798d6dbe1e9d8229cc5c7a63117573c2744077d4
lneto <lneto@cloudflare.com>
2025-09-29 15:27:06 +0100

    TUN-9814: Update virtual network resource

--- internal/services/zero_trust_tunnel_cloudflared_virtual_network/data_source_test.go
+++ internal/services/zero_trust_tunnel_cloudflared_virtual_network/data_source_test.go
@@ -7,10 +7,18 @@ import (
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/acctest"
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/utils"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 )
 
-func TestAccCloudflareTunneVirtualNetworkDatasource_MatchName(t *testing.T) {
+func TestAccCloudflareTunnelVirtualNetworkDatasource_MatchName(t *testing.T) {
 	rnd := utils.GenerateRandomResourceName()
+	resourceName := "cloudflare_zero_trust_tunnel_cloudflared_virtual_network." + rnd
+	dataSourceName := "data.cloudflare_zero_trust_tunnel_cloudflared_virtual_network." + rnd
+	acctest.TestAccPreCheck_AccountID(t)
+
+	accountID := os.Getenv("CLOUDFLARE_ACCOUNT_ID")
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
@@ -18,9 +26,21 @@ func TestAccCloudflareTunneVirtualNetworkDatasource_MatchName(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testCloudflareTunnelVirtualNetworkMatchName(rnd),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.cloudflare_zero_trust_tunnel_cloudflared_virtual_network."+rnd, "comment", "test"),
-				),
+				ConfigStateChecks: []statecheck.StateCheck{
+					// Check resource attributes
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("account_id"), knownvalue.StringExact(accountID)),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("name"), knownvalue.StringExact(rnd)),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("comment"), knownvalue.StringExact("test")),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("is_default_network"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue(resourceName, tfjsonpath.New("created_at"), knownvalue.NotNull()),
+
+					// Check data source attributes match resource attributes
+					statecheck.ExpectKnownValue(dataSourceName, tfjsonpath.New("account_id"), knownvalue.StringExact(accountID)),
+					statecheck.ExpectKnownValue(dataSourceName, tfjsonpath.New("name"), knownvalue.StringExact(rnd)),
+					statecheck.ExpectKnownValue(dataSourceName, tfjsonpath.New("comment"), knownvalue.StringExact("test")),
+					statecheck.ExpectKnownValue(dataSourceName, tfjsonpath.New("is_default_network"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue(dataSourceName, tfjsonpath.New("created_at"), knownvalue.NotNull()),
+				},
 			},
 		},
 	})
