09cad68d7d9f8bafa9e58d89b250724a7f67e514
Vaishak Dinesh <vaishak@cloudflare.com>
2025-09-30 09:35:24 -0700

    chore: test

--- internal/services/queue/model.go
+++ internal/services/queue/model.go
@@ -16,7 +16,7 @@ type QueueModel struct {
 	ID                  types.String                                      `tfsdk:"id" json:"-,computed"`
 	QueueID             types.String                                      `tfsdk:"queue_id" json:"queue_id,computed"`
 	AccountID           types.String                                      `tfsdk:"account_id" path:"account_id,required"`
-	QueueName           types.String                                      `tfsdk:"queue_name" json:"queue_name,required"`
+	QueueName           types.String                                      `tfsdk:"queue_name" json:"queue_name,no_refresh"`
 	Settings            customfield.NestedObject[QueueSettingsModel]      `tfsdk:"settings" json:"settings,computed_optional"`
 	ConsumersTotalCount types.Float64                                     `tfsdk:"consumers_total_count" json:"consumers_total_count,computed"`
 	CreatedOn           types.String                                      `tfsdk:"created_on" json:"created_on,computed"`
