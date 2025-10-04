6cef8985302da3e8ccfe8f4389835eaf21dde93d
AdamBouhmad <adbouhmad@gmail.com>
2025-10-03 17:23:17 -0700

    fix policies test by selecting correct resource group

--- internal/services/account_member/testdata/cloudflareaccountmemberpoliciesconfig.tf
+++ internal/services/account_member/testdata/cloudflareaccountmemberpoliciesconfig.tf
@@ -1,18 +1,20 @@
 data "cloudflare_resource_groups" "example_resource_groups" {
-  account_id = "%s"
+  account_id = "%[1]s"
+  name       = "com.cloudflare.api.account.%[1]s"
 }
 
-resource "cloudflare_account_member" "%s" {
-  account_id = "%s"
-  email      = "%s"
+resource "cloudflare_account_member" "%[2]s" {
+  account_id = "%[3]s"
+  email      = "%[4]s"
   status     = "pending"
   policies = [{
     access = "allow"
     permission_groups = [{
-      id = "%s"
+      id = "%[5]s"
     }]
     resource_groups = [{
       id = data.cloudflare_resource_groups.example_resource_groups.result[0].id
     }]
   }]
 }
+
