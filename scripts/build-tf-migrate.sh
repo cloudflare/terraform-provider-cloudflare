1301b400aacfbf81b4745319bac19b71f346fcbf
Tamas Jozsa <tamas@cloudflare.com>
2025-10-30 15:34:13 +0000

    chore: uptake migrator v2 for dns_record

--- scripts/build-tf-migrate.sh
+++ scripts/build-tf-migrate.sh
@@ -2,19 +2,8 @@
 
 set -e
 
-# Clone the repository
-if [ -d "tf-migrate" ]; then
-    echo "tf-migrate directory already exists. Pulling latest changes..."
-    cd tf-migrate
-    git pull
-else
-    echo "Cloning tf-migrate repository..."
-    git clone https://github.com/cloudflare/tf-migrate.git tf-migrate
-    cd tf-migrate
-fi
-
-# Build the binary
-echo "Building tf-migrate binary..."
-go build -o tf-migrate
+# Install tf-migrate binary to the current directory
+echo "Installing tf-migrate binary..."
+GOBIN=$(pwd) go install github.com/cloudflare/tf-migrate/cmd/tf-migrate@latest
 
 echo "Build complete! Binary available at: $(pwd)/tf-migrate"
