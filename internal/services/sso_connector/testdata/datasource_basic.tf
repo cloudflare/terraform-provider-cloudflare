bc6126eba758127ee5559a2a7bd9a593b3de7929
scabell <scabell@cloudflare.com>
2025-11-05 14:43:12 -0700

    Create ZT IDP before attempting SSO connector operations

--- internal/services/sso_connector/testdata/datasource_basic.tf
+++ internal/services/sso_connector/testdata/datasource_basic.tf
@@ -1,11 +1,23 @@
+resource "cloudflare_zero_trust_access_identity_provider" "%[1]s" {
+  account_id = "%[2]s"
+  name       = "%[1]s"
+  type       = "azureAD"
+  config = {
+    client_id     = "test"
+    client_secret = "test"
+    directory_id  = "directory"
+  }
+}
+
 resource "cloudflare_sso_connector" "%[1]s" {
   account_id         = "%[2]s"
   email_domain       = "%[1]s.example.com"
   begin_verification = false
   enabled            = false
+  depends_on         = [cloudflare_zero_trust_access_identity_provider.%[1]s]
 }
 
 data "cloudflare_sso_connector" "%[1]s" {
-  account_id = "%[2]s"
+  account_id       = "%[2]s"
   sso_connector_id = cloudflare_sso_connector.%[1]s.id
 }
