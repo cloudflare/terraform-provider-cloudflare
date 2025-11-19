432160ef04c30bb13072a2eec84231c197432e69
Musa Jundi <musa@cloudflare.com>
2025-11-19 14:45:34 -0600

    chore(byoip): integrate generated changes for BYOIP resources

--- examples/resources/cloudflare_byo_ip_prefix/resource.tf
+++ examples/resources/cloudflare_byo_ip_prefix/resource.tf
@@ -1,6 +1,7 @@
 resource "cloudflare_byo_ip_prefix" "example_byo_ip_prefix" {
   account_id = "258def64c72dae45f3e4c8516e2111f2"
-  asn = 209242
+  asn = 13335
   cidr = "192.0.2.0/24"
-  loa_document_id = "d933b1530bc56c9953cf8ce166da8004"
+  delegate_loa_creation = true
+  description = "Internal test prefix"
 }
