0957f09c1c1ce7f8ba2d20de9101df3e4e1d4fb4
Vaishak Dinesh <vaishak@cloudflare.com>
2026-02-11 13:53:25 -0800

    feat(spectrum_application): state upgrader

--- internal/services/spectrum_application/schema.go
+++ internal/services/spectrum_application/schema.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/customfield"
 	"github.com/cloudflare/terraform-provider-cloudflare/internal/customvalidator"
+	"github.com/cloudflare/terraform-provider-cloudflare/internal/migrations"
 	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
 	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
@@ -26,7 +27,7 @@ var _ resource.ResourceWithConfigValidators = (*SpectrumApplicationResource)(nil
 
 func ResourceSchema(ctx context.Context) schema.Schema {
 	return schema.Schema{
-		Version: 1,
+		Version: migrations.GetSchemaVersion(1, 500),
 		Attributes: map[string]schema.Attribute{
 			"id": schema.StringAttribute{
 				Description:   "App identifier.",
