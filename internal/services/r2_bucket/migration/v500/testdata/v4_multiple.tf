460269d0105589634ec0d9353968120c8c38199c
rotem <rotem@cloudflare.com>
2026-02-11 10:33:55 -0600

    feat(r2_bucket): add state upgrader logic

---
+++ internal/services/r2_bucket/migration/v500/testdata/v4_multiple.tf
@@ -0,0 +1,10 @@
+resource "cloudflare_r2_bucket" "%s" {
+  account_id = "%s"
+  name       = "%s"
+}
+
+resource "cloudflare_r2_bucket" "%s" {
+  account_id = "%s"
+  name       = "%s"
+  location   = "EEUR"
+}
---
+++ internal/services/r2_bucket/migration/v500/testdata/v4_with_location.tf
@@ -0,0 +1,5 @@
+resource "cloudflare_r2_bucket" "%s" {
+  account_id = "%s"
+  name       = "%s"
+  location   = "WNAM"
+}
