c8c88f08ed77458584177d8fa45ec01df93153a3
Vaishak Dinesh <vaishak@cloudflare.com>
2026-02-14 19:53:30 -0800

    feat(zero_trust_access_identity_provider): state upgraders

---
+++ internal/services/zero_trust_access_identity_provider/migration/v500/testdata/v4_azuread_scim.tf
@@ -0,0 +1,16 @@
+resource "cloudflare_access_identity_provider" "%[1]s" {
+  account_id = "%[2]s"
+  name       = "%[1]s"
+  type       = "azureAD"
+  config {
+    client_id     = "test"
+    client_secret = "test"
+    directory_id  = "directory"
+  }
+  scim_config {
+    enabled                   = true
+    seat_deprovision          = true
+    user_deprovision          = true
+    identity_update_behavior  = "no_action"
+  }
+}
---
+++ internal/services/zero_trust_access_identity_provider/migration/v500/testdata/v4_saml.tf
@@ -0,0 +1,12 @@
+resource "cloudflare_access_identity_provider" "%[1]s" {
+  account_id = "%[2]s"
+  name       = "%[1]s"
+  type       = "saml"
+  config {
+    issuer_url       = "jumpcloud"
+    sso_target_url   = "https://sso.myexample.jumpcloud.com/saml2/cloudflareaccess"
+    attributes       = ["email", "username"]
+    sign_request     = true
+    idp_public_cert  = "MIIDpDCCAoygAwIBAgIGAV2ka+55MA0GCSqGSIb3DQEBCwUAMIGSMQswCQYDVQQGEwJVUzETMBEGA1UEC.....GF/Q2/MHadws97cZguTnQyuOqPuHbnN83d/2l1NSYKCbHt24o"
+  }
+}
