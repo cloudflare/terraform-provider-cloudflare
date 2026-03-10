46253fce9f9ad9f088f94f4a2e84ba880ce8d4f9
Garvit Gupta <garvit@cloudflare.com>
2026-03-09 22:03:00 -0500

    test(pipelines): add acceptance tests

---
+++ internal/services/pipeline/testdata/pipeline_basic.tf
@@ -0,0 +1,82 @@
+resource "cloudflare_r2_bucket" "%[1]s" {
+  account_id = "%[2]s"
+  name       = "%[1]s"
+}
+
+resource "cloudflare_r2_data_catalog" "%[1]s" {
+  account_id  = "%[2]s"
+  bucket_name = cloudflare_r2_bucket.%[1]s.name
+}
+
+data "cloudflare_account_api_token_permission_groups_list" "r2_bucket_item_write" {
+  account_id = "%[2]s"
+  name       = "Workers R2 Storage Bucket Item Write"
+}
+
+data "cloudflare_account_api_token_permission_groups_list" "r2_data_catalog_write" {
+  account_id = "%[2]s"
+  name       = "Workers R2 Data Catalog Write"
+}
+
+resource "cloudflare_account_token" "%[1]s" {
+  name       = "%[1]s"
+  account_id = "%[2]s"
+
+  policies = [{
+    effect = "allow"
+    permission_groups = [
+      { id = data.cloudflare_account_api_token_permission_groups_list.r2_bucket_item_write.result[0].id },
+      { id = data.cloudflare_account_api_token_permission_groups_list.r2_data_catalog_write.result[0].id },
+    ]
+    resources = jsonencode({
+      "com.cloudflare.api.account.%[2]s" = "*"
+    })
+  }]
+}
+
+resource "cloudflare_pipeline_sink" "%[1]s" {
+  account_id = "%[2]s"
+  name       = "%[1]s_sink"
+  type       = "r2_data_catalog"
+  format = {
+    type = "parquet"
+  }
+  schema = {
+    fields = []
+  }
+  config = {
+    account_id = "%[2]s"
+    bucket     = cloudflare_r2_bucket.%[1]s.name
+    table_name = cloudflare_r2_data_catalog.%[1]s.name
+    token      = cloudflare_account_token.%[1]s.value
+  }
+}
+
+resource "cloudflare_pipeline_stream" "%[1]s" {
+  account_id = "%[2]s"
+  name       = "%[1]s_stream"
+  format = {
+    type = "json"
+  }
+  schema = {
+    fields = [{
+      name     = "value"
+      type     = "json"
+      required = true
+    }]
+  }
+  http = {
+    enabled        = true
+    authentication = false
+    cors           = {}
+  }
+  worker_binding = {
+    enabled = false
+  }
+}
+
+resource "cloudflare_pipeline" "%[1]s" {
+  account_id = "%[2]s"
+  name       = "%[1]s"
+  sql        = "INSERT INTO ${cloudflare_pipeline_sink.%[1]s.name} SELECT * FROM ${cloudflare_pipeline_stream.%[1]s.name}"
+}
