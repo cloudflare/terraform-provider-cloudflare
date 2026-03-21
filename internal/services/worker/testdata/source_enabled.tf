fa3a9bb0c18f2e343fb759e2621c923da7b4a58b
trafgals <trafgals@github>
2026-03-21 21:42:20 +1000

    fix(worker): use source = {...} syntax in test fixture
    The source block needs to use argument assignment syntax (source = {...})
    rather than block syntax (source {...}) to be properly recognized
    by the Terraform plugin framework in acceptance tests.

--- internal/services/worker/testdata/source_enabled.tf
+++ internal/services/worker/testdata/source_enabled.tf
@@ -2,9 +2,9 @@ resource "cloudflare_worker" "%[1]s" {
   account_id = "%[2]s"
   name       = "%[1]s"
 
-  source {
+  source = {
     type = "github"
-    config {
+    config = {
       branch                         = "main"
       build_command                  = "npm run build"
       deploy_command                 = "npm run deploy"
