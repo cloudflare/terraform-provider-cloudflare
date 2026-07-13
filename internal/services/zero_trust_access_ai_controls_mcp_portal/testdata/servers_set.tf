832b47697fd090791a6d5ba8de2306e4785bdd56
Michael Borkenstein <mborkenstein@cloudflare.com>
2026-07-13 16:17:45 -0500

    fix(zero_trust_access_ai_controls_mcp_portal): migrate servers state to set

--- internal/services/zero_trust_access_ai_controls_mcp_portal/testdata/servers_set.tf
+++ internal/services/zero_trust_access_ai_controls_mcp_portal/testdata/servers_set.tf
@@ -4,6 +4,7 @@ resource "cloudflare_zero_trust_access_ai_controls_mcp_server" "a" {
   name       = "%[3]s-srv-a"
   hostname   = "https://%[3]s-srv-a.%[2]s/mcp"
   auth_type  = "bearer"
+  is_shared_oauth_callback_enabled = false
 }
 
 resource "cloudflare_zero_trust_access_ai_controls_mcp_server" "b" {
@@ -12,6 +13,7 @@ resource "cloudflare_zero_trust_access_ai_controls_mcp_server" "b" {
   name       = "%[3]s-srv-b"
   hostname   = "https://%[3]s-srv-b.%[2]s/mcp"
   auth_type  = "bearer"
+  is_shared_oauth_callback_enabled = false
 }
 
 resource "cloudflare_zero_trust_access_ai_controls_mcp_portal" "tf-test" {
